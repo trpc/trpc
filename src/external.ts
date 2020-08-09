fd78cf986c310a423733083260ad08e6d793d96c
Colin McDonnell <colinmcd@alum.mit.edu>
2020-08-08 22:55:47 -0700

    adding error to export

--- src/external.ts
+++ src/external.ts
@@ -1,6 +1,6 @@
-import { TRPCApi, TRPCEndpoint, TRPCRouter } from './internal';
+import { TRPCApi, TRPCEndpoint, TRPCRouter, TRPCError, TRPCErrorCode } from './internal';
 
-export { TRPCApi, TRPCRouter, TRPCEndpoint };
+export { TRPCApi, TRPCRouter, TRPCEndpoint, TRPCError, TRPCErrorCode };
 export const api = TRPCApi.create;
 export const router = TRPCRouter.create;
 export const endpoint = TRPCEndpoint.create;
