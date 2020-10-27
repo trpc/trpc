b5a16b18e0d2490c20aaffe22cc3d4ffe6d96190
Colin McDonnell <colinmcd@alum.mit.edu>
2020-10-27 17:17:42 -0400

    Added TRPCPayload and TRPCRequest to module

--- src/external.ts
+++ src/external.ts
@@ -1,7 +1,22 @@
-import { TRPCEndpoint, TRPCRouter, TRPCError, TRPCErrorCode } from './internal';
+import {
+  TRPCEndpoint,
+  TRPCRouter,
+  TRPCError,
+  TRPCErrorCode,
+  TRPCRequest,
+  TRPCPayload,
+} from './internal';
 import { makeSDK } from './sdk';
 
-export { TRPCRouter, TRPCEndpoint, TRPCError, TRPCErrorCode };
+export {
+  TRPCRouter,
+  TRPCEndpoint,
+  TRPCError,
+  TRPCErrorCode,
+  TRPCRequest,
+  TRPCPayload,
+};
+
 export const router = TRPCRouter.create;
 export const endpoint = TRPCEndpoint.create;
 export { makeSDK as sdk };
