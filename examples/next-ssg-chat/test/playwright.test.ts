8be0b957ea50db724381cf32c917ca789b293448
KATT <alexander@n1s.se>
2021-02-12 00:06:20 +0100

    30s

--- examples/next-ssg-chat/test/playwright.test.ts
+++ examples/next-ssg-chat/test/playwright.test.ts
@@ -1,11 +1,15 @@
+jest.setTimeout(30e3);
+
 test('send message', async () => {
   await page.goto('http://localhost:3000');
 
   const viewer = await browser.newPage();
   await viewer.goto('http://localhost:3000');
 
-
-  const nonce = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+  const nonce = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 6);
   await page.type('[name=text]', nonce);
   await page.click('[type=submit]');
 
