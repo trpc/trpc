ddd193ce22dc08d4c6376349edc2b601bf506cee
KATT <alexander@n1s.se>
2021-02-22 22:44:38 +0100

    don’t export procedure internals

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,5 @@ export * from './router';
 export * from './types';
 export * from './subscription';
 export * from './transformer';
-export * from './procedure';
 
 export * from './adapters/standalone';
