f3cc2ab2451ce6b4a70f1d086efd119706212f45
KATT <alexander@n1s.se>
2021-03-04 18:00:51 +0100

    woop

--- examples/next-prisma-todomvc/test/playwright.test.ts
+++ examples/next-prisma-todomvc/test/playwright.test.ts
@@ -1,21 +1,17 @@
 jest.setTimeout(35e3);
 
-test('send message', async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1e3));
+test('add todo', async () => {
   await page.goto('http://localhost:3000');
-
-  const viewer = await browser.newPage();
-  await viewer.goto('http://localhost:3000');
 
   const nonce = Math.random()
     .toString(36)
     .replace(/[^a-z]+/g, '')
     .substr(0, 6);
-  await page.type('[name=text]', nonce);
-  await page.click('[type=submit]');
+  await page.type('.new-todo', nonce);
+  await page.keyboard.press('Enter');
+  await page.reload();
 
-  await viewer.waitForSelector(`text=${nonce}`);
-  viewer.close();
+  await page.waitForSelector(`text=${nonce}`);
 });
 
 export {};
