cf4154674998029ea64d9063006bea8f0923ee53
KATT <alexander@n1s.se>
2021-03-05 12:37:07 +0100

    wip

--- packages/server/src/http.ts
+++ packages/server/src/http.ts
@@ -270,6 +270,6 @@ export async function requestHandler<
   try {
     teardown && (await teardown());
   } catch (err) {
-    console.error('Teardown failed', err);
+    throw new Error('Teardown failed ' + err?.message);
   }
 }
