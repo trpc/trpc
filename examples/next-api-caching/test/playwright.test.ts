379796c121a3f4a0c28bd8c4b92a5891d0a03717
KATT <alexander@n1s.se>
2021-03-07 17:24:56 +0100

    tests

--- examples/next-api-caching/test/playwright.test.ts
+++ examples/next-api-caching/test/playwright.test.ts
@@ -1,7 +1,6 @@
 test('loads data from trpc', async () => {
   await page.goto('http://localhost:3000');
 
-  await page.waitForSelector(`text=hello world`);
-  await page.waitForSelector(`text=hello client`);
+  // ... insert your tests here
 });
 export {};
