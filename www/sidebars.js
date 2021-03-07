afb8d89bd17e8c06dd515d419e102d7893cd478b
KATT <alexander@n1s.se>
2021-03-07 17:13:43 +0100

    context dox

--- www/sidebars.js
+++ www/sidebars.js
@@ -25,6 +25,7 @@ module.exports = {
             'server/merging-routers',
             'server/middlewares',
             'server/data-transformers',
+            'server/context',
           ]
         },
         {
