01304a6930c159322ce7b53203cda3191a080286
KATT <alexander@n1s.se>
2021-03-07 17:05:04 +0100

    refetchInterval

--- examples/next-api-caching/pages/index.tsx
+++ examples/next-api-caching/pages/index.tsx
@@ -2,21 +2,24 @@ import Head from 'next/head';
 import { trpc } from '../utils/trpc';
 
 export default function Home() {
-  const uncachedQuery = trpc.useQuery(['slow-query-uncached']);
-  const cachedQuery = trpc.useQuery(['slow-query-uncached']);
+  const uncachedQuery = trpc.useQuery(['slow-query-uncached'], {
+    refetchInterval: 5e3, // refetch data every 5s
+  });
+  const cachedQuery = trpc.useQuery(['slow-query-cached'], {
+    refetchInterval: 5e3, // refetch data every 5s
+  });
   return (
     <>
       <Head>
         <title>Cached query demo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <h2>cachedQuery</h2>
       <p>status: {cachedQuery.status}</p>
       <p>
         result:{' '}
         {cachedQuery.data && (
-          <pre>{JSON.stringify(cachedQuery.data, null, 4)}</pre>
+          <code>{JSON.stringify(cachedQuery.data, null, 4)}</code>
         )}
       </p>
 
@@ -25,9 +28,21 @@ export default function Home() {
       <p>
         result:{' '}
         {uncachedQuery.data && (
-          <pre>{JSON.stringify(uncachedQuery.data, null, 4)}</pre>
+          <code>{JSON.stringify(uncachedQuery.data, null, 4)}</code>
         )}
       </p>
+      <div style={{ marginTop: '100px' }}>
+        <a
+          href="https://vercel.com/?utm_source=trpc&utm_campaign=oss"
+          target="_blank"
+        >
+          <img
+            src="/powered-by-vercel.svg"
+            alt="Powered by Vercel"
+            height={25}
+          />
+        </a>
+      </div>
     </>
   );
 }
