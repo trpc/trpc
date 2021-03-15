f8b1283368356b67d2218ad63fa3c26a64a6d3ba
KATT <alexander@n1s.se>
2021-03-15 15:44:18 +0100

    it builds

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = {
   presets: [
     "@babel/preset-typescript",
     "@babel/preset-react",
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        loose: true,
+        exclude: [
+          "@babel/plugin-transform-async-to-generator",
+          "@babel/plugin-transform-regenerator",
+        ],
+      },
+    ],
   ],
+  plugins: [
+    "babel-plugin-annotate-pure-calls",
+    "babel-plugin-dev-expression",
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ]
 };
--- packages/server/babel.config.js
+++ packages/server/babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
     [
       '@babel/preset-env', {
         targets: { node: 'current' }
-      }],
+      }
+    ],
     '@babel/preset-typescript',
   ],
 };
