2ea61dad16be14c60419367da1d0c9fcfed047b7
KATT <alexander@n1s.se>
2021-03-16 19:39:18 +0100

    runtime

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = {
     ],
   ],
   plugins: [
+    "@babel/transform-runtime",
     "babel-plugin-annotate-pure-calls",
     "babel-plugin-dev-expression",
     ["@babel/plugin-proposal-class-properties", { loose: true }],
