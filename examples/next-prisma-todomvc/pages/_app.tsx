34e0cdf7a6c5c396cb92924a2fd2f48b85d1417b
KATT <alexander@n1s.se>
2021-04-03 18:47:38 +0200

    update examples

--- examples/next-prisma-todomvc/pages/_app.tsx
+++ examples/next-prisma-todomvc/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { useInstance } from '@trpc/react';
 import type { AppProps /*, AppContext */ } from 'next/app';
-import { QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import { trpc } from '../utils/trpc';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = useInstance(() => new QueryClient());
   return (
-    <QueryClientProvider client={trpc.queryClient}>
+    <QueryClientProvider client={queryClient}>
       <Hydrate state={trpc.useDehydratedState(pageProps.dehydratedState)}>
         <Component {...pageProps} />
       </Hydrate>
