9279c0875f766af4edea39c97d3d702843485ee5
KATT <alexander@n1s.se>
2021-04-24 18:23:56 +0200

    fix

--- examples/next-hello-world/utils/trpc.ts
+++ examples/next-hello-world/utils/trpc.ts
@@ -1,8 +1,4 @@
-import {
-  createReactQueryHooks,
-  createTRPCClient,
-  trpcReact,
-} from '@trpc/react';
+import { trpcReact } from '@trpc/react';
 // Type-only import:
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#type-only-imports-and-export
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
