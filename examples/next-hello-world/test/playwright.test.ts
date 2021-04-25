01971c7cde998e266998c8bbbfa0681edb4c775c
KATT <alexander@n1s.se>
2021-04-25 21:10:46 +0200

    ssr test

--- examples/next-hello-world/test/playwright.test.ts
+++ examples/next-hello-world/test/playwright.test.ts
@@ -1,7 +1,15 @@
 test('loads data from trpc', async () => {
   await page.goto('http://localhost:3000');
 
-  await page.waitForSelector(`text=hello world`);
-  await page.waitForSelector(`text=hello client`);
+  await page.waitForSelector(`text=An example entry`);
+});
+
+test('ssr', async () => {
+  await browser.newPage({
+    javaScriptEnabled: false,
+  });
+  await page.goto('http://localhost:3000');
+
+  await page.waitForSelector(`text=An example entry`);
 });
 export {};
