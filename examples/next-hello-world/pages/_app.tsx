98a5c037b011d5185e280e66df22de043431a3b6
KATT <alexander@n1s.se>
2021-04-25 02:02:05 +0200

    withTRPCClient hoc

--- examples/next-hello-world/pages/_app.tsx
+++ examples/next-hello-world/pages/_app.tsx
@@ -2,86 +2,37 @@ import { TRPCClient } from '@trpc/client';
 import { getDataFromTree } from '@trpc/react';
 import type { Dict } from '@trpc/server';
 import type { AppProps /*, AppContext */ } from 'next/app';
-import { AppContextType } from 'next/dist/next-server/lib/utils';
+import { AppContextType, AppType } from 'next/dist/next-server/lib/utils';
 import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { dehydrate, Hydrate } from 'react-query/hydration';
 import { trpc } from '../utils/trpc';
+import { withTRPCClient } from '../utils/withTRPCClient';
+import type { AppRouter } from './api/trpc/[trpc]';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(
-    () => pageProps.queryClient ?? new QueryClient(),
-  );
-  const [trpcClient] = useState(
-    () =>
-      pageProps.trpcClient ??
-      new TRPCClient({
-        url: '/api/trpc',
-      }),
-  );
-  const hydratedState = trpc.useDehydratedState(
-    trpcClient,
-    pageProps.dehydratedState,
-  );
-  return (
-    <trpc.Provider
-      client={trpcClient}
-      queryClient={queryClient}
-      ssr={!process.browser}
-    >
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={hydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
-    </trpc.Provider>
-  );
-}
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-const getInitialProps = async ({ ctx, AppTree, Component }: AppContextType) => {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
+MyApp.getInitialProps = async ({ ctx }) => {
+  // make it sloow
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  const trpcClient = new TRPCClient({
-    url: `${baseUrl}/api/trpc`,
-    getHeaders() {
-      return ctx.req?.headers ?? {};
-    },
-  });
-  const queryClient = new QueryClient();
-
-  let pageProps: Dict<unknown> = {};
-  if (!process.browser) {
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    const serverPageProps = {
-      ...pageProps,
-      trpcClient,
-      queryClient,
-    };
-
-    await getDataFromTree(<AppTree pageProps={serverPageProps} />, queryClient);
-
-    // make it sloow
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    // ctx.res?.setHeader('Vary', 'Authorization');
-    ctx.res?.setHeader(
-      'Cache-Control',
-      's-maxage=1, stale-while-revalidate=600',
-    );
-  }
-
-  pageProps.dehydratedState = trpcClient.transformer.serialize(
-    dehydrate(queryClient),
-  );
-
+  // ctx.res?.setHeader('Vary', 'Authorization');
+  ctx.res?.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=600');
   return {
-    pageProps,
+    pageProps: {},
   };
 };
-(MyApp as any).getInitialProps = getInitialProps;
+const baseUrl = process.browser
+  ? ''
+  : process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
 
-export default MyApp;
+const url = `${baseUrl}/api/trpc`;
+
+export default withTRPCClient(MyApp, {
+  url,
+  ssr: true,
+});
---
+++ examples/next-hello-world/utils/withTRPCClient.tsx
@@ -0,0 +1,107 @@
+import {
+  CreateTRPCClientOptions,
+  createTRPCClient,
+  trpcReact,
+  getDataFromTree,
+} from '@trpc/react';
+import { AnyRouter, Dict } from '@trpc/server';
+import {
+  AppContextType,
+  AppPropsType,
+  NextComponentType,
+  NextPageContext,
+} from 'next/dist/next-server/lib/utils';
+import { createElement, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { dehydrate, Hydrate } from 'react-query/hydration';
+type QueryClientConfig = ConstructorParameters<typeof QueryClient>[0];
+
+export interface WithTRPCClientOptions<TRouter extends AnyRouter>
+  extends CreateTRPCClientOptions<TRouter> {
+  queryClientConfig?: QueryClientConfig;
+  ssr?: boolean;
+}
+
+export function withTRPCClient<TRouter extends AnyRouter>(
+  AppOrPage: NextComponentType<any, any, any>,
+  opts: WithTRPCClientOptions<TRouter>,
+) {
+  const { queryClientConfig, ssr = false, ...trpcOptions } = opts;
+  const trpc = trpcReact<TRouter>();
+
+  const WithTRPC = (props: AppPropsType) => {
+    const { pageProps } = props;
+    const [queryClient] = useState(
+      () => pageProps.queryClient ?? new QueryClient(queryClientConfig),
+    );
+    const [trpcClient] = useState(
+      () => pageProps.trpcClient ?? trpc.createClient(trpcOptions),
+    );
+
+    const hydratedState = trpc.useDehydratedState(
+      trpcClient,
+      pageProps.dehydratedState,
+    );
+
+    return (
+      <trpc.Provider client={trpcClient} queryClient={queryClient} ssr={ssr}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={hydratedState}>
+            <AppOrPage {...props} />
+          </Hydrate>
+        </QueryClientProvider>
+      </trpc.Provider>
+    );
+  };
+
+  if (AppOrPage.getInitialProps || ssr) {
+    WithTRPC.getInitialProps = async (appOrPageCtx: AppContextType) => {
+      const AppTree = appOrPageCtx.AppTree;
+      const trpcClient = createTRPCClient(trpcOptions);
+
+      // Determine if we are wrapping an App component or a Page component.
+      const isApp = !!appOrPageCtx.Component;
+      const ctx = isApp ? appOrPageCtx : appOrPageCtx.ctx;
+
+      // Run the wrapped component's getInitialProps function.
+      let pageProps: Dict<unknown> = {};
+      if (AppOrPage.getInitialProps) {
+        pageProps = await AppOrPage.getInitialProps(ctx as any);
+      }
+
+      const queryClient = new QueryClient(queryClientConfig);
+
+      if (!process.browser && ssr) {
+        const serverPageProps = {
+          ...pageProps,
+          trpcClient,
+          queryClient,
+        };
+
+        await getDataFromTree(
+          <AppTree pageProps={serverPageProps} />,
+          queryClient,
+        );
+
+        // // ctx.res?.setHeader('Vary', 'Authorization');
+        // ctx.res?.setHeader(
+        //   'Cache-Control',
+        //   's-maxage=1, stale-while-revalidate=600',
+        // );
+      }
+
+      pageProps.dehydratedState = trpcClient.transformer.serialize(
+        dehydrate(queryClient),
+      );
+
+      return {
+        pageProps,
+      };
+    };
+  }
+
+  const displayName = AppOrPage.displayName || AppOrPage.name || 'Component';
+  WithTRPC.displayName = `withTRPCClient(${displayName})`;
+
+  return WithTRPC;
+}
