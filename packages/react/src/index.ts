04b29b8529163a2c9c784f95b5954d908009bbe2
KATT <alexander@n1s.se>
2021-04-26 21:08:31 +0200

    fix

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,3 @@
 export * from '@trpc/client';
 
-export type { OutputWithCursor } from './createReactQueryHooks';
-
-export { createReactQueryHooks } from './createReactQueryHooks';
+export * from './createReactQueryHooks';
