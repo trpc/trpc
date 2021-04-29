36b424caaff36ecbd75205b8a04f9d99eb8b3589
KATT <alexander@n1s.se>
2021-04-29 19:37:25 +0200

    fix

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -69,7 +69,7 @@ module.exports = {
             // },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/trpc/trpc',
             },
           ],
         },
