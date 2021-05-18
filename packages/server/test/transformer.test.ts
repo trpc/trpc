b478e6f77ed072bb52f248802258c8bf020c3a42
KATT <alexander@n1s.se>
2021-05-18 22:30:26 +0200

    make basic superjson test

--- packages/server/test/transformer.test.ts
+++ packages/server/test/transformer.test.ts
@@ -1,15 +1,17 @@
 import * as trpc from '@trpc/server';
 import superjson from 'superjson';
+import { z } from 'zod';
 import { routerToServerAndClient } from './_testHelpers';
 
-test('superjson downstream', async () => {
+test('superjson up and down', async () => {
   const date = new Date();
+  const fn = jest.fn();
   const { client, close } = routerToServerAndClient(
     trpc.router().query('hello', {
-      resolve() {
-        return {
-          date,
-        };
+      input: z.date(),
+      resolve({ input }) {
+        fn(input);
+        return input;
       },
     }),
     {
@@ -17,8 +19,9 @@ test('superjson downstream', async () => {
       server: { transformer: superjson },
     },
   );
-  const res = await client.query('hello');
-  expect(res.date.getTime()).toBe(date.getTime());
+  const res = await client.query('hello', date);
+  expect(res.getTime()).toBe(date.getTime());
+  expect((fn.mock.calls[0][0] as Date).getTime()).toBe(date.getTime());
 
   close();
 });
