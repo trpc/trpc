532ff89c1b93277d8589f9b8f351768ef0bc36d6
KATT <alexander@n1s.se>
2021-05-25 14:38:51 +0200

    oopsy

--- examples/next-prisma-todomvc/next.config.js
+++ examples/next-prisma-todomvc/next.config.js
@@ -2,19 +2,9 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/docs/procedures',
-        destination: '/docs/router',
-        permanent: true,
-      },
-      {
-        source: '/docs/create-react-app',
-        destination: '/docs/react',
-        permanent: true,
-      },
-      {
-        source: '/docs/react-queries-and-mutations',
-        destination: '/docs/react-queries',
-        permanent: true,
+        source: '/',
+        destination: '/all',
+        permanent: false,
       },
     ];
   },
--- www/next.config.js
+++ www/next.config.js
@@ -2,9 +2,19 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/all',
-        permanent: false,
+        source: '/docs/procedures',
+        destination: '/docs/router',
+        permanent: true,
+      },
+      {
+        source: '/docs/create-react-app',
+        destination: '/docs/react',
+        permanent: true,
+      },
+      {
+        source: '/docs/react-queries-and-mutations',
+        destination: '/docs/react-queries',
+        permanent: true,
       },
     ];
   },
