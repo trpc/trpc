7b1fd653d29c800d41d914520488ce109ad2d692
KATT <alexander@n1s.se>
2021-05-26 12:30:26 +0200

    posts add

--- examples/next-prisma-starter/src/pages/index.tsx
+++ examples/next-prisma-starter/src/pages/index.tsx
@@ -6,7 +6,8 @@ import { trpc } from '../utils/trpc';
 
 export default function IndexPage() {
   const postsQuery = trpc.useQuery(['posts.all']);
-  const editPost = trpc.useMutation('posts.edit');
+  const addPost = trpc.useMutation('posts.add');
+  const utils = trpc.useContext();
 
   return (
     <>
@@ -26,6 +27,50 @@ export default function IndexPage() {
           <p>{item.text.substr(0)}</p>
         </article>
       ))}
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          /**
+           * In a real app you probably don't want to use this manually
+           * Checkout React Hook Form - it works great with tRPC
+           * @link https://react-hook-form.com/
+           */
+
+          const $text: HTMLInputElement = (e as any).target.elements.text;
+          const $title: HTMLInputElement = (e as any).target.elements.title;
+          const input = {
+            title: $title.value,
+            text: $text.value,
+          };
+          try {
+            await addPost.mutateAsync(input);
+            utils.invalidateQuery(['posts.all']);
+
+            $title.value = '';
+            $text.value = '';
+          } catch {}
+        }}
+      >
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          id="title"
+          name="title"
+          type="text"
+          disabled={addPost.isLoading}
+        />
+
+        <br />
+        <label htmlFor="text">Text:</label>
+        <br />
+        <textarea id="text" name="text" disabled={addPost.isLoading} />
+        <br />
+        <input type="submit" disabled={addPost.isLoading} />
+        {addPost.error && (
+          <p style={{ color: 'red' }}>{addPost.error.message}</p>
+        )}
+      </form>
 
       {process.env.NODE_ENV !== 'production' && (
         <ReactQueryDevtools initialIsOpen={false} />
