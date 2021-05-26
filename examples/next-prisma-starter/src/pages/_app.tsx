d053f5880c62ab99b7fb36d616d218c46419201b
KATT <alexander@n1s.se>
2021-05-26 15:15:08 +0200

    example update - base url

--- examples/next-prisma-starter/src/pages/_app.tsx
+++ examples/next-prisma-starter/src/pages/_app.tsx
@@ -6,6 +6,24 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+function getBaseUrl() {
+  if (process.browser) {
+    return '';
+  }
+  // // reference for vercel.com
+  // if (process.env.VERCEL_URL) {
+  //   return process.env.VERCEL_URL;
+  // }
+
+  // // reference for render.com
+  // if (process.env.RENDER_INTERNAL_HOSTNAME) {
+  //   return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  // }
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export default withTRPC(
   () => {
     /**
@@ -13,17 +31,21 @@ export default withTRPC(
      * @link https://trpc.io/docs/ssr
      */
     return {
-      url: '/api/trpc',
+      url: `${getBaseUrl()}/api/trpc`,
       /**
        * @link https://trpc.io/docs/data-transformers
        */
       // transformer,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
   },
   {
     /**
      * @link https://trpc.io/docs/ssr
      */
-    // ssr: true,
+    ssr: true,
   },
 )(MyApp);
