1510f7ac510cc8d1d89ac32d52efb8b313edd3c4
KATT <alexander@n1s.se>
2021-06-07 16:18:41 +0200

    comments

--- packages/client/src/internals/dataLoader.ts
+++ packages/client/src/internals/dataLoader.ts
@@ -17,6 +17,11 @@ type BatchLoadFn<TKey, TValue> = (keys: TKey[]) => {
   cancel: CancelFn;
 };
 
+/**
+ * Dataloader that's very inspired by https://github.com/graphql/dataloader
+ * Less configuration, no caching, and allows you to cancel requests
+ * When cancelling a single fetch the whole batch will be cancelled only when _all_ items are cancelled
+ */
 export function dataLoader<TKey, TValue>(fetchMany: BatchLoadFn<TKey, TValue>) {
   let batch: Batch<TKey, TValue> | null = null;
   let dispatchTimer: NodeJS.Timer | number | null = null;
