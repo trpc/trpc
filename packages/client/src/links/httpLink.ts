8dcf789706506278f91af7459c761df796d05672
KATT <alexander@n1s.se>
2021-06-07 03:34:25 +0200

    rm

--- packages/client/src/links/httpLink.ts
+++ packages/client/src/links/httpLink.ts
@@ -1,43 +1,4 @@
-import { DataTransformer } from '@trpc/server';
-import {
-  HttpLinkOptions,
-  httpRequest,
-  PromiseAndCancel,
-  TRPCLink,
-} from './core';
-
-export function fetchAndReturn<TResponseShape = unknown>(config: {
-  fetch: typeof fetch;
-  AbortController?: typeof AbortController;
-  url: string;
-  opts: RequestInit;
-  transformer: DataTransformer;
-}): PromiseAndCancel<any> {
-  const ac = config.AbortController ? new config.AbortController() : null;
-  const reqOpts = {
-    ...config.opts,
-    headers: {
-      'content-type': 'application/json',
-      ...(config.opts.headers ?? {}),
-    },
-    signal: ac?.signal,
-  };
-  const promise = new Promise<TResponseShape>((resolve, reject) => {
-    config
-      .fetch(config.url, reqOpts)
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        resolve(config.transformer.deserialize(json));
-      })
-      .catch(reject);
-  });
-  const cancel = () => {
-    ac?.abort();
-  };
-  return { promise, cancel };
-}
+import { HttpLinkOptions, httpRequest, TRPCLink } from './core';
 
 export function httpLink(opts: HttpLinkOptions): TRPCLink {
   const { url } = opts;
