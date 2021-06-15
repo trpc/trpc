7b2f7640fb8b56a31999931d8d7fb7c48f8ad3df
KATT <alexander@n1s.se>
2021-06-15 23:42:20 +0200

    link

--- examples/express-server/src/client.ts
+++ examples/express-server/src/client.ts
@@ -1,9 +1,9 @@
 import { createTRPCClient } from '@trpc/client';
+import { httpLink } from '@trpc/client/links/httpLink';
+import { loggerLink } from '@trpc/client/links/loggerLink';
 import AbortController from 'abort-controller';
 import fetch from 'node-fetch';
 import type { AppRouter } from './server';
-import { loggerLink } from '@trpc/client/links/loggerLink';
-import { httpLink } from '@trpc/client/links/httpLink';
 
 // polyfill
 global.AbortController = AbortController;
@@ -15,7 +15,18 @@ async function main() {
   const url = `http://localhost:2021/trpc`;
 
   const client = createTRPCClient<AppRouter>({
-    links: [loggerLink(), httpLink({ url })],
+    links: [
+      () =>
+        ({ op, prev, next }) => {
+          console.log('->', op.type, op.path, op.input);
+
+          return next(op, (result) => {
+            console.log('<-', op.type, op.path, op.input, ':', result);
+            prev(result);
+          });
+        },
+      httpLink({ url }),
+    ],
   });
   await sleep();
   await client.query('hello');
