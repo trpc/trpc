0d826c4cd9918e96513b95cab2e57dbe0f29e339
KATT <alexander@n1s.se>
2021-06-15 14:22:14 +0200

    fastify example

--- packages/server/test/adapters/fastify.test.tsx
+++ packages/server/test/adapters/fastify.test.tsx
@@ -1,16 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import f from 'fastify';
 import * as trpc from '../../../server/src';
+import {
+  CreateHttpContextOptions,
+  createHttpHandler,
+  requestHandler,
+} from '../../../server/src';
+import { createTRPCClient } from '@trpc/client';
+import fetch from 'node-fetch';
+import AbortController from 'abort-controller';
+const fetchOpts = { fetch, AbortController } as any;
+
 async function startServer() {
   const fastify = f();
 
-  // Declare a route
-  fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' });
+  type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+  const appRouter = trpc.router<Context>().query('hello', {
+    resolve() {
+      return {
+        text: `hello world`,
+      };
+    },
   });
+  type AppRouter = typeof appRouter;
+
+  function createContext(_opts: CreateHttpContextOptions) {
+    // use opts.req / opts.res here
+    return {
+      // req,
+      // res,
+    };
+  }
+  fastify.all('/trpc/:path', (req, reply) => {
+    requestHandler({
+      req: req.raw,
+      res: reply.raw,
+      router: appRouter,
+      createContext,
+      path: (req.params as any).path,
+    });
+  });
+
   const url = await fastify.listen(0);
 
+  const client = createTRPCClient<AppRouter>({
+    url: `${url}/trpc`,
+    fetchOpts,
+  });
   return {
     url,
+    client,
     close: () => {
       return fastify.close();
     },
@@ -22,9 +62,14 @@ beforeAll(async () => {
   t = await startServer();
 });
 afterAll(async () => {
-  await t.close();
+  // await t.close();
 });
 
-test('start server', async () => {
-  expect(t.url).toBeTruthy();
+test('fastify hello query', async () => {
+  const res = await t.client.query('hello');
+  expect(res).toMatchInlineSnapshot(`
+    Object {
+      "text": "hello world",
+    }
+  `);
 });
