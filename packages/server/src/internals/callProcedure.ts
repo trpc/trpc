ec4ba877ab628177d7c1bfe55e7d5825ef06a724
KATT <alexander@n1s.se>
2021-06-20 23:26:26 +0200

    mv callProcedure

---
+++ packages/server/src/internals/callProcedure.ts
@@ -0,0 +1,29 @@
+import { AnyRouter, ProcedureType } from '../router';
+import { Subscription } from '../subscription';
+
+export async function callProcedure<
+  TRouter extends AnyRouter<TContext>,
+  TContext,
+>(opts: {
+  path: string;
+  input: unknown;
+  router: TRouter;
+  ctx: TContext;
+  type: ProcedureType;
+}): Promise<unknown | Subscription<TRouter>> {
+  const { type, path, input } = opts;
+
+  const caller = opts.router.createCaller(opts.ctx);
+  if (type === 'query') {
+    return caller.query(path, input);
+  }
+  if (type === 'mutation') {
+    return caller.mutation(path, input);
+  }
+  if (type === 'subscription') {
+    const sub = (await caller.subscription(path, input)) as Subscription;
+    return sub;
+  }
+  /* istanbul ignore next */
+  throw new Error(`Unknown procedure type ${type}`);
+}
