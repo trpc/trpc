8619b62fb49b1f6ca127e6fca1e40fc6242c724e
KATT <alexander@n1s.se>
2021-07-13 15:01:53 +0200

    fixes

--- examples/next-prisma-starter-websockets/next.config.js
+++ examples/next-prisma-starter-websockets/next.config.js
@@ -2,15 +2,13 @@
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
 
-const wsPort = process.env.NODE_ENV === 'production' ? 3000 : 3001;
-
 module.exports = {
   serverRuntimeConfig: {
     // Will only be available on the server side
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    APP_URL: process.env.APP_URL || `http://localhost:3000`,
-    WS_URL: process.env.WS_URL || `ws://localhost:${wsPort}`,
+    APP_URL: process.env.APP_URL,
+    WS_URL: process.env.WS_URL,
   },
 };
