b69f19c9c0fe64a1cffe01a022d3723866a54e3c
KATT <alexander@n1s.se>
2021-07-17 17:33:04 +0200

    wip

--- examples/next-prisma-starter-websockets/src/server/env.ts
+++ examples/next-prisma-starter-websockets/src/server/env.ts
@@ -1,11 +1,7 @@
 import { envsafe, str } from 'envsafe';
 export const env = envsafe({
-  GITHUB_CLIENT_ID: str({
-    devDefault: '832513c5ece0dfa99e82',
-  }),
-  GITHUB_SECRET: str({
-    devDefault: 'a129ccea8efa0e5ad6dd7bde35b74666a1346cb5',
-  }),
+  GITHUB_CLIENT_ID: str(),
+  GITHUB_SECRET: str(),
   APP_ENV: str({
     default: process.env.NODE_ENV,
     desc: 'Only used to signal if we are in test mode',
