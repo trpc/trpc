4ae42fd047198c95c98da8ab9ff068c8ff6ba510
KATT <alexander@n1s.se>
2021-07-17 20:20:00 +0200

    appUrl

--- examples/next-prisma-starter-websockets/next.config.js
+++ examples/next-prisma-starter-websockets/next.config.js
@@ -2,13 +2,18 @@
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
 
+let appUrl = process.env.APP_URL;
+if (!appUrl && process.env.HEROKU_APP_NAME && process.env.HEROKU_PR_NUMBER) {
+  appUrl = `https://${process.env.HEROKU_APP_NAME}-pr-${process.env.HEROKU_PR_NUMBER}.herokuapp.com`;
+}
+
 module.exports = {
   serverRuntimeConfig: {
     // Will only be available on the server side
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    APP_URL: process.env.APP_URL,
+    APP_URL: appUrl,
     WS_URL: process.env.WS_URL,
   },
 };
