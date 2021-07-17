d398ee268147baedd5b1030e8c6546e6ce215eed
KATT <alexander@n1s.se>
2021-07-17 13:10:58 +0200

    wip

---
+++ examples/next-prisma-starter-websockets/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
