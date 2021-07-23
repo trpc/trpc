7cd8d7eac879b86566d7d6e89e1549169000d387
KATT <alexander@n1s.se>
2021-07-23 13:36:46 +0200

    Revert "demo of #703"
    This reverts commit 43f328c5ff06417e23ca4e308189726f22505f0b.

--- examples/next-prisma-starter/src/server/routers/app.ts
+++ examples/next-prisma-starter/src/server/routers/app.ts
@@ -4,8 +4,6 @@
 import superjson from 'superjson';
 import { createRouter } from '../trpc';
 import { postsRouter } from './posts';
-import zodToJsonSchema from 'zod-to-json-schema';
-import { ZodType } from 'zod';
 
 /**
  * Create your application's root router
@@ -13,7 +11,7 @@ import { ZodType } from 'zod';
  * @link https://trpc.io/docs/ssg
  * @link https://trpc.io/docs/router
  */
-const rootRouter = createRouter()
+export const appRouter = createRouter()
   /**
    * Add data transformers
    * @link https://trpc.io/docs/data-transformers
@@ -25,29 +23,5 @@ const rootRouter = createRouter()
    */
   // .formatError(({ shape, error }) => { })
   .merge('posts.', postsRouter);
-
-export const appRouter = createRouter()
-  .merge(rootRouter)
-  .merge(
-    'schemas.',
-    createRouter()
-      //
-      .query('mutations', {
-        resolve() {
-          const mutations = Object.entries(rootRouter._def.mutations);
-          const schemas = mutations.flatMap(([path, procedure]) => {
-            if (procedure.inputParser instanceof ZodType) {
-              return {
-                path,
-                schema: zodToJsonSchema(procedure.inputParser),
-              };
-            }
-
-            return [];
-          });
-          return schemas;
-        },
-      }),
-  );
 
 export type AppRouter = typeof appRouter;
