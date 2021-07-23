43f328c5ff06417e23ca4e308189726f22505f0b
KATT <alexander@n1s.se>
2021-07-23 13:28:03 +0200

    demo of #703

--- examples/next-prisma-starter/src/server/routers/app.ts
+++ examples/next-prisma-starter/src/server/routers/app.ts
@@ -4,6 +4,8 @@
 import superjson from 'superjson';
 import { createRouter } from '../trpc';
 import { postsRouter } from './posts';
+import zodToJsonSchema from 'zod-to-json-schema';
+import { ZodType } from 'zod';
 
 /**
  * Create your application's root router
@@ -11,7 +13,7 @@ import { postsRouter } from './posts';
  * @link https://trpc.io/docs/ssg
  * @link https://trpc.io/docs/router
  */
-export const appRouter = createRouter()
+const rootRouter = createRouter()
   /**
    * Add data transformers
    * @link https://trpc.io/docs/data-transformers
@@ -23,5 +25,29 @@ export const appRouter = createRouter()
    */
   // .formatError(({ shape, error }) => { })
   .merge('posts.', postsRouter);
+
+export const appRouter = createRouter()
+  .merge(rootRouter)
+  .merge(
+    'schemas.',
+    createRouter()
+      //
+      .query('mutations', {
+        resolve() {
+          const mutations = Object.entries(rootRouter._def.mutations);
+          const schemas = mutations.flatMap(([path, procedure]) => {
+            if (procedure.inputParser instanceof ZodType) {
+              return {
+                path,
+                schema: zodToJsonSchema(procedure.inputParser),
+              };
+            }
+
+            return [];
+          });
+          return schemas;
+        },
+      }),
+  );
 
 export type AppRouter = typeof appRouter;
