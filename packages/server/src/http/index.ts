743cac26e1ebbf93de95ec37c4a47480aa9aa0b5
KATT <alexander@n1s.se>
2021-08-13 15:22:10 +0200

    tweak

--- packages/server/src/http/index.ts
+++ packages/server/src/http/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { assertNotBrowser } from '../assertNotBrowser';
-export * from './errors';
 export * from './requestHandler';
 assertNotBrowser();
