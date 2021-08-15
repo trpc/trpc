9c39f8fdabd63c605768d11465165d8b24463c12
KATT <alexander@n1s.se>
2021-08-15 21:41:16 +0200

    remove 404 example

--- examples/next-prisma-starter/src/pages/post/[id].tsx
+++ examples/next-prisma-starter/src/pages/post/[id].tsx
@@ -1,13 +1,9 @@
-import { ErrorPage } from 'components/ErrorPage';
 import { useRouter } from 'next/dist/client/router';
 import { trpc } from 'utils/trpc';
 
 export default function PostViewPage() {
   const id = useRouter().query.id as string;
   const postQuery = trpc.useQuery(['post.byId', id]);
-  if (postQuery.error) {
-    return <ErrorPage error={postQuery.error} />;
-  }
   return (
     <>
       <h1>{postQuery.data?.title}</h1>
