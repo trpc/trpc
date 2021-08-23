01de585c5470d52caf8345c2d1c46498cce66c9f
KATT <alexander@n1s.se>
2021-08-23 03:00:42 +0200

    add `responseHeaders` to `@trpc/next`

--- packages/next/src/withTRPC.tsx
+++ packages/next/src/withTRPC.tsx
@@ -8,6 +8,7 @@ import {
   createTRPCClient,
   CreateTRPCClientOptions,
   TRPCClient,
+  TRPCClientError,
 } from '@trpc/react';
 import type { AnyRouter, Dict } from '@trpc/server';
 import {
@@ -28,11 +29,23 @@ export type WithTRPCConfig<TRouter extends AnyRouter> =
     queryClientConfig?: QueryClientConfig;
   };
 
-export function withTRPC<TRouter extends AnyRouter>(opts: {
-  config: (info: { ctx?: NextPageContext }) => WithTRPCConfig<TRouter>;
-  ssr?: boolean;
-}) {
-  const { config: getClientConfig, ssr = false } = opts;
+export function withTRPC<TRouter extends AnyRouter>(
+  opts: {
+    config: (info: { ctx?: NextPageContext }) => WithTRPCConfig<TRouter>;
+  } & (
+    | {
+        ssr?: false;
+      }
+    | {
+        ssr: true;
+        responseHeaders?: (opts: {
+          ctx: NextPageContext;
+          clientErrors: TRPCClientError<TRouter>[];
+        }) => Dict<string>;
+      }
+  ),
+) {
+  const { config: getClientConfig } = opts;
   type TRPCPrepassProps = {
     config: WithTRPCConfig<TRouter>;
     queryClient: QueryClient;
@@ -86,7 +99,7 @@ export function withTRPC<TRouter extends AnyRouter>(opts: {
       );
     };
 
-    if (AppOrPage.getInitialProps || ssr) {
+    if (AppOrPage.getInitialProps || opts.ssr) {
       WithTRPC.getInitialProps = async (appOrPageCtx: AppContextType) => {
         const AppTree = appOrPageCtx.AppTree;
 
@@ -95,7 +108,7 @@ export function withTRPC<TRouter extends AnyRouter>(opts: {
         const ctx: NextPageContext = isApp
           ? appOrPageCtx.ctx
           : (appOrPageCtx as any as NextPageContext);
-        const isServer = typeof window === 'undefined' && ssr;
+        const isServer = typeof window === 'undefined' && opts.ssr;
 
         // Run the wrapped component's getInitialProps function.
         let pageProps: Dict<unknown> = {};
@@ -115,7 +128,7 @@ export function withTRPC<TRouter extends AnyRouter>(opts: {
         const getAppTreeProps = (props: Record<string, unknown>) =>
           isApp ? { pageProps: props } : props;
 
-        if (typeof window !== 'undefined' || !ssr) {
+        if (typeof window !== 'undefined' || !opts.ssr) {
           return getAppTreeProps(pageProps);
         }
         const config = getClientConfig(isServer ? { ctx } : {});
@@ -168,6 +181,25 @@ export function withTRPC<TRouter extends AnyRouter>(opts: {
 
         const appTreeProps = getAppTreeProps(pageProps);
 
+        const headers =
+          opts.responseHeaders?.({
+            ctx,
+            clientErrors: [...dehydrated.queries, ...dehydrated.queries]
+              .map((v) => v.state.error)
+              .flatMap((err) =>
+                err instanceof Error && err.name === 'TRPCClientError'
+                  ? [err as TRPCClientError<TRouter>]
+                  : [],
+              ),
+          }) ?? {};
+
+        for (const [key, value] of Object.entries(headers)) {
+          if (!value) {
+            continue;
+          }
+
+          ctx.res?.setHeader(key, value);
+        }
         return appTreeProps;
       };
     }
