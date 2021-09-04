29fb1723f8a9effc42938fae5559d0955b5929dd
KATT <alexander@n1s.se>
2021-09-05 01:33:21 +0200

    cleanup example

--- examples/lambda-api-gateway/server.ts
+++ examples/lambda-api-gateway/server.ts
@@ -1,11 +1,10 @@
 import * as trpc from '@trpc/server';
-import { z } from 'zod';
-import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
 import {
   createApiGatewayHandler,
   CreateLambdaContextOptions,
 } from '@trpc/server/adapters/aws-lambda';
 import { inferAsyncReturnType } from '@trpc/server/src';
+import { z } from 'zod';
 
 function createContext(opts: CreateLambdaContextOptions) {
   return {
@@ -22,12 +21,7 @@ const appRouter = trpc.router<Context>().query('/greet', {
 });
 export type AppRouter = typeof appRouter;
 
-// TODO: This is not any!
 export const handler = createApiGatewayHandler({
   router: appRouter,
   createContext,
 });
-
-export const lambdaHandler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent,
-) => await handler(event);
