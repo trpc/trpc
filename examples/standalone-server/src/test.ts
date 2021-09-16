7caef3bd135eee7513b83c65c605d233e60a4e72
KATT <alexander@n1s.se>
2021-09-16 16:10:31 +0200

    Revert "this should break in ci"
    This reverts commit 018fad92c15bfd8955bd533994afb674eb327733.

--- examples/standalone-server/src/test.ts
+++ examples/standalone-server/src/test.ts
@@ -2,25 +2,56 @@ import { expectTypeOf } from 'expect-type';
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
 
-const appRouter = trpc.router().merge(
-  'admin/',
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Context = {};
+const appRouter = trpc.router<Context>().merge(
+  'admin.',
   trpc
-    .router()
+    .router<Context>()
     .mutation('testMutation', {
       input: z.object({
-        foo: z.string(),
+        in: z.string(),
       }),
-      resolve: async ({ input }) => `${input.foo}!`,
+      resolve: async ({ input }) => `out` as const,
     })
-    .middleware(async ({ next }) => next()),
+    .query('hello', {
+      input: z.object({
+        in: z.string(),
+      }),
+      resolve() {
+        return 'out' as const;
+      },
+    })
+    .middleware(async ({ next, ctx }) =>
+      next({
+        ctx: {
+          ...ctx,
+          _test: '1',
+        },
+      }),
+    )
+    .query('hello-2', {
+      input: z.object({
+        in: z.string(),
+      }),
+      resolve() {
+        return 'out' as const;
+      },
+    }),
 );
 
 type Mutations = typeof appRouter._def.mutations;
+type Queries = typeof appRouter._def.queries;
+
+type TInput = trpc.inferProcedureInput<Queries['admin.hello']>;
+expectTypeOf<trpc.inferProcedureInput<Queries['admin.hello']>>().toEqualTypeOf<{
+  in: string;
+}>();
 
 expectTypeOf<
-  trpc.inferProcedureInput<Mutations['admin/testMutation']>
->().toEqualTypeOf<{ foo: string }>();
+  trpc.inferProcedureInput<Mutations['admin.testMutation']>
+>().toEqualTypeOf<{ in: string }>();
 
 expectTypeOf<
-  trpc.inferProcedureInput<Mutations['admin/testMutation']>
+  trpc.inferProcedureInput<Mutations['admin.testMutation']>
 >().not.toBeUnknown();
