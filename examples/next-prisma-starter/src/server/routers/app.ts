2f93d95c8c0601e8f965e2fc38342400c490c11f
KATT <alexander@n1s.se>
2021-09-16 13:55:00 +0200

    split up `createRouter` and `createContext` in example

--- examples/next-prisma-starter/src/server/routers/app.ts
+++ examples/next-prisma-starter/src/server/routers/app.ts
@@ -2,7 +2,7 @@
  * This file contains the root router of your tRPC-backend
  */
 import superjson from 'superjson';
-import { createRouter } from '../trpc';
+import { createRouter } from '../createRouter';
 import { postRouter } from './post';
 
 /**
