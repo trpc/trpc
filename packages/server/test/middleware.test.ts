5b584c53e5bb8f88edfee859487e4065a0d43412
KATT <alexander@n1s.se>
2021-09-16 15:51:04 +0200

    add regression test

--- packages/server/test/middleware.test.ts
+++ packages/server/test/middleware.test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { AsyncLocalStorage } from 'async_hooks';
 import { expectTypeOf } from 'expect-type';
+import { z } from 'zod';
 import * as trpc from '../src';
 import { TRPCError, inferProcedureOutput } from '../src';
 import { MiddlewareResult } from '../src/internals/middlewares';
@@ -536,4 +537,67 @@ test('mutate context and combine with other routes', async () => {
   >;
 
   expectTypeOf<MeResponse>().toMatchTypeOf<User>();
+});
+
+test('inferProcedureFromInput regression', async () => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  type Context = {};
+  const appRouter = trpc.router<Context>().merge(
+    'admin.',
+    trpc
+      .router<Context>()
+      .mutation('testMutation', {
+        input: z.object({
+          in: z.string(),
+        }),
+        resolve: async () => `out` as const,
+      })
+      .query('hello', {
+        input: z.object({
+          in: z.string(),
+        }),
+        resolve() {
+          return 'out' as const;
+        },
+      })
+      .middleware(async ({ next, ctx }) =>
+        next({
+          ctx: {
+            ...ctx,
+            _test: '1',
+          },
+        }),
+      )
+      .query('hello-2', {
+        input: z.object({
+          in: z.string(),
+        }),
+        resolve() {
+          return 'out' as const;
+        },
+      }),
+  );
+
+  type Mutations = typeof appRouter._def.mutations;
+  type Queries = typeof appRouter._def.queries;
+
+  expectTypeOf<
+    trpc.inferProcedureInput<Queries['admin.hello']>
+  >().toEqualTypeOf<{
+    in: string;
+  }>();
+
+  expectTypeOf<
+    trpc.inferProcedureInput<Queries['admin.hello-2']>
+  >().toEqualTypeOf<{
+    in: string;
+  }>();
+
+  expectTypeOf<
+    trpc.inferProcedureInput<Mutations['admin.testMutation']>
+  >().toEqualTypeOf<{ in: string }>();
+
+  expectTypeOf<
+    trpc.inferProcedureInput<Mutations['admin.testMutation']>
+  >().not.toBeUnknown();
 });
