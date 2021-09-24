d892f28958370ccd093ce844c0e4e65ef0ecb96c
KATT <alexander@n1s.se>
2021-09-24 23:10:28 +0100

    loading state

--- examples/next-prisma-starter/src/pages/post/[id].tsx
+++ examples/next-prisma-starter/src/pages/post/[id].tsx
@@ -1,20 +1,12 @@
 import { useRouter } from 'next/dist/client/router';
 import { trpc } from 'utils/trpc';
-import NextError from 'next/error';
 
 export default function PostViewPage() {
   const id = useRouter().query.id as string;
   const postQuery = trpc.useQuery(['post.byId', id]);
-  if (postQuery.error) {
-    return (
-      <NextError
-        title={postQuery.error.message}
-        statusCode={postQuery.error.data?.httpStatus ?? 500}
-      />
-    );
-  }
-  if (postQuery.status !== 'success') {
-    return <>Loading...</>;
+
+  if (!postQuery.data) {
+    return <>Imagine error/loading state handling here</>;
   }
   return (
     <>
