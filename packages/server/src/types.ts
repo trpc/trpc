a0f806b9aa912a952c0bd07d29943a728be3847b
KATT <alexander@n1s.se>
2021-10-05 10:49:38 +0200

    deprecate instead of removing

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -8,6 +8,32 @@ export type Prefix<K extends string, T extends string> = `${K}${T}`;
 
 /**
  * @internal
+ * @deprecated will be removed in next major
+ */
+export type identity<T> = T;
+
+/**
+ * @internal
+ * @deprecated will be removed in next major
+ */
+export type format<T> = {
+  [k in keyof T]: T[k];
+};
+
+/**
+ * @internal
+ * @deprecated will be removed in next major
+ */
+export type flatten<T, Q> = identity<{
+  [k in keyof T | keyof Q]: k extends keyof T
+    ? T[k]
+    : k extends keyof Q
+    ? Q[k]
+    : never;
+}>;
+
+/**
+ * @internal
  */
 export type Prefixer<
   TObj extends Record<string, any>,
