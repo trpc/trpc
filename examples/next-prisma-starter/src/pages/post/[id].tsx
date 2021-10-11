899fd005be1ac88873ffe0dd969255322efb46b9
KATT <alexander@n1s.se>
2021-10-11 18:12:01 +0200

    simplify

--- examples/next-prisma-starter/src/pages/post/[id].tsx
+++ examples/next-prisma-starter/src/pages/post/[id].tsx
@@ -19,6 +19,9 @@ export default function PostViewPage() {
     return <>Loading...</>;
   }
   const { data } = postQuery;
+  if (!data) {
+    return <>No post found :(</>;
+  }
   return (
     <>
       <h1>{data.title}</h1>
