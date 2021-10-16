70dd465ecf7e143aa5b3fcc39196e61177d411aa
KATT <alexander@n1s.se>
2021-10-16 14:44:56 +0200

    add exports

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,3 +9,9 @@ export * from './types';
 export * from './deprecated/createHttpServer';
 export * from './deprecated/requestHandler';
 export * from './deprecated/LegacyRouter';
+
+export * as express from './adapters/express';
+export * as nextjs from './adapters/next';
+export * as standalone from './adapters/standalone';
+export * as ws from './adapters/ws';
+export * as nodeHTTP from './adapters/node-http';
