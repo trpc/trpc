72bf05b2f70ce0d3e96671438857b02d05b1cc98
Alex Johansson <alexander@n1s.se>
2021-10-27 05:13:00 +0200

    upgrade examples to next 12 (#1174)

--- examples/next-prisma-starter-websockets/prisma/migrations/20210704013430_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
-
-    PRIMARY KEY ("id")
-);
---
+++ examples/next-prisma-starter-websockets/prisma/migrations/20211027030641_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "PosterSource" AS ENUM ('RAW', 'GITHUB');
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "source" "PosterSource" NOT NULL DEFAULT E'RAW',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_createdAt_key" ON "Post"("createdAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_updatedAt_key" ON "Post"("updatedAt");
