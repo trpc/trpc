cd949043649bcaa523459072fe5cac8a6b21dea3
Alex Johansson <alexander@n1s.se>
2021-11-11 16:21:02 +0100

    improve big routers performance (#1218)

---
+++ examples/.wip/next-prisma-starter--big/src/server/context.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === 'development'
+      ? ['query', 'error', 'warn']
+      : ['error'],
+});
+/**
+ * Creates context for an incoming request
+ * @link https://trpc.io/docs/context
+ */
+export const createContext = async ({
+  req,
+  res,
+}: trpcNext.CreateNextContextOptions) => {
+  // for API-response caching see https://trpc.io/docs/caching
+  return {
+    req,
+    res,
+    prisma,
+  };
+};
+
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
