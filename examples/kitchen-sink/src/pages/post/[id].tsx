9d1bb51bd60a7b8c4bff9803cd75602aa00dcb0d
KATT <alexander@n1s.se>
2021-11-12 10:03:04 +0100

    kitchen sink init

---
+++ examples/kitchen-sink/src/pages/post/[id].tsx
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router';
+import { trpc } from 'utils/trpc';
+import NextError from 'next/error';
+
+export default function PostViewPage() {
+  const id = useRouter().query.id as string;
+  const postQuery = trpc.useQuery(['post.byId', { id }]);
+
+  if (postQuery.error) {
+    return (
+      <NextError
+        title={postQuery.error.message}
+        statusCode={postQuery.error.data?.httpStatus ?? 500}
+      />
+    );
+  }
+
+  if (postQuery.status !== 'success') {
+    return <>Loading...</>;
+  }
+  const { data } = postQuery;
+  return (
+    <>
+      <h1>{data.title}</h1>
+      <em>Created {data.createdAt.toLocaleDateString()}</em>
+
+      <p>{data.text}</p>
+
+      <h2>Raw data:</h2>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+    </>
+  );
+}
---
+++ examples/kitchen-sink/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
