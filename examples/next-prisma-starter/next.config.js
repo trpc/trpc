b2293b6671b852ef32eaf3f70ae6c57e76e2b8b2
KATT <alexander@n1s.se>
2021-12-21 14:36:24 +0100

    attempt to fix mismatch error

--- examples/next-prisma-starter/next.config.js
+++ examples/next-prisma-starter/next.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
 /**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  * @type {import('next').NextConfig}
  */
-module.exports = {};
+module.exports = {
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+    };
+    return config;
+  },
+};
