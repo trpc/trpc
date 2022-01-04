011ee3a05f0d3f2dbc2f3f640c25ad47b33f65ad
KATT <alexander@n1s.se>
2022-01-04 16:19:56 +0100

    map function

--- packages/client/src/rx/observable.test.ts
+++ packages/client/src/rx/observable.test.ts
@@ -1,5 +1,8 @@
 import { observable } from './observable';
 import { share } from './operators/share';
+import { EventEmitter } from 'events';
+import { map } from './operators';
+import { expectTypeOf } from 'expect-type';
 
 test('smoke', () => {
   const obs = observable<number, Error>((observer) => {
@@ -89,4 +92,70 @@ test('pipe', () => {
     expect(complete.mock.calls).toHaveLength(0);
     expect(error.mock.calls).toHaveLength(0);
   }
+});
+
+interface SubscriptionEvents<TOutput> {
+  data: (data: TOutput) => void;
+}
+declare interface CustomEventEmitter<TOutput> {
+  on<U extends keyof SubscriptionEvents<TOutput>>(
+    event: U,
+    listener: SubscriptionEvents<TOutput>[U],
+  ): this;
+
+  once<U extends keyof SubscriptionEvents<TOutput>>(
+    event: U,
+    listener: SubscriptionEvents<TOutput>[U],
+  ): this;
+
+  emit<U extends keyof SubscriptionEvents<TOutput>>(
+    event: U,
+    ...args: Parameters<SubscriptionEvents<TOutput>[U]>
+  ): boolean;
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class CustomEventEmitter<TOutput> extends EventEmitter {}
+
+test('map', () => {
+  type EventShape = { num: number };
+  const ee = new CustomEventEmitter<EventShape>();
+  const eventObservable = observable<EventShape, unknown>((subscriber) => {
+    const callback = (data: EventShape) => {
+      subscriber.next(data);
+    };
+    ee.on('data', callback);
+
+    return () => {
+      ee.off('data', callback);
+    };
+  });
+  const pipeCalls = jest.fn();
+  const piped = eventObservable.pipe(
+    map((...args) => {
+      pipeCalls(...args);
+      const [value] = args;
+      return value.num;
+    }),
+  );
+
+  const next = jest.fn();
+  const subscription = piped.subscribe({
+    next(value) {
+      expectTypeOf<number>(value);
+      next(value);
+    },
+  });
+  expect(next).not.toHaveBeenCalled();
+  ee.emit('data', { num: 1 });
+  ee.emit('data', { num: 2 });
+  expect(next).toHaveBeenCalledTimes(2);
+  expect(next.mock.calls).toEqual([[1], [2]]);
+  expect(pipeCalls.mock.calls).toEqual([
+    [{ num: 1 }, 0],
+    [{ num: 2 }, 1],
+  ]);
+
+  expect(ee.listeners('data')).toHaveLength(1);
+  subscription.unsubscribe();
+  expect(ee.listeners('data')).toHaveLength(0);
 });
--- packages/client/src/rx/operators/index.ts
+++ packages/client/src/rx/operators/index.ts
@@ -1,1 +1,2 @@
 export * from './share';
+export * from './map';
