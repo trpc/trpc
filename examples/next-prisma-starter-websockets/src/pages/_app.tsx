aced52cc0ac6e8b1a21d17a17ed44f647f47caf2
KATT <alexander@n1s.se>
2022-01-08 00:17:48 -0500

    fix

--- examples/next-prisma-starter-websockets/src/pages/_app.tsx
+++ examples/next-prisma-starter-websockets/src/pages/_app.tsx
@@ -1,16 +1,6 @@
-import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
-import { wsLink, createWSClient } from '@trpc/client/links/wsLink';
-import { loggerLink } from '@trpc/client/links/loggerLink';
-import { withTRPC } from '@trpc/next';
-import { AppType } from 'next/dist/shared/lib/utils';
-import type { AppRouter } from 'server/routers/_app';
-import superjson from 'superjson';
-import getConfig from 'next/config';
 import { getSession, Provider } from 'next-auth/client';
+import { AppType } from 'next/dist/shared/lib/utils';
 import { trpc } from 'utils/trpc';
-const { publicRuntimeConfig } = getConfig();
-
-const { APP_URL, WS_URL } = publicRuntimeConfig;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
