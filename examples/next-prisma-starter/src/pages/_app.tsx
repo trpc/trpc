cd214a2ef84ea2e074960f9b0d9c4ced0521c8ab
KATT <alexander@n1s.se>
2022-01-24 08:57:36 +0100

    5732

--- examples/next-prisma-starter/src/pages/_app.tsx
+++ examples/next-prisma-starter/src/pages/_app.tsx
@@ -77,20 +77,20 @@ export default withTRPC<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: true,
+  ssr: false,
   /**
    * Set headers or status code when doing SSR
    */
-  responseMeta({ clientErrors }) {
-    if (clientErrors.length) {
-      // propagate http first error from API calls
-      return {
-        status: clientErrors[0].data?.httpStatus ?? 500,
-      };
-    }
+  // responseMeta({ clientErrors }) {
+  //   if (clientErrors.length) {
+  //     // propagate http first error from API calls
+  //     return {
+  //       status: clientErrors[0].data?.httpStatus ?? 500,
+  //     };
+  //   }
 
-    // for app caching with SSR see https://trpc.io/docs/caching
+  //   // for app caching with SSR see https://trpc.io/docs/caching
 
-    return {};
-  },
+  //   return {};
+  // },
 })(MyApp);
