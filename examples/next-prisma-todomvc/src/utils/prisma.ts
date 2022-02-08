c6a2e1501c98325c16ffe1ed3f3b28860ff96f29
KATT <alexander@n1s.se>
2022-02-08 13:49:08 +0100

    tweak

--- examples/next-prisma-todomvc/src/utils/prisma.ts
+++ examples/next-prisma-todomvc/src/utils/prisma.ts
@@ -3,12 +3,6 @@
  */
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+export const prisma = (global as any).prisma || new PrismaClient();
 
-export const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') (global as any).prisma = prisma;
