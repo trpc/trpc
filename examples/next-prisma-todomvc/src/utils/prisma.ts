2a451da624d66aad1b2206aa06407051ae642f37
KATT <alexander@n1s.se>
2022-02-08 14:04:16 +0100

    tweak

--- examples/next-prisma-todomvc/src/utils/prisma.ts
+++ examples/next-prisma-todomvc/src/utils/prisma.ts
@@ -6,4 +6,6 @@ import { PrismaClient } from '@prisma/client';
 export const prisma: PrismaClient =
   (global as any).prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') (global as any).prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  (global as any).prisma = prisma;
+}
