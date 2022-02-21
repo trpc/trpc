7d99abbe8347895010f519ea5d078a553257150f
KATT <alexander@n1s.se>
2022-02-21 10:03:11 +0100

    fix fastify plugin

--- packages/server/rollup.config.js
+++ packages/server/rollup.config.js
@@ -4,12 +4,13 @@ const config = getRollupConfig({
   input: [
     'src/index.ts',
     'src/adapters/express.ts',
+    'src/adapters/fastify/index.ts',
     'src/adapters/next.ts',
     'src/adapters/node-http/index.ts',
     'src/adapters/standalone.ts',
     'src/adapters/ws.ts',
-    'src/ws/index.ts',
     'src/rpc/index.ts',
+    'src/ws/index.ts',
   ],
 });
 
