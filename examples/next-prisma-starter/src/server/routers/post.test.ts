346218b9bc6a9d1d0a7fc824583a7dc8c4ea566f
KATT <alexander@n1s.se>
2022-03-07 14:03:12 +0100

    add integration test

---
+++ examples/next-prisma-starter/src/server/routers/post.test.ts
@@ -0,0 +1,17 @@
+import { inferMutationInput } from '~/utils/trpc';
+import { appRouter } from './_app';
+
+test('add and get post', async () => {
+  const caller = appRouter.createCaller({});
+
+  const input: inferMutationInput<'post.add'> = {
+    text: 'hello test',
+    title: 'hello test',
+  };
+  const post = await caller.mutation('post.add', input);
+  const byId = await caller.query('post.byId', {
+    id: post.id,
+  });
+
+  expect(byId).toMatchObject(input);
+});
