fee56d92d8c66c629c27eb7bff39319523fffd54
KATT <alexander@n1s.se>
2022-03-07 14:48:06 +0100

    info

--- examples/next-prisma-starter/src/server/routers/post.ts
+++ examples/next-prisma-starter/src/server/routers/post.ts
@@ -9,6 +9,11 @@ import { TRPCError } from '@trpc/server';
 import { prisma } from '../prisma';
 import { Prisma } from '@prisma/client';
 
+/**
+ * Default selector for Post.
+ * It's important to always explicitly say which fields you want to return in order to not leak extra information
+ * @see https://github.com/prisma/prisma/issues/9353
+ */
 const defaultPostSelect = Prisma.validator<Prisma.PostSelect>()({
   id: true,
   title: true,
