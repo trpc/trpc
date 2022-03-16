19abe70aca4164ac58c038b402958493c72b5329
KATT <alexander@n1s.se>
2022-03-16 18:09:41 +0100

    :tada:

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -1,16 +1,7 @@
 import { getRollupConfig } from '../../scripts/rollup';
 
 const config = getRollupConfig({
-  input: [
-    'src/index.ts',
-    'src/links/httpBatchLink.ts',
-    'src/links/httpLink.ts',
-    'src/links/loggerLink.ts',
-    'src/links/splitLink.ts',
-    'src/links/transformerLink.ts',
-    'src/links/wsLink.ts',
-    'src/observable/index.ts',
-  ],
+  input: ['src/index.ts', 'src/observable/index.ts'],
 });
 
 export default config;
