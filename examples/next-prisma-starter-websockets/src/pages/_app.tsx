8cc78c64f4915093784e4005184e4edd125dfa70
KATT <alexander@n1s.se>
2022-03-17 00:32:27 +0100

    tweaks

--- examples/next-prisma-starter-websockets/src/pages/_app.tsx
+++ examples/next-prisma-starter-websockets/src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import { getSession, SessionProvider } from 'next-auth/react';
-import getConfig from 'next/config';
 import { AppType } from 'next/dist/shared/lib/utils';
 import { trpc } from 'utils/trpc';
-const { publicRuntimeConfig } = getConfig();
-
-const { APP_URL, WS_URL } = publicRuntimeConfig;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
