29298a8aa4025a00ecc62c7e349b2852009574bc
KATT <alexander@n1s.se>
2022-03-21 18:01:54 +0100

    seed

---
+++ examples/next-prisma-starter-websockets/prisma/seed.ts
@@ -0,0 +1,19 @@
+/**
+ * Adds seed data to your db
+ *
+ * @link https://www.prisma.io/docs/guides/database/seed-database
+ */
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
