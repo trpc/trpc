a0a8b1b62d5702965fdcc14170c0957085eb9bee
KATT <alexander@n1s.se>
2022-03-21 21:03:27 +0100

    tweak

--- packages/server/test/__packages.ts
+++ packages/server/test/__packages.ts
@@ -4,6 +4,8 @@ import * as trpcServer from '../../server/src';
 jest.mock('@trpc/server', () => trpcServer);
 import * as trpcClient from '../../client/src';
 jest.mock('@trpc/client', () => trpcClient);
+import * as trpcClient__observable from '../../client/src/observable';
+jest.mock('@trpc/client/observable', () => trpcClient__observable);
 import * as trpcReact from '../../react/src';
 jest.mock('@trpc/react', () => trpcReact);
 import * as trpcReact__ssg from '../../react/src/ssg';
