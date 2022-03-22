29d89cc92aea069548e61f476f967feee4803a85
KATT <alexander@n1s.se>
2022-03-23 00:11:34 +0100

    fix :shrug:

--- packages/server/src/adapters/fastify/fastifyRequestHandler.ts
+++ packages/server/src/adapters/fastify/fastifyRequestHandler.ts
@@ -1,13 +1,13 @@
-import { URLSearchParams } from 'url';
-import { FastifyReply, FastifyRequest } from 'fastify';
 import { assertNotBrowser } from '../../assertNotBrowser';
-import { NodeHTTPCreateContextOption } from '../node-http';
-import { AnyRouter, inferRouterContext } from '../../router';
-import { resolveHTTPResponse } from '../../http/resolveHTTPResponse';
 import {
   HTTPBaseHandlerOptions,
   HTTPRequest,
 } from '../../http/internals/types';
+import { resolveHTTPResponse } from '../../http/resolveHTTPResponse';
+import { AnyRouter, inferRouterContext } from '../../router';
+import { NodeHTTPCreateContextOption } from '../node-http';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { URLSearchParams } from 'url';
 
 assertNotBrowser();
 
@@ -72,7 +72,7 @@ export async function fastifyRequestHandler<
       continue;
     }
 
-    await res.header(key, value);
+    void res.header(key, value);
   }
   await res.send(result.body);
 }
