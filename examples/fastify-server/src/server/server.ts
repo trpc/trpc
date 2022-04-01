6d27958ca46b995ec412aef9e4175dff61c72f9d
skarab42 <skarab@bluewin.ch>
2022-04-01 12:43:20 +0200

    fix: fastify adapter using fastify-plugin (#1717)

--- examples/fastify-server/src/server/server.ts
+++ examples/fastify-server/src/server/server.ts
@@ -1,5 +1,4 @@
 import fastify from 'fastify';
-import fp from 'fastify-plugin';
 import ws from 'fastify-websocket';
 import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
 import { createContext } from './router/context';
@@ -18,15 +17,27 @@ export function createServer(opts: ServerOptions) {
   const server = fastify({ logger: dev });
 
   server.register(ws);
-  server.register(fp(fastifyTRPCPlugin), {
+  server.register(fastifyTRPCPlugin, {
     prefix,
     useWSS: true,
     trpcOptions: { router: appRouter, createContext },
   });
 
   server.get('/', async () => {
-    return { hello: 'world' };
+    return { hello: 'wait-on 💨' };
   });
+
+  server.get('/hello', async () => {
+    return { hello: 'GET' };
+  });
+
+  server.post<{ Body: { text: string; life: number } }>(
+    '/hello',
+    async ({ body }) => {
+      console.log('BODY:', typeof body, body);
+      return { hello: 'POST', body };
+    },
+  );
 
   const stop = () => server.close();
   const start = async () => {
