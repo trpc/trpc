426120900f5e2a1f0874e8b4ff7134c2bed0e95e
KATT <alexander@n1s.se>
2022-04-05 23:19:13 +0200

    Revert "wip"
    This reverts commit 54c0c18f9088b917f6dbe6f859d26deca5c117ac.

--- examples/next-prisma-starter/tailwind.config.js
+++ examples/next-prisma-starter/tailwind.config.js
@@ -2,10 +2,7 @@
 
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
