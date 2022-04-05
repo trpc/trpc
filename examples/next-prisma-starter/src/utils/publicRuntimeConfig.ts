5281e144c3a37cd603f4972591c67a5e36876754
KATT <alexander@n1s.se>
2022-04-05 22:15:00 +0200

    inferred

--- examples/next-prisma-starter/src/utils/publicRuntimeConfig.ts
+++ examples/next-prisma-starter/src/utils/publicRuntimeConfig.ts
@@ -7,7 +7,7 @@ import type * as config from '../../next.config';
 import getConfig from 'next/config';
 
 /**
- * Infers the type from the `publicRuntime` in `next.config.js`
+ * Inferred type from `publicRuntime` in `next.config.js`
  */
 type PublicRuntimeConfig = typeof config.publicRuntimeConfig;
 
