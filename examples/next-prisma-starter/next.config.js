9175d793ff7281acd1f636ad4a834bd7be16aa47
KATT <alexander@n1s.se>
2022-04-05 22:09:06 +0200

    add comment

--- examples/next-prisma-starter/next.config.js
+++ examples/next-prisma-starter/next.config.js
@@ -1,7 +1,11 @@
 // @ts-check
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { env } = require('./src/server/env');
+
 /**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
  * @template {import('next').NextConfig} T
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
