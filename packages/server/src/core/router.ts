c2367751652190597f815908c0c0cb10a7fa1cd8
KATT <alexander@n1s.se>
2022-05-02 13:05:10 +0200

    callbag for router

--- packages/server/src/core/router.ts
+++ packages/server/src/core/router.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { ErrorFormatter } from '../error/formatter';
+import { TRPCErrorShape } from '../rpc';
 import { CombinedDataTransformer } from '../transformer';
 import { mergeWithoutOverrides } from './internals/mergeWithoutOverrides';
 import { Overwrite, PickFirstDefined, ValidateShape } from './internals/utils';
@@ -8,30 +9,94 @@ import { Procedure } from './procedure';
 // FIXME this should properly use TContext
 type ProcedureRecord<_TContext> = Record<string, Procedure<any>>;
 
-/**
- * This only exists b/c of interop mode
- * @deprecated
- * @internal
- */
-interface RouterDef<TContext> {
-  queries: ProcedureRecord<TContext>;
-  mutations: ProcedureRecord<TContext>;
-  subscriptions: ProcedureRecord<TContext>;
-  errorFormatter: ErrorFormatter<TContext, any>;
+interface RouterParams<
+  TContext,
+  TErrorShape extends TRPCErrorShape<number>,
+  TQueries extends ProcedureRecord<TContext>,
+  TMutations extends ProcedureRecord<TContext>,
+  TSubscriptions extends ProcedureRecord<TContext>,
+> {
+  /**
+   * @internal
+   */
+  _ctx: TContext;
+  /**
+   * @internal
+   */
+  _errorShape: TErrorShape;
+  queries: TQueries;
+  mutations: TMutations;
+  subscriptions: TSubscriptions;
+  errorFormatter: ErrorFormatter<TContext, TErrorShape>;
   transformer: CombinedDataTransformer;
 }
 
-export interface Router<TContext> extends RouterDef<TContext> {
+type AnyRouterParams<TContext = any> = RouterParams<
+  TContext,
+  any,
+  any,
+  any,
+  any
+>;
+
+/**
+ * @internal
+ */
+export type inferHandlerInput<TProcedure extends Procedure<any>> =
+  TProcedure extends Procedure<infer TParams>
+    ? undefined extends TParams['_input_in'] // ? is input optional
+      ? unknown extends TParams['_input_in'] // ? is input unset
+        ? [(null | undefined)?] // -> there is no input
+        : [(TParams['_input_in'] | null | undefined)?] // -> there is optional input
+      : [TParams['_input_in']] // -> input is required
+    : [(undefined | null)?]; // -> there is no input
+
+/**
+ * @internal
+ */
+type inferHandlerFn<TProcedures extends ProcedureRecord<any>> = <
+  TProcedure extends TProcedures[TPath],
+  TPath extends keyof TProcedures & string,
+>(
+  path: TPath,
+  ...args: inferHandlerInput<TProcedure>
+) => ReturnType<TProcedure>;
+
+/**
+ * This only exists b/c of interop mode
+ * @internal
+ */
+
+type RouterCaller<TParams extends AnyRouterParams> = (ctx: TParams['_ctx']) => {
+  query: inferHandlerFn<TParams['queries']>;
+  mutation: inferHandlerFn<TParams['mutations']>;
+  subscription: inferHandlerFn<TParams['subscriptions']>;
+};
+
+export interface Router<TParams extends AnyRouterParams> {
   /**
    * @deprecated Deprecated since v10
    */
-  _def: RouterDef<TContext>;
+  _def: RouterParams<
+    TParams['_ctx'],
+    TParams['_errorShape'],
+    TParams['queries'],
+    TParams['mutations'],
+    TParams['subscriptions']
+  >;
+  queries: TParams['queries'];
+  mutations: TParams['mutations'];
+  subscriptions: TParams['subscriptions'];
+  errorFormatter: TParams['errorFormatter'];
+  transformer: TParams['transformer'];
+
+  createCaller: RouterCaller<TParams>;
 }
 
 /**
  * @internal
  */
-export type RouterOptions<TContext> = Partial<Router<TContext>>;
+export type RouterOptions<TContext> = Partial<AnyRouterParams<TContext>>;
 
 /**
  * @internal
@@ -60,6 +125,17 @@ const defaultErrorFormatter: ErrorFormatter<any, any> = ({ shape }) => {
   return shape;
 };
 
+const emptyRouter = {
+  _ctx: null as any,
+  _errorShape: null as any,
+  queries: {},
+  mutations: {},
+  subscriptions: {},
+  errorFormatter: defaultErrorFormatter,
+  transformer: defaultTransformer,
+};
+// type EmptyRouter = typeof emptyRouter;
+
 /**
  *
  * @internal
@@ -72,22 +148,23 @@ export function createRouterWithContext<TContext>(
     TProcedures extends RouterBuildOptions<TContext>,
   >(
     procedures: ValidateShape<TProcedures, RouterBuildOptions<TContext>>,
-  ): Overwrite<Router<TContext>, Overwrite<TDefaults, TProcedures>> {
+  ): Overwrite<AnyRouter, Overwrite<TDefaults, TProcedures>> {
     const result = mergeWithoutOverrides<
       RouterDefaultOptions<TContext> & RouterBuildOptions<TContext>
     >(defaults || {}, procedures);
 
-    const _def: RouterDef<TContext> = {
-      queries: {},
-      mutations: {},
-      subscriptions: {},
-      errorFormatter: result.errorFormatter || defaultErrorFormatter,
-      transformer: result.transformer || defaultTransformer,
+    const _def: AnyRouterParams<TContext> = {
+      ...emptyRouter,
+      errorFormatter: result.errorFormatter || emptyRouter.errorFormatter,
+      transformer: result.transformer || emptyRouter.transformer,
       ...result,
     };
-    const router: Router<TContext> = {
+    const router: AnyRouter = {
       _def,
       ..._def,
+      createCaller: () => {
+        throw new Error('Unimpl');
+      },
     };
     return router as any;
   };
@@ -96,9 +173,8 @@ export function createRouterWithContext<TContext>(
 type EnsureRecord<T> = undefined extends T ? {} : T;
 
 type mergeRouters<
-  TContext,
-  A extends RouterOptions<TContext>,
-  B extends RouterOptions<TContext>,
+  A extends Partial<AnyRouter>,
+  B extends Partial<AnyRouter>,
 > = {
   queries: EnsureRecord<A['queries']> & EnsureRecord<B['queries']>;
   mutations: EnsureRecord<A['mutations']> & EnsureRecord<B['mutations']>;
@@ -106,18 +182,16 @@ type mergeRouters<
     EnsureRecord<B['subscriptions']>;
   errorFormatter: PickFirstDefined<B['errorFormatter'], A['errorFormatter']>;
   transformer: PickFirstDefined<B['transformer'], A['transformer']>;
+  createCaller: A['createCaller'];
 };
 
-/**
- * @internal
- */
-type mergeRoutersVariadic<Routers extends RouterOptions<any>[]> =
+type mergeRoutersVariadic<Routers extends Partial<AnyRouter>[]> =
   Routers extends []
     ? {}
     : Routers extends [infer First, ...infer Rest]
-    ? First extends RouterOptions<any>
-      ? Rest extends RouterOptions<any>[]
-        ? mergeRouters<any, First, mergeRoutersVariadic<Rest>>
+    ? First extends Partial<AnyRouter>
+      ? Rest extends Partial<AnyRouter>[]
+        ? mergeRouters<First, mergeRoutersVariadic<Rest>>
         : never
       : never
     : never;
