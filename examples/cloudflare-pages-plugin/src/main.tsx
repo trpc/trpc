1e2eb7cb44e2ed5a01221aac40f0546b4571827b
toyamarinyon <toyamarinyon@gmail.com>
2022-05-14 21:50:38 +0900

    lint

--- examples/cloudflare-pages-plugin/src/main.tsx
+++ examples/cloudflare-pages-plugin/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
