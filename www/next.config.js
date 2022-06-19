1db5aabb96e6e9b1cfa394af65f13e279d1a6536
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-19 02:57:34 +0100

    feat: sets up mdx

--- www/next.config.js
+++ www/next.config.js
@@ -1,6 +1,15 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ['ts', 'tsx', 'mdx'],
+};
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig);
