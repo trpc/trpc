1478ba7ef3b123c3d736606df7ea21fa14c0f800
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-19 02:50:09 +0100

    feat: adds path aliases

--- www/jest.config.js
+++ www/jest.config.js
@@ -6,6 +6,10 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
 };
 
