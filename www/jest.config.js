d95509ff32e9bd6d4dadf1ba5cb89b9ee8347477
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-19 02:47:12 +0100

    feat: sets up jest

---
+++ www/jest.config.js
@@ -0,0 +1,12 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
