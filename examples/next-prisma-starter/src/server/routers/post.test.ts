042a3f559827869cde76a14ec26a6d4c58cbf2ef
KATT <alexander@n1s.se>
2022-06-19 02:57:32 +0200

    fix tests

--- examples/next-prisma-starter/src/server/routers/post.test.ts
+++ examples/next-prisma-starter/src/server/routers/post.test.ts
@@ -9,12 +9,12 @@ test('add and get post', async () => {
   const ctx = await createContextInner({});
   const caller = appRouter.createCaller(ctx);
 
-  const input: inferMutationInput<'post.add'> = {
+  const input: inferMutationInput<'postAdd'> = {
     text: 'hello test',
     title: 'hello test',
   };
-  const post = await caller.mutation('post.add', input);
-  const byId = await caller.query('post.byId', {
+  const post = await caller.mutation('postAdd', input);
+  const byId = await caller.query('postById', {
     id: post.id,
   });
 
