61d02a3f8366df0746e953b670b070196ae8bd3d
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-20 23:25:28 +0100

    refactor: lighten dark mode a bit

--- www/src/pages/_document.tsx
+++ www/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="dark:bg-black dark:text-white">
+      <body className="dark:bg-zinc-900 dark:text-zinc-50">
         <Main />
         <NextScript />
       </body>
