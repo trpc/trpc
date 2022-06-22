8792f78914546d80cd1b41eb30cd3cef27febe22
jgoux <hi@jgoux.dev>
2022-06-22 23:12:37 +0200

    fix linting

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,9 +9,14 @@ export * from './core';
 /**
  * These exports are necessary for type definition files generation when consuming this package
  */
-export type { DefaultErrorShape, ErrorFormatter } from "./error/formatter";
-export type { mergeRoutersGeneric } from "./core/internals/__generated__/mergeRoutersGeneric";
-export type { ProcedureBuilder } from "./core/internals/procedureBuilder";
-export type { ValidateShape, EnsureRecord, Overwrite, unsetMarker } from "./core/internals/utils";
-export type { MiddlewareFunction } from "./core/middleware";
-export type { Router } from "./core/router";
+export type { DefaultErrorShape, ErrorFormatter } from './error/formatter';
+export type { mergeRoutersGeneric } from './core/internals/__generated__/mergeRoutersGeneric';
+export type { ProcedureBuilder } from './core/internals/procedureBuilder';
+export type {
+  ValidateShape,
+  EnsureRecord,
+  Overwrite,
+  unsetMarker,
+} from './core/internals/utils';
+export type { MiddlewareFunction } from './core/middleware';
+export type { Router } from './core/router';
