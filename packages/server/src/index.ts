b9e99db9083be3b6e4a9e896e2870a8cbd1299e1
jgoux <hi@jgoux.dev>
2022-06-22 23:05:47 +0200

    fix(types): export internal types for type definition files generation

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,3 +5,13 @@ export * from './error/TRPCError';
 export * from './types';
 export { router } from './deprecated/router';
 export * from './core';
+
+/**
+ * These exports are necessary for type definition files generation when consuming this package
+ */
+export type { DefaultErrorShape, ErrorFormatter } from "./error/formatter";
+export type { mergeRoutersGeneric } from "./core/internals/__generated__/mergeRoutersGeneric";
+export type { ProcedureBuilder } from "./core/internals/procedureBuilder";
+export type { ValidateShape, EnsureRecord, Overwrite, unsetMarker } from "./core/internals/utils";
+export type { MiddlewareFunction } from "./core/middleware";
+export type { Router } from "./core/router";
