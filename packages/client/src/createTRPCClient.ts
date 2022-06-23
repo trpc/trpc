118f8ea1d4711d69b589eac9ed3c5e183fd74d0a
KATT <alexander@n1s.se>
2022-06-23 02:00:19 +0200

    cleanup

--- packages/client/src/createTRPCClient.ts
+++ packages/client/src/createTRPCClient.ts
@@ -61,9 +61,7 @@ export function createTRPCClient<TRouter extends AnyRouter>(
   opts: CreateTRPCClientOptions<TRouter>,
 ) {
   const client = new Client<TRouter>(opts);
-  // Here we need to wrap the client in a Proxy to be able to call deep objects and translate them to `client.query` calls
-  // And then for instance a call to `client.child.grandchild.queries.foo` should translate to `client.query('child.grandchild.foo')`
-  // TODO wrap with Proxy
+  // Here we need to wrap the client in a Proxy to be able to call deep objects and translate them to `client.query`-calls
   return makeProxy(client);
 }
 
