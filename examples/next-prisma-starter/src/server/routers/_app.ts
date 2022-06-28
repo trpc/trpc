606353088739f19b6ca0ec12b5445998aadd1408
Sachin Raja <sachinraja2349@gmail.com>
2022-06-28 09:37:21 -0700

    fix example

--- examples/next-prisma-starter/src/server/routers/_app.ts
+++ examples/next-prisma-starter/src/server/routers/_app.ts
@@ -2,28 +2,14 @@
  * This file contains the root router of your tRPC-backend
  */
 import { t } from '../trpc';
-import { createTRPCClient } from '@trpc/client';
-import { z } from 'zod';
-
-// import { healthRouter } from './health';
-// import { postRouter } from './post';
+import { healthRouter } from './health';
+import { postRouter } from './post';
 
 export const appRouter = t.router({
   procedures: {
-    post: t.procedure.query(() => 1),
-    health: {
-      a: t.procedure.query(() => 5),
-      b: t.procedure.query(() => 6),
-      c: {
-        d: t.procedure.input(z.string().nullish()).query(() => 7),
-      },
-    },
+    post: postRouter,
+    health: healthRouter,
   },
 });
 
-console.log(appRouter._def.procedures);
 export type AppRouter = typeof appRouter;
-
-const client = createTRPCClient<AppRouter>({ url: '' });
-
-client.health.c.d.query();
