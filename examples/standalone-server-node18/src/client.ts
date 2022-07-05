e432889733e10128fcfbad131db6e8f2fab3424a
KATT <alexander@n1s.se>
2022-07-05 09:02:59 +0200

    add node 18 test

---
+++ examples/standalone-server-node18/src/client.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createTRPCClient } from '@trpc/client';
+import { httpLink } from '@trpc/client/links/httpLink';
+import { splitLink } from '@trpc/client/links/splitLink';
+import { createWSClient, wsLink } from '@trpc/client/links/wsLink';
+import ws from 'ws';
+import type { AppRouter } from './server';
+
+// polyfill WebSocket
+const globalAny = global as any;
+globalAny.WebSocket = ws;
+// 🎉 `fetch` doesn't need polyfill b/c this is node 18
+
+async function main() {
+  // http calls
+  const wsClient = createWSClient({
+    url: `ws://localhost:2022`,
+  });
+  const client = createTRPCClient<AppRouter>({
+    links: [
+      // call subscriptions through websockets and the rest over http
+      splitLink({
+        condition(op) {
+          return op.type === 'subscription';
+        },
+        true: wsLink({
+          client: wsClient,
+        }),
+        false: httpLink({
+          url: `http://localhost:2022`,
+        }),
+      }),
+    ],
+  });
+
+  const helloResponse = await client.query('hello', {
+    name: 'world',
+  });
+
+  console.log('helloResponse', helloResponse);
+
+  const createPostRes = await client.mutation('createPost', {
+    title: 'hello world',
+    text: 'check out tRPC.io',
+  });
+  console.log('createPostResponse', createPostRes);
+
+  let count = 0;
+  const unsub = client.subscription('randomNumber', null, {
+    onNext(data) {
+      // ^ note that `data` here is inferred
+      console.log('received', data);
+      count++;
+      if (count > 3) {
+        // stop after 3 pulls
+        unsub();
+      }
+    },
+    onError(err) {
+      console.error('error', err);
+    },
+    onDone() {
+      console.log('done called - closing websocket');
+      wsClient.close();
+    },
+  });
+}
+
+main();
