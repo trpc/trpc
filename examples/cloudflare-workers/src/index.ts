9ae2e0c882b77e8a4200151e2fc641cdec9185f0
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-11 20:08:30 +0200

    fix: bump wrangler version

--- examples/cloudflare-workers/src/index.ts
+++ examples/cloudflare-workers/src/index.ts
@@ -3,6 +3,7 @@
  *
  * - Run `yarn dev` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ *   Tip: Test going to the /hello or /post.listPosts endpoints
  * - Run `wrangler publish --name my-worker` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
@@ -16,7 +17,7 @@ export default {
       endpoint: '',
       req: request,
       router: appRouter,
-      createContext: ({ req }) => ({ req }),
+      createContext: () => ({}),
     });
   },
 };
