6e0ef91b79cc880eeca90c8a958908b925dddb44
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-11 17:46:58 +0200

    add v10 express example

---
+++ examples/express-server/src/server.ts
@@ -0,0 +1,164 @@
+import { TRPCError, inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { EventEmitter } from 'events';
+import express from 'express';
+import { z } from 'zod';
+
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  const getUser = () => {
+    if (req.headers.authorization !== 'secret') {
+      return null;
+    }
+    return {
+      name: 'alex',
+    };
+  };
+
+  return {
+    req,
+    res,
+    user: getUser(),
+  };
+};
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC<{ ctx: Context }>()();
+
+// --------- create procedures etc
+
+let id = 0;
+
+const ee = new EventEmitter();
+const db = {
+  posts: [
+    {
+      id: ++id,
+      title: 'hello',
+    },
+  ],
+  messages: [createMessage('initial message')],
+};
+function createMessage(text: string) {
+  const msg = {
+    id: ++id,
+    text,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  };
+  ee.emit('newMessage', msg);
+  return msg;
+}
+
+/*const posts = createRouter()
+  .mutation('create', {
+    input: z.object({
+      title: z.string(),
+    }),
+    resolve: ({ input }) => {
+      const post = {
+        id: ++id,
+        ...input,
+      };
+      db.posts.push(post);
+      return post;
+    },
+  })
+  .query('list', {
+    resolve: () => db.posts,
+  });
+
+const messages = createRouter()
+  .query('list', {
+    resolve: () => db.messages,
+  })
+  .mutation('add', {
+    input: z.string(),
+    resolve: async ({ input }) => {
+      const msg = createMessage(input);
+
+      db.messages.push(msg);
+
+      return msg;
+    },
+  });
+  */
+
+const postRouter = t.router({
+  createPost: t.procedure
+    .input(z.object({ title: z.string() }))
+    .mutation(({ input }) => {
+      const post = {
+        id: ++id,
+        ...input,
+      };
+      db.posts.push(post);
+      return post;
+    }),
+  listPosts: t.procedure.query(() => db.posts),
+});
+
+const messageRouter = t.router({
+  addMessage: t.procedure.input(z.string()).mutation(({ input }) => {
+    const msg = createMessage(input);
+    db.messages.push(msg);
+
+    return msg;
+  }),
+  listMessages: t.procedure.query(() => db.messages),
+});
+
+// root router to call
+const appRouter = t.router({
+  // merge predefined routers
+  post: postRouter,
+  message: messageRouter,
+  // or individual procedures
+  hello: t.procedure.input(z.string().nullish()).query(({ input, ctx }) => {
+    return `hello ${input ?? ctx.user?.name ?? 'world'}`;
+  }),
+  // or inline a router
+  admin: t.router({
+    secret: t.procedure.query(({ ctx }) => {
+      if (!ctx.user) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+      }
+      if (ctx.user?.name !== 'alex') {
+        throw new TRPCError({ code: 'FORBIDDEN' });
+      }
+      return {
+        secret: 'sauce',
+      };
+    }),
+  }),
+});
+
+export type AppRouter = typeof appRouter;
+
+async function main() {
+  // express implementation
+  const app = express();
+
+  app.use((req, _res, next) => {
+    // request logger
+    console.log('⬅️ ', req.method, req.path, req.body ?? req.query);
+
+    next();
+  });
+
+  app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    }),
+  );
+  app.get('/', (_req, res) => res.send('hello'));
+  app.listen(2021, () => {
+    console.log('listening on port 2021');
+  });
+}
+
+main();
