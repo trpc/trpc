4c3096157531085b9f54cf7de8ee55d9f8dcac38
jgoux <hi@jgoux.dev>
2022-07-13 15:30:22 +0200

    fix types

--- packages/server/src/internals.ts
+++ packages/server/src/internals.ts
@@ -5,10 +5,10 @@ export type { DefaultErrorShape, ErrorFormatter } from './error/formatter';
 export type { mergeRoutersGeneric } from './core/internals/__generated__/mergeRoutersGeneric';
 export type { ProcedureBuilder } from './core/internals/procedureBuilder';
 export type {
-  ValidateShape,
-  EnsureRecord,
   Overwrite,
   unsetMarker,
+  ValidateShape,
+  EnsureRecord,
 } from './core/internals/utils';
 export type { MiddlewareFunction } from './core/middleware';
 export type { Router } from './core/router';
