aa81646eb745295fe5038bd5d3bf74c161e97a2b
jgoux <hi@jgoux.dev>
2022-07-13 16:00:27 +0200

    update types correctly

--- packages/server/src/internals.ts
+++ packages/server/src/internals.ts
@@ -1,14 +1,14 @@
 /**
  * These types have to be exported so users can generate their own types definitions files
  */
-export type { DefaultErrorShape, ErrorFormatter } from './error/formatter';
+export type { DefaultErrorShape } from './error/formatter';
 export type { mergeRoutersGeneric } from './core/internals/__generated__/mergeRoutersGeneric';
 export type { ProcedureBuilder } from './core/internals/procedureBuilder';
-export type {
-  Overwrite,
-  unsetMarker,
-  ValidateShape,
-  EnsureRecord,
-} from './core/internals/utils';
+export type { Overwrite, unsetMarker } from './core/internals/utils';
 export type { MiddlewareFunction } from './core/middleware';
-export type { Router } from './core/router';
+export type { Router, RouterDef } from './core/router';
+export type {
+  MutationProcedure,
+  QueryProcedure,
+  SubscriptionProcedure,
+} from './core/procedure';
