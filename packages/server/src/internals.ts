eb9ff08ed969d0e7b34dc3e0c263a0426032a539
Sachin Raja <sachinraja2349@gmail.com>
2022-07-13 08:05:52 -0700

    nevermind it's generated

--- packages/server/src/internals.ts
+++ packages/server/src/internals.ts
@@ -2,6 +2,7 @@
  * These types have to be exported so users can generate their own types definitions files
  */
 export type { DefaultErrorShape } from './error/formatter';
+export type { mergeRoutersGeneric } from './core/internals/__generated__/mergeRoutersGeneric';
 export type { ProcedureBuilder } from './core/internals/procedureBuilder';
 export type { Overwrite, unsetMarker } from './core/internals/utils';
 export type { MiddlewareFunction } from './core/middleware';
