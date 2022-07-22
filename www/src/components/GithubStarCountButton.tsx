082f5da36f71b48458d2bc823a74729cdf9059cd
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-07-22 21:34:32 +0300

    refactor(www): GithubStarCountButton styling adjustments

--- www/src/components/GithubStarCountButton.tsx
+++ www/src/components/GithubStarCountButton.tsx
@@ -10,9 +10,6 @@ export const GithubStarCountButton = () => {
   const [starCount, setStarCount] = React.useState(starCountStart);
 
   React.useEffect(() => {
-    // setTimeout(() => {
-    //   setStarCount(10_000);
-    // }, 1000);
     void fetch('https://api.github.com/repos/tRPC/tRPC')
       .then((res) => res.json())
       .then((data) => {
@@ -28,22 +25,21 @@ export const GithubStarCountButton = () => {
       href="https://github.com/trpc/trpc/stargazers"
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center gap-2 px-4 text-sm font-bold text-black transition rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300"
+      className="flex items-center gap-[5px] px-4 text-sm font-bold text-black transition rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300"
     >
-      <div className="flex gap-2 items-center py-2">
+      <div className="flex items-center gap-2">
         <FiStar strokeWidth={3} /> Star
       </div>
 
       <div
         className={`transition-all duration-1000 overflow-hidden font-mono ${
-          starCount ? 'w-14' : 'w-0'
+          starCount ? 'w-9 md:w-11' : 'w-0'
         }`}
       >
         <BrowserOnly>
           {() => (
             <Suspense fallback={null}>
               <AnimatedNumbers
-                includeComma
                 animateToNumber={starCount}
                 fontStyle={{ paddingTop: '0.5rem', paddingBottom: '0.5rem' }}
                 configs={[{ mass: 1, tension: 220, friction: 100 }]}
