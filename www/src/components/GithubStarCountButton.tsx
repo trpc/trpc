249ebc7de6a9b239b34afe706db62ce14fd985c4
KATT <alexander@n1s.se>
2022-07-22 10:04:49 +0200

    fix

--- www/src/components/GithubStarCountButton.tsx
+++ www/src/components/GithubStarCountButton.tsx
@@ -10,6 +10,9 @@ export const GithubStarCountButton = () => {
   );
 
   React.useEffect(() => {
+    // setTimeout(() => {
+    //   setStarCount(10000);
+    // }, 1000);
     void fetch('https://api.github.com/repos/tRPC/tRPC')
       .then((res) => res.json())
       .then((data) => {
@@ -24,21 +27,26 @@ export const GithubStarCountButton = () => {
       href="https://github.com/trpc/trpc/stargazers"
       target="_blank"
       rel="noopener noreferrer"
-      className={`flex items-center gap-2 px-4 text-sm font-bold text-black transition-all duration-1000 rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300 overflow-hidden ${
-        starCount ? 'w-36' : 'w-24'
-      }`}
+      className="flex items-center gap-2 px-4 text-sm font-bold text-black transition rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300"
     >
       <div className="flex gap-2 items-center py-2">
         <FiStar strokeWidth={3} /> Star
       </div>
-      {starCount && (
-        <AnimatedNumbers
-          includeComma
-          animateToNumber={starCount}
-          fontStyle={{ paddingTop: '0.5rem', paddingBottom: '0.5rem' }}
-          configs={[{ mass: 1, tension: 220, friction: 100 }]}
-        ></AnimatedNumbers>
-      )}
+
+      <div
+        className={`transition-all overflow-hidden font-mono ${
+          starCount ? 'w-12' : 'w-0'
+        }`}
+      >
+        {starCount && (
+          <AnimatedNumbers
+            includeComma
+            animateToNumber={starCount}
+            fontStyle={{ paddingTop: '0.5rem', paddingBottom: '0.5rem' }}
+            configs={[{ mass: 1, tension: 220, friction: 100 }]}
+          ></AnimatedNumbers>
+        )}
+      </div>
     </a>
   );
 };
