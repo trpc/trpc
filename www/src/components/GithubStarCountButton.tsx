b7493854445b7dd5fd6fd495429f7749975d4ba9
KATT <alexander@n1s.se>
2022-07-22 09:35:54 +0200

    attempt to get it to not jerk

--- www/src/components/GithubStarCountButton.tsx
+++ www/src/components/GithubStarCountButton.tsx
@@ -3,7 +3,7 @@ import AnimatedNumbers from 'react-animated-numbers';
 import { FiStar } from 'react-icons/fi';
 
 export const GithubStarCountButton = () => {
-  const [starCount, setStarCount] = React.useState(0);
+  const [starCount, setStarCount] = React.useState<number | null>(null);
 
   React.useEffect(() => {
     void fetch('https://api.github.com/repos/tRPC/tRPC')
@@ -16,17 +16,23 @@ export const GithubStarCountButton = () => {
       href="https://github.com/trpc/trpc/stargazers"
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center gap-2 px-4 text-sm font-bold text-black transition-colors rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300"
+      className="flex items-center gap-2 px-4 text-sm font-bold text-black transition-colors rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300 "
     >
       <div className="flex gap-2 items-center py-2">
         <FiStar strokeWidth={3} /> Star
       </div>
-      <AnimatedNumbers
-        includeComma
-        animateToNumber={starCount}
-        fontStyle={{ paddingTop: '0.5rem', paddingBottom: '0.5rem' }}
-        configs={[{ mass: 1, tension: 220, friction: 100 }]}
-      ></AnimatedNumbers>
+      <span className="font-mono">
+        {starCount ? (
+          <AnimatedNumbers
+            includeComma
+            animateToNumber={starCount}
+            fontStyle={{ paddingTop: '0.5rem', paddingBottom: '0.5rem' }}
+            configs={[{ mass: 1, tension: 220, friction: 100 }]}
+          ></AnimatedNumbers>
+        ) : (
+          <span className="py-2">0,000</span>
+        )}
+      </span>
     </a>
   );
 };
