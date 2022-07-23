9d566676bb3a8fe3da5f6601555292b00e7306b4
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-07-23 09:19:10 +0100

    feat(www): implements description section (#2294)
    Co-authored-by: KATT <alexander@n1s.se>

--- www/src/components/SectionTitle.tsx
+++ www/src/components/SectionTitle.tsx
@@ -1,17 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 
 type SectionTitleProps = {
-  readonly title: string;
-  readonly description: string;
+  readonly title: ReactNode;
+  readonly description?: ReactNode;
 };
 
 export const SectionTitle: FC<SectionTitleProps> = ({ title, description }) => {
   return (
     <div className="text-center">
       <h2 className="text-3xl font-bold">{title}</h2>
-      <p className="text-gray-600 dark:text-gray-400 max-w-[60ch] mx-auto">
-        {description}
-      </p>
+      {description && (
+        <p className="text-gray-600 dark:text-gray-400 max-w-[60ch] mx-auto">
+          {description}
+        </p>
+      )}
     </div>
   );
 };
