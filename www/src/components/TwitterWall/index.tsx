5848ff138dfc97f2a2c16a7ef23039ea40f0a8d7
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-27 16:47:33 +0200

    fix profile hover

--- www/src/components/TwitterWall/index.tsx
+++ www/src/components/TwitterWall/index.tsx
@@ -3,7 +3,7 @@ import { tweets } from './tweets';
 
 export const TwitterWall = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 mt-6 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 mt-6 overflow-y-scroll lg:grid-cols-3 max-h-[80vh]">
       {tweets.map((column, index) => (
         <div key={`column-${index}`} className="flex flex-col gap-4">
           {column.map((tweet) => (
@@ -16,11 +16,11 @@ export const TwitterWall = () => {
                 key={tweet.id}
                 className="p-6 rounded-lg dark:bg-zinc-800 bg-zinc-100 hover:bg-zinc-200 hover:dark:bg-zinc-900"
               >
-                <a
-                  href={`https://twitter.com/${tweet.handle}`}
-                  className="cursor-pointer"
-                >
-                  <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between">
+                  <a
+                    href={`https://twitter.com/${tweet.handle}`}
+                    className="cursor-pointer"
+                  >
                     <div className="flex items-center gap-3 group">
                       <img
                         src={tweet.avatar}
@@ -34,12 +34,12 @@ export const TwitterWall = () => {
                         </p>
                       </div>
                     </div>
-
-                    <div className="text-sm text-zinc-600 dark:text-zinc-400">
-                      <p>{tweet.date}</p>
-                    </div>
+                  </a>
+                  <div className="text-sm text-zinc-600 dark:text-zinc-400">
+                    <p>{tweet.date}</p>
                   </div>
-                </a>
+                </div>
+
                 <p className="pt-4 whitespace-pre-wrap text-zinc-600 dark:text-zinc-400">
                   {tweet.content}
                 </p>
