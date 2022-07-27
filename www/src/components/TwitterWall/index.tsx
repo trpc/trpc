fc5708d2e4e3c904ff9ce1b1f40e14e1d30ae342
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-27 11:56:25 +0200

    add links to profile and tweet

--- www/src/components/TwitterWall/index.tsx
+++ www/src/components/TwitterWall/index.tsx
@@ -5,32 +5,45 @@ export const TwitterWall = () => {
   return (
     <div className="grid grid-cols-1 gap-4 mt-6 lg:grid-cols-3">
       {tweets.map((column, index) => (
-        <div key={`column-${index}`} className="flex flex-col gap-4">
+        <div key={`column-${index}`} className="flex flex-col gap-4 group">
           {column.map((tweet) => (
-            <div
+            <a
+              href={`https://twitter.com/${tweet.handle}/status/${tweet.id}`}
               key={tweet.id}
-              className="p-6 rounded-lg dark:bg-zinc-800 bg-zinc-100"
             >
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-3">
-                  <img
-                    src={tweet.avatar}
-                    alt={tweet.name}
-                    className="w-12 h-12 rounded-full"
-                  />
-                  <div>
-                    <p className="text-base font-bold">{tweet.name}</p>
-                    <p className="text-sm text-zinc-600 dark:text-zinc-400">
-                      {tweet.handle}
-                    </p>
+              <div
+                key={tweet.id}
+                className="p-6 rounded-lg dark:bg-zinc-800 bg-zinc-100 hover:bg-zinc-200 hover:dark:bg-zinc-900"
+              >
+                <a
+                  href={`https://twitter.com/${tweet.handle}`}
+                  className="cursor-pointer"
+                >
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-3">
+                      <img
+                        src={tweet.avatar}
+                        alt={tweet.name}
+                        className="w-12 h-12 rounded-full"
+                      />
+                      <div>
+                        <p className="text-base font-bold">{tweet.name}</p>
+                        <p className="text-sm text-zinc-600 dark:text-zinc-400">
+                          {tweet.handle}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="text-sm text-zinc-600 dark:text-zinc-400">
+                      <p>{tweet.date}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="text-sm text-zinc-600 dark:text-zinc-400">
-                  <p>{tweet.date}</p>
-                </div>
+                </a>
+                <p className="pt-4 whitespace-pre-wrap text-zinc-600 dark:text-zinc-400">
+                  {tweet.content}
+                </p>
               </div>
-              <p className="pt-4 whitespace-pre-wrap">{tweet.content}</p>
-            </div>
+            </a>
           ))}
         </div>
       ))}
