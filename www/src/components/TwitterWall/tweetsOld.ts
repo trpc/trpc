54371f8f7f45a1b86188cb5d5c60ffda33e7ce56
Julius Marminge <julius0216@outlook.com>
2022-07-29 19:02:41 +0200

    feat(www): tweak twitterwall and link stuff (#2331)

---
+++ www/src/components/TwitterWall/tweetsOld.ts
@@ -0,0 +1,100 @@
+// backup of old tweets.ts if you like to keep that
+
+export const tweets = [
+  [
+    {
+      id: '1549812970594545664',
+      name: 'Theo - ping.gg',
+      handle: '@t3dotgg',
+      date: 'Jul 20',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1475643465069301763/FUR05HHs_400x400.jpg',
+      content:
+        'My new favorite thing is introducing startups to tRPC and waiting two weeks to hear back from them about how it changed everything',
+    },
+    {
+      id: '1488149257383198725',
+      name: 'Sunny Singh',
+      handle: '@sunnysinghio',
+      date: 'Jan 31',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1544368488260804608/O4sYXEwj_400x400.jpg',
+      content:
+        'I am extremely impressed with @trpcio. It provides a great experience for developing full stack apps with @nextjs, and notable projects like @calcom are using it.',
+    },
+    {
+      id: '1422111411422040068',
+      name: 'Peter Cilliers-Pistorius 🇺🇦',
+      handle: '@appfactory',
+      date: 'Aug 2',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1384539886980681738/XIT9pYbY_400x400.jpg',
+      content:
+        "I am massively impressed by @alexdotjs's http://trpc.io:\n\n end-to-end type-safety is awesome, but so is the simplicity of the architecture: It fits so nicely in my brain! And It uses GET requests for the queries!\n\n I'm using it in production on @_snaplet, an am very happy",
+    },
+  ],
+  [
+    {
+      id: '1549140656752234496',
+      name: 'Minko Gechev',
+      handle: '@mgechev',
+      date: 'Jul 19',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1541061664098947073/kUH07uES_400x400.jpg',
+      content:
+        "tRPC - Safe API between your client and server 🔥\n\nI LOVE this project!\n\nIf you're building your client and server with TypeScript, tRPC allows you to have static verification of your communication protocol with zero overhead on the client.",
+    },
+    {
+      id: '1459572282116902915',
+      name: 'Nick Schrock',
+      handle: '@schrockn',
+      date: 'Nov 13',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1452768656711032833/Vq8wBRJc_400x400.jpg',
+      content:
+        "https://trpc.io Looks super nice. Great lightweight alternative to @GraphQL when you don't need the full power of the language and just to build endpoints. Vertical integration of server- and client-side TS also really slick.",
+    },
+    {
+      id: '1444402124901781506',
+      name: 'Lori Karikari',
+      handle: '@LoriKarikari',
+      date: 'Oct 2',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1524849753921667072/7GnfNHtU_400x400.jpg',
+      content:
+        'Removed the GraphQL API in a project and replaced it with http://trpc.io. End-to-end type safety across the desktop, mobile, and web apps without all the bloat and complexity 🔥 A super nice alternative for adding some Blitz-like DX to your existing apps 😍',
+    },
+  ],
+  [
+    {
+      id: '1508383436863541250',
+      name: 'Tobias Lins',
+      handle: '@linstobias',
+      date: 'Mar 28',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1361293861813956608/FBJWYoXx_400x400.jpg',
+      content:
+        "I just tried out http://trpc.io for the first time and I'm impressed. Sharing types between my backend and frontend has never been so easy.",
+    },
+    {
+      id: '1457100617818779656',
+      name: 'Ryan Edge',
+      handle: '@chimon1984',
+      date: 'Nov 7',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1397570977526165510/GYG3e634_400x400.jpg',
+      content:
+        "I have a strong affinity for technologies that don't rely heavily on code generation. In that regard, TRPC is the only library that I have used that allows you to build a Full Stack API without it (albeit only with JavaScript).",
+    },
+    {
+      id: '1403008330641989637',
+      name: 'anthonyshort',
+      handle: '@anthonyshort',
+      date: 'Jun 10',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1186549365332033537/15YLBZQt_400x400.jpg',
+      content:
+        "trpc is most impressive library I've seen for Typescript+Next.js in a long time https://trpc.io\n\nIf your app uses API routes between the UI and external APIs this is a super simple, clean way to handle it. No build steps.\n\nI really hope this gains traction.",
+    },
+  ],
+];
