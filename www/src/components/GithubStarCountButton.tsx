c4f151d2fd6a1fd333da49cfdb1feac9a16d5ac7
KATT <alexander@n1s.se>
2022-07-29 18:39:23 +0200

    tweaks

--- www/src/components/GithubStarCountButton.tsx
+++ www/src/components/GithubStarCountButton.tsx
@@ -1,7 +1,5 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { FiStar } from 'react-icons/fi';
-
-const AnimatedNumbers = React.lazy(() => import('react-animated-numbers'));
 
 let starCountStart = 0;
 
@@ -19,34 +17,28 @@ export const GithubStarCountButton = () => {
       });
   }, []);
 
+  const starCountStr = starCount
+    ? new Intl.NumberFormat().format(starCount)
+    : null;
+
   return (
     <a
       href="https://github.com/trpc/trpc/stargazers"
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center gap-[5px] px-4 text-sm font-bold text-black transition rounded-lg md:text-base hover:no-underline hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300 hover:from-cyan-300 hover:via-cyan-300 hover:to-cyan-300"
+      className="flex items-center gap-[5px] px-4 text-sm font-bold text-black transition rounded-lg md:text-base hover:no-underline py-2 hover:text-black bg-gradient-to-r from-cyan-100 via-cyan-200 to-cyan-300 hover:from-cyan-300 hover:via-cyan-300 hover:to-cyan-300"
     >
       <div className="flex items-center gap-2">
         <FiStar strokeWidth={3} /> Star
       </div>
 
       <div
-        className={`transition-all duration-1000 overflow-hidden font-mono ${
-          starCount ? 'w-12 md:w-14' : 'w-0'
+        className={`transition-all duration-1000 inline-block overflow-hidden font-mono whitespace-nowrap ${
+          starCount ? 'w-[58px] opacity-100' : 'w-0 opacity-0'
         }`}
       >
-        {/* This little thing is an awful hack and any OSS-contributor is welcome to come up / implement another idea for how we deal with loading state of the stars */}
-        <Suspense fallback={null}>
-          <AnimatedNumbers
-            //includeComma={true} // <-- buggy on some i18n systems. See https://github.com/heyman333/react-animated-numbers/issues/34
-            animateToNumber={starCount}
-            fontStyle={{ paddingTop: '0.5rem', paddingBottom: '0.5rem' }}
-            configs={[{ mass: 1, tension: 220, friction: 100 }]}
-          />
-        </Suspense>
+        {starCount ? new Intl.NumberFormat().format(starCount) : null}
       </div>
-      {/* Fix height jank */}
-      <span className="py-2">&nbsp;</span>
     </a>
   );
 };
