e430d807b52aedbe17ad8a462de091a4e51dc664
Julius Marminge <julius0216@outlook.com>
2022-08-02 21:34:03 +0200

    examples: Add a more minimal express example (#2345)

---
+++ examples/express-minimal/src/router.ts
@@ -0,0 +1,18 @@
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
+
+const t = initTRPC()();
+
+const helloRouter = t.router({
+  greeting: t.procedure
+    .input(z.object({ name: z.string() }).nullish())
+    .query(({ input }) => {
+      return `Hello ${input?.name ?? 'World'}`;
+    }),
+});
+
+export const appRouter = t.router({
+  hello: helloRouter,
+});
+
+export type AppRouter = typeof appRouter;
