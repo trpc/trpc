4d6a74155429a99710352b777d25fe23b34d48c1
Alex Johansson <alexander@n1s.se>
2022-08-05 15:42:13 +0200

    tweak sponsors (#2372)

--- www/src/components/sponsors/script.pull.ts
+++ www/src/components/sponsors/script.pull.ts
@@ -13,11 +13,6 @@ const graphqlWithAuth = graphql.defaults({
   },
 });
 
-const MONTH_AS_SECONDS = 30 * 24 * 60 * 60;
-function getMultiplier(since: number) {
-  return (Date.now() - since) / 1000 / MONTH_AS_SECONDS;
-}
-
 function ensureHttp(url: string) {
   if (url.startsWith('http')) {
     return url;
@@ -34,7 +29,6 @@ function flattenSponsor(node: Node) {
     monthlyPriceInDollars: node.tier.monthlyPriceInDollars,
     link,
     privacyLevel: node.privacyLevel,
-    multiplier: 1 + getMultiplier(Date.parse(node.createdAt)),
     login: node.sponsorEntity.login,
     createdAt: Date.parse(node.createdAt),
   };
@@ -115,8 +109,10 @@ async function main() {
 
   const text = [
     '// prettier-ignore',
+    '// eslint-disable',
     '',
     `export const sponsors = ${json} as const`,
+    '',
   ].join('\n');
 
   fs.writeFileSync(__dirname + '/script.output.ts', text);
