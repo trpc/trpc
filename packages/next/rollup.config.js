4aa73c2467e1f1c9fb13e22291564214fdaf5367
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-06 14:14:05 +0200

    single rollup config

--- packages/next/rollup.config.js
+++
@@ -1,7 +0,0 @@
-import { getRollupConfig } from '../../scripts/rollup';
-
-const config = getRollupConfig({
-  input: ['src/index.ts'],
-});
-
-export default config;
---
+++ rollup.config.ts
@@ -0,0 +1,158 @@
+import { babel } from '@rollup/plugin-babel';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import path from 'path';
+import { RollupOptions, defineConfig } from 'rollup';
+import del from 'rollup-plugin-delete';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - no typedefs exist for this plugin
+import multiInput from 'rollup-plugin-multi-input';
+import externals from 'rollup-plugin-node-externals';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+const isWatchMode = process.argv.includes('--watch');
+const isProd = process.env.NODE_ENV === 'production';
+const extensions = ['.ts', '.tsx'];
+
+const getMultiInputPlugin = (packageDir: string) =>
+  multiInput({ relative: path.join(packageDir, 'src/') });
+
+const clientInput = [
+  'src/index.ts',
+  'src/links/httpLink.ts',
+  'src/links/httpBatchLink.ts',
+  'src/links/splitLink.ts',
+  'src/links/loggerLink.ts',
+  'src/links/wsLink.ts',
+];
+const nextInput = ['src/index.ts'];
+const reactInput = ['src/index.ts', 'src/ssg.ts'];
+const serverInput = [
+  'src/index.ts',
+  'src/adapters/aws-lambda/index.ts',
+  'src/adapters/express.ts',
+  'src/adapters/fastify/index.ts',
+  'src/adapters/next.ts',
+  'src/adapters/node-http/index.ts',
+  'src/adapters/standalone.ts',
+  'src/adapters/ws.ts',
+  'src/adapters/fetch/index.ts',
+  'src/rpc/index.ts',
+  'src/observable/index.ts',
+  'src/subscription.ts',
+  // Utils that can be shared with clients
+  'src/shared/index.ts',
+];
+
+export default function rollup(): RollupOptions[] {
+  return defineConfig([
+    ...buildConfig({
+      input: serverInput,
+      packageDir: 'packages/server',
+    }),
+    ...buildConfig({
+      input: clientInput,
+      packageDir: 'packages/client',
+    }),
+    ...buildConfig({
+      input: reactInput,
+      packageDir: 'packages/react',
+    }),
+    ...buildConfig({
+      input: nextInput,
+      packageDir: 'packages/next',
+    }),
+  ]);
+}
+
+type Options = {
+  input: string[];
+  packageDir: string;
+};
+
+function buildConfig({ input, packageDir }: Options): RollupOptions[] {
+  const resolvedInput = input.map((file) => path.resolve(packageDir, file));
+  const options: Options = {
+    input: resolvedInput,
+    packageDir,
+  };
+
+  return [types(options), esm(options), cjs(options)];
+}
+
+function types({ input, packageDir }: Options): RollupOptions {
+  return {
+    input,
+    output: {
+      dir: `${packageDir}/dist`,
+      entryFileNames: '[name].ts.js',
+    },
+    plugins: [
+      !isWatchMode &&
+        del({
+          targets: ['dist'],
+        }),
+      getMultiInputPlugin(packageDir),
+      typescript({
+        tsconfig: path.resolve(packageDir, 'tsconfig.build.json'),
+        abortOnError: !isWatchMode,
+        exclude: ['**/*.test.ts', '**/*.test.tsx'],
+      }),
+    ],
+  };
+}
+
+function cjs({ input, packageDir }: Options): RollupOptions {
+  return {
+    input,
+    output: {
+      dir: `${packageDir}/dist`,
+      format: 'cjs',
+      entryFileNames: '[name].js',
+      plugins: [isProd && terser({ module: false })],
+    },
+    plugins: [
+      getMultiInputPlugin(packageDir),
+      externals({
+        deps: true,
+        packagePath: path.resolve(packageDir, 'package.json'),
+      }),
+      nodeResolve({
+        extensions,
+      }),
+      babel({
+        babelHelpers: 'runtime',
+        configFile: path.join(__dirname, 'babel.config.js'),
+        extensions,
+      }),
+    ],
+  };
+}
+
+function esm({ input, packageDir }: Options): RollupOptions {
+  return {
+    input,
+    output: {
+      dir: `${packageDir}/dist`,
+      format: 'esm',
+      entryFileNames: '[name].mjs',
+      chunkFileNames: '[name]-[hash].mjs',
+      plugins: [isProd && terser({ module: true })],
+    },
+    plugins: [
+      getMultiInputPlugin(packageDir),
+      externals({
+        deps: true,
+        packagePath: path.resolve(packageDir, 'package.json'),
+      }),
+      nodeResolve({
+        extensions,
+      }),
+      babel({
+        babelHelpers: 'runtime',
+        configFile: path.join(__dirname, 'babel.config.js'),
+        extensions,
+      }),
+    ],
+  };
+}
