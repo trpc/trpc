64149ae16c4409226f4f9e49cbc71d1e88921890
KATT <alexander@n1s.se>
2022-08-08 18:40:30 +0200

    tweaks

--- examples/next-minimal-starter/.eslintrc.js
+++ examples/next-minimal-starter/.eslintrc.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // FIXME: This should not be necessary and it is probably some underlying
-  // issue somewhere in the monorepo that is causing it.
   parserOptions: { project: ['./tsconfig.json'] },
   extends: ['next'],
 };
