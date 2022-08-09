fd5f6f454d614d1a3ecc0888e02640d0cefb3bdb
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-09 13:25:12 +0200

    add clean commands and centralize jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  rootDir: '../',
+  rootDir: '.',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   collectCoverageFrom: [
     './*/src/**/*.{ts,tsx,js,jsx}',
