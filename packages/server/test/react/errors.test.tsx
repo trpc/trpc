d18eb11d21bd3bf98a75ed3e58085ff093039624
KATT <alexander@n1s.se>
2022-08-10 13:39:09 +0200

    add another failing test

--- packages/server/test/react/errors.test.tsx
+++ packages/server/test/react/errors.test.tsx
@@ -107,3 +107,91 @@ describe('custom error formatter', () => {
     `);
   });
 });
+
+describe('no custom formatter', () => {
+  const ctx = konn()
+    .beforeEach(() => {
+      const t = initTRPC()();
+      const appRouter = t.router({
+        post: t.router({
+          byId: t.procedure
+            .input(
+              z.object({
+                // Minimum post id 1
+                id: z.number().min(1),
+              }),
+            )
+            .query(() => {
+              return '__result' as const;
+            }),
+        }),
+        /**
+         * @deprecated
+         */
+        deprecatedRouter: t.router({
+          /**
+           * @deprecated
+           */
+          deprecatedProcedure: t.procedure.query(() => '..'),
+        }),
+      });
+
+      return getServerAndReactClient(appRouter);
+    })
+    .afterEach(async (ctx) => {
+      await ctx?.close?.();
+    })
+    .done();
+
+  test('query that fails', async () => {
+    const { proxy, App, appRouter } = ctx;
+    const queryErrorCallback = jest.fn();
+    function MyComponent() {
+      const query1 = proxy.post.byId.useQuery({
+        id: 0,
+      });
+
+      if (query1.error) {
+        expectTypeOf(query1.error).toMatchTypeOf<
+          TRPCClientErrorLike<typeof appRouter>
+        >();
+        queryErrorCallback(query1.error);
+        return (
+          <>
+            __ERROR_RESULT__:
+            <pre>{JSON.stringify(query1.error, null, 4)}</pre>
+          </>
+        );
+      }
+
+      return <>....</>;
+    }
+
+    render(
+      <App>
+        <MyComponent />
+      </App>,
+    );
+    await waitFor(() => {
+      expect(queryErrorCallback).toHaveBeenCalled();
+    });
+
+    const errorDataResult = queryErrorCallback.mock.calls[0][0];
+
+    expect(errorDataResult).toBeInstanceOf(TRPCClientError);
+    expect(errorDataResult).toMatchInlineSnapshot(`
+      [TRPCClientError: [
+        {
+          "code": "too_small",
+          "minimum": 1,
+          "type": "number",
+          "inclusive": true,
+          "message": "Number must be greater than or equal to 1",
+          "path": [
+            "id"
+          ]
+        }
+      ]]
+    `);
+  });
+});
