65912ac93e19183309f397651a7a782f9c8bdb5c
KATT <alexander@n1s.se>
2022-08-14 15:19:39 +0200

    add mutation test

--- packages/server/test/react/trpc-options.test.tsx
+++ packages/server/test/react/trpc-options.test.tsx
@@ -1,7 +1,7 @@
 import { getServerAndReactClient } from './__reactHelpers';
 import { render, waitFor } from '@testing-library/react';
 import { konn } from 'konn';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { z } from 'zod';
 import { initTRPC } from '../../src';
 
@@ -16,6 +16,13 @@ const ctx = konn()
           }),
         )
         .query(() => '__result' as const),
+      doSomething: t.procedure
+        .input(
+          z.object({
+            id: z.string(),
+          }),
+        )
+        .mutation(() => '__result' as const),
     });
 
     return getServerAndReactClient(appRouter);
@@ -66,6 +73,55 @@ test('useQuery()', async () => {
       },
       "path": "greeting",
       "type": "query",
+    }
+  `);
+});
+
+test('useMutation()', async () => {
+  const { proxy, App } = ctx;
+  function MyComponent() {
+    const doSomethingMutation = proxy.doSomething.useMutation({
+      trpc: {
+        context: {
+          foo: 'bar',
+        },
+      },
+    });
+
+    useEffect(() => {
+      doSomethingMutation.mutate({
+        id: '1',
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+      <pre>{JSON.stringify(doSomethingMutation.data ?? 'n/a', null, 4)}</pre>
+    );
+  }
+
+  const utils = render(
+    <App>
+      <MyComponent />
+    </App>,
+  );
+  await waitFor(() => {
+    expect(utils.container).toHaveTextContent(`__result`);
+  });
+  expect(ctx.spyLink).toHaveBeenCalledTimes(1);
+  const firstCall = ctx.spyLink.mock.calls[0]![0]!;
+  expect(firstCall.context.foo).toBe('bar');
+  expect(firstCall).toMatchInlineSnapshot(`
+    Object {
+      "context": Object {
+        "foo": "bar",
+      },
+      "id": 1,
+      "input": Object {
+        "id": "1",
+      },
+      "path": "doSomething",
+      "type": "mutation",
     }
   `);
 });
