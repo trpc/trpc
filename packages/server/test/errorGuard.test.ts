17c835e51d9ba874527f1d8314f0c10ab4ea0b13
KATT <alexander@n1s.se>
2022-08-16 12:15:47 +0200

    fix error typing

--- packages/server/test/errorGuard.test.ts
+++ packages/server/test/errorGuard.test.ts
@@ -3,6 +3,7 @@ import { TRPCClientError } from '@trpc/client';
 import { expectTypeOf } from 'expect-type';
 import { konn } from 'konn';
 import { DefaultErrorShape, initTRPC } from '../src';
+import { DefaultErrorData } from '../src/error/formatter';
 
 describe('no custom error formatter', () => {
   const t = initTRPC()();
@@ -48,57 +49,13 @@ describe('no custom error formatter', () => {
       throw new Error('Bad');
     }
     expectTypeOf(err.data).not.toBeAny();
-    expectTypeOf(err.data!).toMatchTypeOf<DefaultErrorShape>();
+    expectTypeOf(err.shape).not.toBeAny();
+    expectTypeOf(err.data!).toMatchTypeOf<DefaultErrorData>();
+    expectTypeOf(err.shape!).toMatchTypeOf<DefaultErrorShape>();
   });
 });
 
 describe('with custom error formatter', () => {
-  const t = initTRPC()();
-
-  const appRouter = t.router({
-    greeting: t.procedure.query(() => {
-      if (Math.random() >= 0) {
-        // always fails
-        throw new Error('Fails');
-      }
-      return 'never';
-    }),
-  });
-  const ctx = konn()
-    .beforeEach(() => {
-      const opts = routerToServerAndClientNew(appRouter);
-
-      return opts;
-    })
-    .afterEach(async (ctx) => {
-      await ctx?.close?.();
-    })
-    .done();
-
-  function isTRPCClientError(
-    cause: unknown,
-  ): cause is TRPCClientError<typeof appRouter> {
-    if (cause instanceof TRPCClientError) {
-      return true;
-    }
-    return false;
-  }
-
-  test('infer errors with type guard', async () => {
-    const err = await waitError(ctx.proxy.greeting.query());
-
-    if (!isTRPCClientError(err)) {
-      throw new Error('Bad');
-    }
-    expectTypeOf(err.data).not.toBeAny();
-    expectTypeOf(err.data!).toMatchTypeOf<DefaultErrorShape>();
-
-    expect(err.data).toMatchInlineSnapshot();
-    expect(err.shape).toMatchInlineSnapshot();
-  });
-});
-
-describe('no custom error formatter', () => {
   const t = initTRPC()({
     errorFormatter({ shape }) {
       return {
@@ -147,8 +104,33 @@ describe('no custom error formatter', () => {
       throw new Error('Bad');
     }
     expectTypeOf(err.data).not.toBeAny();
-    expectTypeOf(err.data!.data.foo).toEqualTypeOf<'bar'>();
-    expect(err.data).toMatchInlineSnapshot();
-    expect(err.shape).toMatchInlineSnapshot();
+    expectTypeOf(err.shape).not.toBeAny();
+    expectTypeOf(err.data!).toMatchTypeOf<DefaultErrorData>();
+    expectTypeOf(err.shape!).toMatchTypeOf<DefaultErrorShape>();
+
+    err.data!.stack = '[redacted]';
+
+    expect(err.data).toMatchInlineSnapshot(`
+      Object {
+        "code": "INTERNAL_SERVER_ERROR",
+        "foo": "bar",
+        "httpStatus": 500,
+        "path": "greeting",
+        "stack": "[redacted]",
+      }
+    `);
+    expect(err.shape).toMatchInlineSnapshot(`
+      Object {
+        "code": -32603,
+        "data": Object {
+          "code": "INTERNAL_SERVER_ERROR",
+          "foo": "bar",
+          "httpStatus": 500,
+          "path": "greeting",
+          "stack": "[redacted]",
+        },
+        "message": "Fails",
+      }
+    `);
   });
 });
