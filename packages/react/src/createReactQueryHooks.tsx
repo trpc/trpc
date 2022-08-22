49b338686089fadd866f6e2f7c9133b87767c104
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-22 15:10:50 +0200

    typeerror when passing signal to react

--- packages/react/src/createReactQueryHooks.tsx
+++ packages/react/src/createReactQueryHooks.tsx
@@ -47,7 +47,9 @@ export type OutputWithCursor<TData, TCursor extends any = any> = {
   data: TData;
 };
 
-export interface TRPCReactRequestOptions extends TRPCRequestOptions {
+export interface TRPCReactRequestOptions
+  // For RQ, we use their internal AbortSignals instead of letting the user pass their own
+  extends Omit<TRPCRequestOptions, 'signal'> {
   /**
    * Opt out of SSR for this query by passing `ssr: false`
    */
