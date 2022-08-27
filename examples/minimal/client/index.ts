70e601d40ce77433b24fe2d5556e6f4c31e6d736
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-08-28 02:26:58 +0300

    roll back to node 17

--- examples/minimal/client/index.ts
+++ examples/minimal/client/index.ts
@@ -1,5 +1,9 @@
 import { createTRPCProxyClient } from '@trpc/client';
+import fetch from 'node-fetch';
 import type { AppRouter } from '../server';
+
+// Only needed in node
+global.fetch = fetch as any;
 
 const client = createTRPCProxyClient<AppRouter>({
   url: 'http://localhost:2022',
