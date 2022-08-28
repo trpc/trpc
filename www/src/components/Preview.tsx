f13f91b07a60d379fbbfbb23fcc85a87227f4549
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-08-28 14:49:13 +0200

    fix(www): moves conditional rendering of gifs to css for reliability

--- www/src/components/Preview.tsx
+++ www/src/components/Preview.tsx
@@ -1,30 +1,21 @@
-import { useColorMode } from '@docusaurus/theme-common';
 import React from 'react';
 
 export const Preview = () => {
-  const { colorMode } = useColorMode();
   return (
     <div className="flex justify-center pt-12">
       <figure>
-        {colorMode === 'dark' && (
-          <img
-            src="/img/preview-dark.gif"
-            alt="Demo"
-            width="550px"
-            className="rounded-md shadow-xl trpcgif trpcgif--v10"
-          />
-        )}
-
-        {/* hack to prevent the light gif from showing on dark mode until dark version loads */}
-        {colorMode === 'light' && (
-          <img
-            src="/img/preview-light.gif"
-            alt="Demo"
-            width="550px"
-            className="rounded-md shadow-xl trpcgif trpcgif--v10"
-          />
-        )}
-
+        <img
+          src="/img/preview-dark.gif"
+          alt="Demo"
+          width="550px"
+          className="rounded-md shadow-xl trpcgif trpcgif--v10-dark"
+        />
+        <img
+          src="/img/preview-light.gif"
+          alt="Demo"
+          width="550px"
+          className="rounded-md shadow-xl trpcgif trpcgif--v10-light"
+        />
         <img
           src="https://storage.googleapis.com/trpc/trpcgif.gif"
           alt="Demo"
