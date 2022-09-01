7257827701eedc1ec5371bc455389ad509a5883c
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-01 15:51:47 +0200

    remove `proxy` from trpc/next

--- packages/next/src/setupNext.tsx
+++ packages/next/src/setupNext.tsx
@@ -1,5 +1,11 @@
-import { createReactQueryHooks, createReactQueryHooksProxy } from '@trpc/react';
+import { createReactQueryHooks } from '@trpc/react';
+import type {
+  CreateReactQueryHooks,
+  DecoratedProcedureRecord,
+  DecoratedProcedureUtilsRecord,
+} from '@trpc/react/shared';
 import { AnyRouter } from '@trpc/server';
+import { createProxy } from '@trpc/server/shared';
 import { NextPageContext } from 'next/types';
 import { WithTRPCNoSSROptions, WithTRPCSSROptions, withTRPC } from './withTRPC';
 
@@ -8,13 +14,76 @@ export function setupTRPC<
   TSSRContext extends NextPageContext = NextPageContext,
 >(opts: WithTRPCNoSSROptions<TRouter> | WithTRPCSSROptions<TRouter>) {
   const hooks = createReactQueryHooks<TRouter, TSSRContext>();
-  const proxy = createReactQueryHooksProxy<TRouter, TSSRContext>(hooks);
 
-  // TODO: maybe set TSSRContext to `never` when using `WithTRPCNoSSROptions`
+  //type SSRContext = typeof opts.ssr extends true ? TSSRContext : never;
   const _withTRPC = withTRPC<TRouter, TSSRContext>(opts);
 
-  return {
-    proxy,
+  const proxy: unknown = new Proxy(
+    () => {
+      // no-op
+    },
+    {
+      get(_obj, name) {
+        switch (name) {
+          case 'useProxyContext':
+            return hooks.useContext;
+          case 'useContext':
+            return hooks.useContext;
+          case 'useInfiniteQuery':
+            return hooks.useInfiniteQuery;
+          case 'useMutation':
+            return hooks.useMutation;
+          case 'useQuery':
+            return hooks.useQuery;
+          case 'useSubscription':
+            return hooks.useSubscription;
+
+          case 'withTRPC':
+            return _withTRPC;
+
+          default:
+            if (typeof name !== 'string') {
+              throw new Error('Invalid name');
+            }
+            return createProxy((opts) => {
+              // FIXME: Code duplication from `createReactQueryHooksProxy`
+              const args = opts.args;
+
+              const pathCopy = [name, ...opts.path];
+
+              // The last arg is for instance `.useMutation` or `.useQuery()`
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              const lastArg = pathCopy.pop()!;
+
+              // The `path` ends up being something like `post.byId`
+              const path = pathCopy.join('.');
+              if (lastArg === 'useMutation') {
+                return (hooks as any)[lastArg](path, ...args);
+              }
+              const [input, ...rest] = args;
+
+              const queryKey = input === undefined ? [path] : [path, input];
+
+              return (hooks as any)[lastArg](queryKey, ...rest);
+            });
+        }
+      },
+    },
+  );
+
+  return proxy as {
+    useProxyContext(): DecoratedProcedureUtilsRecord<TRouter>;
+    useContext(): typeof hooks.useContext;
+    useInfiniteQuery: typeof hooks.useInfiniteQuery;
+    useMutation: typeof hooks.useMutation;
+    useQuery: typeof hooks.useQuery;
+    useSubscription: typeof hooks.useSubscription;
+    withTRPC: typeof _withTRPC;
+  } & DecoratedProcedureRecord<TRouter['_def']['record']>;
+
+  /*return {
+    ...proxy,
+    useProxyContext: proxyHooks.useContext,
     useContext: hooks.useContext,
     useInfiniteQuery: hooks.useInfiniteQuery,
     useMutation: hooks.useMutation,
@@ -22,5 +91,5 @@ export function setupTRPC<
     useSubscription: hooks.useSubscription,
     withTRPC: _withTRPC,
     queries: hooks.queries,
-  };
+  };*/
 }
---
+++ packages/react/src/shared/index.ts
@@ -0,0 +1,5 @@
+export * from '../internals/getQueryKey';
+
+export type { DecoratedProcedureUtilsRecord } from '../internals/utilsProxy';
+export type { CreateReactQueryHooks } from '../createReactQueryHooks';
+export type { DecoratedProcedureRecord } from '../createReactQueryHooksProxy';
