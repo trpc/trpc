730ad2b995f0067898e2bfe8618635e91d41ea43
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-07 14:59:39 +0200

    fix test

--- packages/server/test/createClient.test.ts
+++ packages/server/test/createClient.test.ts
@@ -1,21 +1,23 @@
-import { createTRPCProxyClient } from '@trpc/client';
+import { createTRPCClient } from '../../client';
 import { httpBatchLink } from '../../client/src';
+
+global.fetch = jest.fn();
 
 describe('typedefs on createClient', () => {
   test('ok to pass only links', () => {
-    createTRPCProxyClient({
+    createTRPCClient({
       links: [httpBatchLink({ url: 'foo' })],
     });
   });
 
   test('ok to pass only url', () => {
-    createTRPCProxyClient({
+    createTRPCClient({
       url: 'foo',
     });
   });
 
   test('error if both url and links are passed', () => {
-    createTRPCProxyClient({
+    createTRPCClient({
       links: [httpBatchLink({ url: 'foo' })],
       // @ts-expect-error - can't pass url along with links
       url: 'foo',
