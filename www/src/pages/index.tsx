2b94b8d1876bb07e55001349b68573d9f54414ae
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-09 20:06:12 +0200

    improvements

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -38,6 +38,19 @@ function useLocalStorageVersion() {
 
 function Home() {
   const context = useDocusaurusContext();
+
+  /** initial theme used for stackblitz embed. dont want to reload the embed on theme-toggle so we just load it once */
+  const [initialTheme] = React.useState(() => {
+    // const localStorageTheme = window.localStorage.getItem('theme');
+    // if (localStorageTheme) {
+    //   return localStorageTheme;
+    // }
+    const prefersDark = window.matchMedia(
+      '(prefers-color-scheme: dark)',
+    ).matches;
+    return prefersDark ? 'dark' : 'light';
+  });
+
   const { siteConfig } = context;
 
   const version = useLocalStorageVersion();
@@ -101,13 +114,7 @@ function Home() {
         <section>
           <Features />
         </section>
-        <section>
-          <iframe
-            className="h-[500px] w-full"
-            src="https://stackblitz.com/github/trpc/next-minimal-starter?embed=1&file=src/pages/index.tsx&hideExplorer=1&hideNavigation=1"
-            frameBorder="0"
-          ></iframe>
-        </section>
+
         <section className="max-w-[80ch] px-6 mx-auto md:px-0">
           <SectionTitle title={<>You may not need a traditional API</>} />
           <blockquote
@@ -141,6 +148,34 @@ function Home() {
             </div>
           </a>
         </section>
+
+        <section>
+          <SectionTitle title={<>Try it out for yourself!</>} />
+          <iframe
+            className="h-[648px] w-full py-4"
+            src={`https://stackblitz.com/github/trpc/next-minimal-starter?embed=1&file=src/pages/index.tsx&hideExplorer=1&hideNavigation=1&theme=${initialTheme}`}
+            frameBorder="0"
+          />
+          <a
+            id="sponsor-button"
+            href="https://github.com/trpc/next-minimal-starter/generate"
+            className="h-12 flex items-center gap-4 dark:bg-zinc-800 px-8 py-4 w-max rounded-lg group hover:dark:bg-zinc-900 border-2 dark:border-zinc-900 hover:dark:border-zinc-700 bg-zinc-200 hover:bg-zinc-100"
+          >
+            <svg
+              role="img"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-[#181717] dark:fill-white h-8"
+            >
+              <title>GitHub</title>
+              <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+            </svg>
+            <span className="dark:text-zinc-300 text-zinc-900 no-underline font-semibold mx-auto">
+              Use this template
+            </span>
+          </a>
+        </section>
+
         <section>
           <SectionTitle title="Don't take our word for it!" />
           <TwitterWall />
