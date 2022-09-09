2cb5f7e94b2e4bb29bbe4f4e008284311895f00c
KATT <alexander@n1s.se>
2022-09-09 09:58:40 +0200

    slightly clearer hover

--- www/src/components/Button.tsx
+++ www/src/components/Button.tsx
@@ -21,7 +21,7 @@ export const Button = ({
   const className = clsx(
     'inline-grid appearance-none cursor-pointer font-bold tracking-normal px-4 py-2 gap-1.5 grid-flow-col rounded-lg shadow-xl shadow-sky-500/20 no-underline hover:no-underline justify-center items-center transition-all duration-300 ',
     {
-      ['bg-primary text-white hover:text-white hover:bg-sky-600']: primary,
+      ['bg-primary text-white hover:text-white hover:bg-sky-700']: primary,
       ['bg-gradient-to-r from-sky-50 to-sky-200 text-slate-800 hover:text-primary']:
         secondary,
     },
