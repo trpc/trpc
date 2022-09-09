c9ee68762635f061bd031bc46554a62c5532f2c0
KATT <alexander@n1s.se>
2022-09-10 00:24:14 +0200

    tweak tweak

--- www/src/components/Button.tsx
+++ www/src/components/Button.tsx
@@ -19,7 +19,7 @@ export const Button = ({
   ...props
 }: Props) => {
   const className = clsx(
-    'inline-grid appearance-none cursor-pointer text-sm sm:text-base font-bold tracking-normal px-3 py-2 sm:px-4 sm:py-2 gap-1.5 sm:gap-1.5 grid-flow-col rounded-lg shadow-xl shadow-sky-500/20 no-underline hover:no-underline justify-center items-center transition-all duration-300 ',
+    'inline-grid appearance-none cursor-pointer text-sm sm:text-base font-bold tracking-normal px-4 py-2 gap-1.5 sm:gap-1.5 grid-flow-col rounded-lg shadow-xl shadow-sky-500/20 no-underline hover:no-underline justify-center items-center transition-all duration-300 ',
     {
       ['bg-primary text-white hover:text-white hover:bg-sky-700']: primary,
       ['bg-gradient-to-r from-sky-50 to-sky-200 text-slate-800 hover:text-primary-darker']:
