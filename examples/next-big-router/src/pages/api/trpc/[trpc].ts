1d730c8d588cbd674f27a5356d5018847b33d25f
KATT <alexander@n1s.se>
2022-09-17 18:41:37 +0200

    fix

--- examples/next-big-router/src/pages/api/trpc/[trpc].ts
+++ examples/next-big-router/src/pages/api/trpc/[trpc].ts
@@ -2,8 +2,8 @@
  * This is the API-handler of your app that contains all your API routes.
  * On a bigger app, you will probably want to split this file up into multiple files.
  */
-import { appRouter } from '../../../__generated__/_app';
 import * as trpcNext from '@trpc/server/adapters/next';
+import { appRouter } from '~/server/routers/_app';
 
 // export only the type definition of the API
 // None of the actual implementation is exposed to the client
