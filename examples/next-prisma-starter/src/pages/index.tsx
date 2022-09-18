e46d6bda9a8e0c0b06537ce26b745b2cd794660c
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-18 12:32:22 +0200

    fix link

--- examples/next-prisma-starter/src/pages/index.tsx
+++ examples/next-prisma-starter/src/pages/index.tsx
@@ -37,9 +37,8 @@ const IndexPage: NextPageWithLayout = () => {
     <>
       <h1>Welcome to your tRPC starter!</h1>
       <p>
-        Check <a href="https://trpc.io/docs">the docs</a> whenever you get
-        stuck, or ping <a href="https://twitter.com/alexdotjs">@alexdotjs</a> on
-        Twitter.
+        Check <a href="https://trpc.io/">the docs</a> whenever you get stuck, or
+        ping <a href="https://twitter.com/alexdotjs">@alexdotjs</a> on Twitter.
       </p>
 
       <h2>
