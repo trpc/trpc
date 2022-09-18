bf5f539026386c61f60cf8b4c40c8d66d6b7c228
KATT <alexander@n1s.se>
2022-09-18 15:49:56 +0200

    tweak

--- examples/next-prisma-starter/src/pages/index.tsx
+++ examples/next-prisma-starter/src/pages/index.tsx
@@ -37,8 +37,13 @@ const IndexPage: NextPageWithLayout = () => {
     <>
       <h1>Welcome to your tRPC starter!</h1>
       <p>
-        Check <a href="https://trpc.io/">the docs</a> whenever you get stuck, or
-        ping <a href="https://twitter.com/alexdotjs">@alexdotjs</a> on Twitter.
+        If you get stuck, check <a href="https://trpc.io">the docs</a>, write a
+        message in our <a href="https://trpc.io/discord">Discord-channel</a>, or
+        write a message in{' '}
+        <a href="https://github.com/trpc/trpc/discussions">
+          GitHub Discussions
+        </a>
+        .
       </p>
 
       <h2>
