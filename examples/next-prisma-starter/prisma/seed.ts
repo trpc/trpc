5c84d3eaebd0889019691b1f1c084c15118e6d59
Sachin Raja <sachinraja2349@gmail.com>
2022-09-17 22:49:11 -0700

    add pnpm

--- examples/next-prisma-starter/prisma/seed.ts
+++ examples/next-prisma-starter/prisma/seed.ts
@@ -3,7 +3,7 @@
  *
  * @link https://www.prisma.io/docs/guides/database/seed-database
  */
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '.prisma/client';
 
 const prisma = new PrismaClient();
 
