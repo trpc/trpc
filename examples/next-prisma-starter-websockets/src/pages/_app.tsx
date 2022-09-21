6e9613e688bc5d7723a5f23e7b3ddcd56849dc0d
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-21 20:56:37 +0200

    ts-expect-error

--- examples/next-prisma-starter-websockets/src/pages/_app.tsx
+++ examples/next-prisma-starter-websockets/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { getSession, SessionProvider } from 'next-auth/react';
 import type { AppType } from 'next/app';
 import { trpc } from 'utils/trpc';
 
+// @ts-expect-error AppType['getInitialProps'] does not contain pageProps
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps,
@@ -15,6 +16,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
+// @ts-expect-error AppType['getInitialProps'] does not contain pageProps
 MyApp.getInitialProps = async ({ ctx }) => {
   return {
     pageProps: {
