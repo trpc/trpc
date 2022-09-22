c7188e843912e1544fafbb687d3e6cd283c83c3f
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-22 11:43:34 +0100

    feat(www): adds structured json to faq page

--- www/src/pages/faq.tsx
+++ www/src/pages/faq.tsx
@@ -1,76 +1,88 @@
+import Head from '@docusaurus/Head';
 import { Disclosure, Transition } from '@headlessui/react';
 import Layout from '@theme/Layout';
 import React from 'react';
 import { FiMinus, FiPlus } from 'react-icons/fi';
 
-const list = [
+const data = [
   {
-    question: <>Is V10 stable for production?</>,
-    answer: (
-      <>
-        Yes, V10 is stable enough for production usage but there is the
-        possibility that some API might change until the stable release comes
-        out.
-      </>
-    ),
+    '@content': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: {
+      '@type': 'Question',
+      name: 'Is V10 stable for production?',
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: 'Yes, V10 is stable enough for production usage but there is the possibility that some API might change until the stable release comes out.',
+      },
+    },
   },
   {
-    question: <>Input parsers doesn&apos;t give me the correct types</>,
-    answer: (
-      <>
-        A possible solution for this is to make sure that strict mode is enabled
-        in your tsconfig file.
-      </>
-    ),
+    '@content': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: {
+      '@type': 'Question',
+      name: "Input parsers doesn't give me the correct types",
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: 'A possible solution for this is to make sure that strict mode is enabled in your tsconfig file.',
+      },
+    },
   },
   {
-    question: <>getServerSideProps is not working for me.</>,
-    answer: (
-      <>
-        Due to the nature of how SSR in tRPC works and how it utilizes
-        getInitialProps, any getServerSideProps call won&apos;t be picked up. If
-        you would like to use getServerSideProps, we would recommend disabling
-        ssr for tRPC and using the ssg helper function to prefetch data inside
-        your getServerSideProps function.
-      </>
-    ),
+    '@content': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: {
+      '@type': 'Question',
+      name: 'getServerSideProps is not working for me.',
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: "Due to the nature of how SSR in tRPC works and how it utilizes getInitialProps, any getServerSideProps call won't be picked up. If you would like to use getServerSideProps, we would recommend disabling ssr for tRPC and using the ssg helper function to prefetch data inside your getServerSideProps function.",
+      },
+    },
   },
   {
-    question: <>Superjson transformer isn&apos;t working.</>,
-    answer: (
-      <>
-        Make sure to setup superjson as your transformer on both the server and
-        the client side.
-      </>
-    ),
+    '@content': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: {
+      '@type': 'Question',
+      name: "Superjson transformer isn't working.",
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: 'Make sure to setup superjson as your transformer on both the server and the client side.',
+      },
+    },
   },
 ];
 
 const Faq = () => {
   return (
     <Layout>
-      <main className="container mx-auto py-12 lg:py-28">
-        <h1 className="text-center text-2xl lg:text-3xl font-bold">
+      <Head>
+        <script type="application/ld+json">{JSON.stringify(data)}</script>
+      </Head>
+      <main className="container py-12 mx-auto lg:py-28">
+        <h1 className="text-2xl font-bold text-center lg:text-3xl">
           Most frequently asked questions
         </h1>
-        <p className="text-center text-zinc-700 pt-2 dark:text-zinc-300">
+        <p className="pt-2 text-center text-zinc-700 dark:text-zinc-300">
           A collection of the most commonly asked questions in regard to
           everything tRPC.
         </p>
         <div className="mt-6">
-          {list.map((item, index) => (
+          {data.map((item, index) => (
             <Disclosure
               as="div"
               key={`item-${index}`}
-              className="mt-3 max-w-3xl mx-auto"
+              className="max-w-3xl mx-auto mt-3"
             >
               {({ open }) => (
                 <>
                   <Disclosure.Button
                     as="button"
-                    className="flex justify-between items-center w-full text-base lg:text-lg bg-zinc-100 dark:bg-zinc-800/50 transition-colors ring-1 ring-zinc-200 dark:ring-zinc-800 py-3 px-6 rounded-xl"
+                    className="flex items-center justify-between w-full px-6 py-3 text-base transition-colors lg:text-lg bg-zinc-100 dark:bg-zinc-800/50 ring-1 ring-zinc-200 dark:ring-zinc-800 rounded-xl"
                   >
-                    {item.question}
+                    {item.mainEntity.name}
                     {open ? <FiMinus /> : <FiPlus />}
                   </Disclosure.Button>
                   <Transition
@@ -81,8 +93,8 @@ const Faq = () => {
                     leaveFrom="transform scale-100 opacity-100"
                     leaveTo="transform scale-95 opacity-0"
                   >
-                    <Disclosure.Panel className="py-3 px-6">
-                      {item.answer}
+                    <Disclosure.Panel className="px-6 py-3">
+                      {item.mainEntity.acceptedAnswer.text}
                     </Disclosure.Panel>
                   </Transition>
                 </>
