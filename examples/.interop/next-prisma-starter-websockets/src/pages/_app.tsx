8c204f0e3d31e9376eaaabf50712247a38ac0061
Julius Marminge <julius0216@outlook.com>
2022-09-22 22:28:46 +0200

    fix: use public AppType import and leverage generic (#2777)
    * lock some versions
    
    * use public import
    
    * fix type in ws example
    
    * utilize generic in component type

--- examples/.interop/next-prisma-starter-websockets/src/pages/_app.tsx
+++ examples/.interop/next-prisma-starter-websockets/src/pages/_app.tsx
@@ -3,9 +3,10 @@ import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
 import { loggerLink } from '@trpc/client/links/loggerLink';
 import { wsLink, createWSClient } from '@trpc/client/links/wsLink';
 import { withTRPC } from '@trpc/next';
+import type { Session } from 'next-auth';
 import { getSession, SessionProvider } from 'next-auth/react';
+import type { AppType } from 'next/app';
 import getConfig from 'next/config';
-import { AppType } from 'next/dist/shared/lib/utils';
 import type { AppRouter } from 'server/routers/_app';
 import superjson from 'superjson';
 
@@ -13,10 +14,12 @@ const { publicRuntimeConfig } = getConfig();
 
 const { APP_URL, WS_URL } = publicRuntimeConfig;
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps,
+}) => {
   return (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    <SessionProvider session={(pageProps as any).session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
@@ -24,9 +27,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
 MyApp.getInitialProps = async ({ ctx }) => {
   return {
-    pageProps: {
-      session: await getSession(ctx),
-    },
+    session: await getSession(ctx),
   };
 };
 
