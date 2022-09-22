cefaa9171ac0149748620764170ad06b942df1cb
KATT <alexander@n1s.se>
2022-09-22 19:09:49 +0200

    regression test

--- packages/server/test/reservedWords.test.ts
+++ packages/server/test/reservedWords.test.ts
@@ -1,14 +1,24 @@
 import './___packages';
 import { initTRPC } from '../src/core';
 
-const t = initTRPC.create();
+const t = initTRPC.create<{}>();
 
-test('`then` is a reserved word', async () => {
-  expect(() => {
-    return t.router({
-      then: t.procedure.query(() => 'hello'),
-    });
-  }).toThrowErrorMatchingInlineSnapshot(
-    `"Reserved words used in \`router({})\` call: then"`,
-  );
+describe('`then`', () => {
+  test('is a reserved word', async () => {
+    expect(() => {
+      return t.router({
+        then: t.procedure.query(() => 'hello'),
+      });
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"Reserved words used in \`router({})\` call: then"`,
+    );
+  });
+
+  // Regression https://github.com/trpc/trpc/pull/2562
+  test('because it creates async fns that returns proxy objects', async () => {
+    const appRouter = t.router({});
+    const asyncFnThatReturnsCaller = async () => appRouter.createCaller({});
+
+    await asyncFnThatReturnsCaller();
+  });
 });
