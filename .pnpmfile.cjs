28a797b096bbee4764a5f7ced48a523778cb8667
Sachin Raja <sachinraja2349@gmail.com>
2022-09-24 13:46:47 -0700

    unique engines too

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,4 +1,5 @@
 // prisma clients must all be different instances
+let id = 0;
 function readPackage(pkg) {
   const prismaClientVersion = pkg.dependencies['@prisma/client'];
   if (prismaClientVersion) {
@@ -10,6 +11,29 @@ function readPackage(pkg) {
     ] = `https://registry.npmjs.com/@prisma/client/-/client-${version}.tgz?id=${encodeURIComponent(
       pkg.name,
     )}`;
+  }
+
+  let dependencies = {};
+  if (pkg.dependencies.prisma) {
+    dependencies = pkg.dependencies;
+  } else if (pkg.devDependencies.prisma) {
+    dependencies = pkg.devDependencies;
+  }
+
+  const prismaVersion = dependencies.prisma;
+  if (prismaVersion) {
+    const version = prismaVersion.replace('^', '');
+    dependencies.prisma = `https://registry.npmjs.com/prisma/-/prisma-${version}.tgz?id=${encodeURIComponent(
+      pkg.name,
+    )}`;
+  }
+
+  const prismaEnginesVersion = pkg.dependencies['@prisma/engines'];
+  if (prismaEnginesVersion) {
+    pkg.dependencies[
+      '@prisma/engines'
+    ] = `https://registry.npmjs.com/@prisma/engines/-/engines-${prismaEnginesVersion}.tgz?id=${id}`;
+    id += 1;
   }
 
   return pkg;
