234e945cc15d4ab6950897ee0eefb4c8da09ecfb
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-25 20:27:30 +0200

    sqlite migrations

--- examples/next-prisma-starter/prisma/migrations/20220918091608_pagination/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[createdAt]` on the table `Post` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Post_createdAt_key" ON "Post"("createdAt");
--- examples/next-prisma-starter/prisma/migrations/20220918134120_revert/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Post_createdAt_key";
