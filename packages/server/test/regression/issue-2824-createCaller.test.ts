23ef4c9b5f7f92eb5517e5496e8b4dce4c347028
KATT <alexander@n1s.se>
2022-09-28 18:44:02 +0200

    add more tests

--- packages/server/test/regression/issue-2824-createCaller.test.ts
+++ packages/server/test/regression/issue-2824-createCaller.test.ts
@@ -2,6 +2,13 @@ import { routerToServerAndClientNew } from '../___testHelpers';
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
 
+const ignoreErrors = async (fn: () => Promise<void> | void) => {
+  try {
+    await Promise.reject(new Error('foo'));
+  } catch {
+    // ignore
+  }
+};
 test('createCaller', async () => {
   interface Context {
     userId: string;
@@ -11,10 +18,14 @@ test('createCaller', async () => {
 
   const createRouter = createRouterWithContext;
 
-  const legacyRouter = createRouter().mutation('test', {
-    input: z.string(),
-    resolve: () => 'this is a test',
-  });
+  const legacyRouter = createRouter()
+    .mutation('withInput', {
+      input: z.string(),
+      resolve: () => 'this is a test',
+    })
+    .mutation('noInput', {
+      resolve: () => 'this is also a test',
+    });
 
   const router = legacyRouter.interop();
 
@@ -22,11 +33,24 @@ test('createCaller', async () => {
 
   const caller = opts.router.createCaller({ userId: 'user1' });
 
-  expect(await caller.mutation('test', 'hello world')).toMatchInlineSnapshot(
+  expect(
+    await caller.mutation('withInput', 'hello world'),
+  ).toMatchInlineSnapshot(`"this is a test"`);
+
+  expect(await opts.client.mutation('withInput', 'foo')).toMatchInlineSnapshot(
     `"this is a test"`,
   );
 
-  expect(await opts.client.mutation('test', 'foo')).toMatchInlineSnapshot(
-    `"this is a test"`,
-  );
+  await ignoreErrors(async () => {
+    // @ts-expect-error this should complain
+    await caller.mutation('withInput');
+
+    // @ts-expect-error this should complain
+    await caller.mutation('withInput');
+
+    await caller.mutation('noInput');
+
+    // @ts-expect-error this should complain
+    await caller.mutation('noInput', 'this has no input');
+  });
 });
