e58094dc44e14a6b4a0a42b7eb13d2e737c1f022
KATT <alexander@n1s.se>
2022-09-29 18:38:23 +0200

    add repro for #2842

---
+++ packages/server/test/regression/issue-2842-createSSGHelpers-promise.test.ts
@@ -0,0 +1,22 @@
+import '../___packages';
+import { createProxySSGHelpers } from '@trpc/react/ssg';
+import { initTRPC } from '@trpc/server';
+
+test('createSSGPromise', async () => {
+  const t = initTRPC.create();
+
+  const router = t.router({
+    foo: t.procedure.query(() => 'bar'),
+  });
+  async function createSSGProxy() {
+    return createProxySSGHelpers({
+      router,
+      ctx: {},
+    });
+  }
+
+  const ssg = await createSSGProxy();
+
+  const foo = await ssg.foo.fetch();
+  expect(foo).toBe('bar');
+});
