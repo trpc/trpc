008ce7f8f6f18557b679735e90fd0c80edf10364
KATT <alexander@n1s.se>
2022-10-01 18:10:53 +0200

    add failing test

---
+++ packages/server/test/regression/issue-2856-middleware-infer.test.ts
@@ -0,0 +1,14 @@
+import '../___packages';
+import { TRPCError, initTRPC } from '@trpc/server';
+import { expectTypeOf } from 'expect-type';
+
+test('middleware next()', async () => {
+  const t = initTRPC.create();
+  t.middleware(async (opts) => {
+    const result = await opts.next();
+
+    expectTypeOf(result.error).toEqualTypeOf<TRPCError>();
+
+    return result;
+  });
+});
