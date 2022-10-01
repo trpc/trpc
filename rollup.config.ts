3eb941bec253040718e78b9b42139179185159ea
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-10-01 18:22:23 +0100

    simple config for client from root

--- rollup.config.ts
+++ rollup.config.ts
@@ -46,58 +46,52 @@ export default function rollup(): RollupOptions[] {
   return [
     ...buildConfig({
       input: INPUTS.server,
-      packageDir: 'packages/server',
     }),
     ...buildConfig({
       input: INPUTS.client,
-      packageDir: 'packages/client',
     }),
     ...buildConfig({
       input: INPUTS.react,
-      packageDir: 'packages/react',
     }),
     ...buildConfig({
       input: INPUTS.next,
-      packageDir: 'packages/next',
     }),
   ];
 }
 
 type Options = {
   input: string[];
-  packageDir: string;
 };
 
-function buildConfig({ input, packageDir }: Options): RollupOptions[] {
-  const resolvedInput = input.map((file) => path.resolve(packageDir, file));
+export function buildConfig({ input }: Options): RollupOptions[] {
+  const resolvedInput = input.map((file) => path.resolve('./', file));
   const options: Options = {
     input: resolvedInput,
-    packageDir,
   };
 
   return [types(options), lib(options)];
 }
 
-function types({ input, packageDir }: Options): RollupOptions {
+function types({ input }: Options): RollupOptions {
   return {
     input,
     output: {
-      dir: `${packageDir}/dist`,
+      dir: `./dist`,
     },
     plugins: [
       !isWatchMode &&
         del({
-          targets: `${packageDir}/dist`,
+          targets: `./dist`,
         }),
-      multiInput({ relative: path.resolve(packageDir, 'src/') }),
+      multiInput({ relative: path.resolve('./', 'src/') }),
       externals({
-        packagePath: path.resolve(packageDir, 'package.json'),
+        packagePath: path.resolve('./', 'package.json'),
         deps: true,
         devDeps: true,
         peerDeps: true,
       }),
       typescript({
-        tsconfig: path.resolve(packageDir, 'tsconfig.build.json'),
+        tsconfig: path.resolve('./', 'tsconfig.build.json'),
         tsconfigOverride: { emitDeclarationOnly: true },
         abortOnError: !isWatchMode,
       }),
@@ -105,27 +99,27 @@ function types({ input, packageDir }: Options): RollupOptions {
   };
 }
 
-function lib({ input, packageDir }: Options): RollupOptions {
+function lib({ input }: Options): RollupOptions {
   return {
     input,
     output: [
       {
-        dir: `${packageDir}/dist`,
+        dir: `./dist`,
         format: 'cjs',
         entryFileNames: '[name].js',
         chunkFileNames: '[name]-[hash].js',
       },
       {
-        dir: `${packageDir}/dist`,
+        dir: `./dist`,
         format: 'esm',
         entryFileNames: '[name].mjs',
         chunkFileNames: '[name]-[hash].mjs',
       },
     ],
     plugins: [
-      multiInput({ relative: path.resolve(packageDir, 'src/') }),
+      multiInput({ relative: path.resolve('./', 'src/') }),
       externals({
-        packagePath: path.resolve(packageDir, 'package.json'),
+        packagePath: path.resolve('./', 'package.json'),
       }),
       nodeResolve({
         extensions,
