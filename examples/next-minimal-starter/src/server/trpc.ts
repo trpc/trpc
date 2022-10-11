bf77d298c423b9772256ddeddb2b100550fe6211
KATT <alexander@n1s.se>
2022-10-11 11:31:53 +0200

    below

--- examples/next-minimal-starter/src/server/trpc.ts
+++ examples/next-minimal-starter/src/server/trpc.ts
@@ -3,7 +3,7 @@
  * - `initTRPC` should only be used once per app.
  * - We export only the functionality that we use so we can enforce which base procedures should be used
  *
- * Learn how to create protected base procedures and other things below.
+ * Learn how to create protected base procedures and other things below:
  * @see https://trpc.io/docs/v10/router
  * @see https://trpc.io/docs/v10/procedures
  */
