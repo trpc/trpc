bf7e9e890244934a9c38d1924adf33f86e0105cb
ivan <ivan.xu@applovin.com>
2022-11-07 15:28:49 +0800

    refactor: remove core/router.ts unreachable code

--- packages/server/src/core/router.ts
+++ packages/server/src/core/router.ts
@@ -225,13 +225,13 @@ export function createRouterFactory<TConfig extends AnyRootConfig>(
       procedures: routerProcedures,
       ...emptyRouter,
       record: procedures,
-      queries: Object.entries(routerProcedures || {})
+      queries: Object.entries(routerProcedures)
         .filter((pair) => (pair[1] as any)._def.query)
         .reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {}),
-      mutations: Object.entries(routerProcedures || {})
+      mutations: Object.entries(routerProcedures)
         .filter((pair) => (pair[1] as any)._def.mutation)
         .reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {}),
-      subscriptions: Object.entries(routerProcedures || {})
+      subscriptions: Object.entries(routerProcedures)
         .filter((pair) => (pair[1] as any)._def.subscription)
         .reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {}),
     };
