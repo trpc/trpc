74f27672977368e07731cbfaf42d549a377947b7
KATT <alexander@n1s.se>
2022-11-25 11:56:46 +0100

    seems to work

--- packages/tests/server/jsonify.test.ts
+++ packages/tests/server/jsonify.test.ts
@@ -5,6 +5,7 @@ import { routerToServerAndClientNew } from './___testHelpers';
 import { initTRPC } from '@trpc/server';
 import { expectTypeOf } from 'expect-type';
 import { konn } from 'konn';
+import superjson from 'superjson';
 
 describe('no transformer specified', () => {
   const ctx = konn()
@@ -57,6 +58,89 @@ describe('no transformer specified', () => {
         "date": "1970-01-01T00:00:00.000Z",
         "map": Object {},
         "set": Object {},
+      }
+    `);
+  });
+});
+
+describe('with transformer specified', () => {
+  const ctx = konn()
+    .beforeEach(() => {
+      const t = initTRPC.create({
+        transformer: superjson,
+      });
+
+      const appRouter = t.router({
+        happy: t.procedure.query(() => ({
+          map: new Map<string, string>([['foo', 'bar']]),
+          set: new Set<string>(['foo', 'bar']),
+          date: new Date(0),
+          fn: () => {
+            return 'hello';
+          },
+          arrayWithUndefined: [1, undefined, 2],
+        })),
+      });
+      const opts = routerToServerAndClientNew(appRouter);
+
+      return opts;
+    })
+    .afterEach(async (ctx) => {
+      await ctx?.close?.();
+    })
+    .done();
+  test('it works', async () => {
+    const result = await ctx.proxy.happy.query();
+
+    expectTypeOf(result.date).toEqualTypeOf<Date>();
+    result.set;
+    //     ^?
+
+    result.map;
+    //     ^?
+    expectTypeOf(result).toMatchTypeOf<{
+      date: Date;
+      map: Map<string, string>;
+      set: Set<string>;
+    }>();
+    //                     ^?
+
+    expect(result).toMatchInlineSnapshot(`
+      Object {
+        "json": Object {
+          "arrayWithUndefined": Array [
+            1,
+            null,
+            2,
+          ],
+          "date": "1970-01-01T00:00:00.000Z",
+          "map": Array [
+            Array [
+              "foo",
+              "bar",
+            ],
+          ],
+          "set": Array [
+            "foo",
+            "bar",
+          ],
+        },
+        "meta": Object {
+          "values": Object {
+            "arrayWithUndefined.1": Array [
+              "undefined",
+            ],
+            "date": Array [
+              "Date",
+            ],
+            "map": Array [
+              "map",
+            ],
+            "set": Array [
+              "set",
+            ],
+          },
+        },
       }
     `);
   });
