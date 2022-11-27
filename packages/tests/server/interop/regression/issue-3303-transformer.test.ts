c62d06e2ae90024e6191fa6bf5c57de982671d13
KATT <alexander@n1s.se>
2022-11-28 00:54:00 +0100

    add regression test

---
+++ packages/tests/server/interop/regression/issue-3303-transformer.test.ts
@@ -0,0 +1,74 @@
+import { routerToServerAndClientNew } from '../../___testHelpers';
+import { createQueryClient } from '../__queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { render, waitFor } from '@testing-library/react';
+import { createReactQueryHooks, httpBatchLink } from '@trpc/react-query/src';
+import * as interop from '@trpc/server/src';
+import { inferProcedureOutput, initTRPC } from '@trpc/server/src';
+import { expectTypeOf } from 'expect-type';
+import { konn } from 'konn';
+import React, { useState } from 'react';
+import superjson from 'superjson';
+import { z } from 'zod';
+
+type Context = {
+  foo: 'bar';
+};
+
+describe('transformer on legacy router', () => {
+  const ctx = konn()
+    .beforeEach(() => {
+      const t = initTRPC.context<Context>().create();
+      const legacyRouter = interop
+        .router<Context>()
+        .transformer(superjson)
+        .query('oldProcedure', {
+          resolve() {
+            return {
+              date: new Date(),
+            };
+          },
+        });
+
+      const newAppRouter = t.router({
+        newProcedure: t.procedure.query(() => {
+          date: new Date();
+        }),
+      });
+
+      const appRouter = t.mergeRouters(legacyRouter.interop(), newAppRouter);
+
+      const opts = routerToServerAndClientNew(appRouter, {
+        server: {
+          createContext() {
+            return {
+              foo: 'bar',
+            };
+          },
+        },
+        client({ httpUrl }) {
+          return {
+            transformer: superjson,
+            links: [
+              httpBatchLink({
+                url: httpUrl,
+              }),
+            ],
+          };
+        },
+      });
+      return {
+        ...opts,
+        appRouter,
+      };
+    })
+    .afterEach(async (ctx) => {
+      await ctx?.close?.();
+    })
+    .done();
+
+  test('call old', async () => {
+    const res = await ctx.client.query('oldProcedure');
+    expectTypeOf(res.date).toEqualTypeOf<string>();
+  });
+});
