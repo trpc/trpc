a50be628854da18a8e857ac350b08f8110709501
Ben Heimberg <bnheimberg@gmail.com>
2022-11-27 16:02:18 +0200

    feat: add `reset()` to utils proxy (#3142)

--- packages/react-query/src/shared/proxy/utilsProxy.ts
+++ packages/react-query/src/shared/proxy/utilsProxy.ts
@@ -5,6 +5,7 @@ import {
   InvalidateQueryFilters,
   RefetchOptions,
   RefetchQueryFilters,
+  ResetOptions,
   SetDataOptions,
   Updater,
 } from '@tanstack/react-query';
@@ -107,6 +108,14 @@ type DecorateProcedure<
   cancel(
     input?: inferProcedureInput<TProcedure>,
     options?: CancelOptions,
+  ): Promise<void>;
+
+  /**
+   * @link https://react-query.tanstack.com/reference/QueryClient#queryclientresetqueries
+   */
+  reset(
+    input?: inferProcedureInput<TProcedure>,
+    options?: ResetOptions,
   ): Promise<void>;
 
   /**
@@ -248,6 +257,7 @@ export function createReactQueryUtilsProxy<
         prefetchInfinite: () =>
           context.prefetchInfiniteQuery(queryKey, ...rest),
         invalidate: () => context.invalidateQueries(queryKey, ...rest),
+        reset: () => context.resetQueries(queryKey, ...rest),
         refetch: () => context.refetchQueries(queryKey, ...rest),
         cancel: () => context.cancelQuery(queryKey, ...rest),
         setData: () => context.setQueryData(queryKey, updater, ...rest),
