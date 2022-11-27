12aa81173287139da5c5ac90e024a2e5110dc6e0
KATT <alexander@n1s.se>
2022-11-27 15:29:44 +0100

    add

--- packages/tests/server/regression/issue-3085-bad-responses.test.ts
+++ packages/tests/server/regression/issue-3085-bad-responses.test.ts
@@ -1,38 +1,138 @@
-import { routerToServerAndClientNew } from '../___testHelpers';
-import { initTRPC } from '@trpc/server/src';
-import { konn } from 'konn';
+import '../___packages';
+import { waitError } from '../___testHelpers';
+import { TRPCClientError, createTRPCProxyClient, httpLink } from '@trpc/client';
+import { observable, observableToPromise } from '@trpc/server/observable';
+import http from 'http';
+import fetch from 'node-fetch';
 import superjson from 'superjson';
 
-describe('superjson happy path', () => {
-  const t = initTRPC.create({
+type Handler = (opts: {
+  req: http.IncomingMessage;
+  res: http.ServerResponse;
+}) => void;
+
+function createServer(handler: Handler) {
+  const server = http.createServer((req, res) => handler({ req, res }));
+  server.listen(0);
+
+  const port = (server.address() as any).port as number;
+
+  return {
+    url: `http://localhost:${port}`,
+    close: () => server.close(),
+  };
+}
+
+test('badly formatted response', async () => {
+  const server = createServer(({ res }) => {
+    res.setHeader('content-type', 'application/json');
+    res.write(JSON.stringify({}));
+    res.end();
+  });
+  const client: any = createTRPCProxyClient({
+    links: [
+      httpLink({
+        url: server.url,
+        fetch: fetch as any,
+      }),
+    ],
+  });
+
+  const error = await waitError(client.test.query(), TRPCClientError);
+  expect(error).toMatchInlineSnapshot(
+    `[TRPCClientError: Badly formatted response from server]`,
+  );
+
+  server.close();
+});
+
+test('badly formatted superjson response', async () => {
+  const server = createServer(({ res }) => {
+    res.setHeader('content-type', 'application/json');
+    res.write(
+      JSON.stringify({
+        result: {},
+      }),
+    );
+    res.end();
+  });
+  const client: any = createTRPCProxyClient({
+    links: [
+      httpLink({
+        url: server.url,
+        fetch: fetch as any,
+      }),
+    ],
     transformer: superjson,
   });
 
-  const appRouter = t.router({
-    greeting: t.procedure.query(() => {
-      return {
-        now: new Date(),
-      };
-    }),
+  const error = await waitError(client.test.query(), TRPCClientError);
+  expect(error).toMatchInlineSnapshot(
+    `[TRPCClientError: Cannot read properties of undefined (reading 'json')]`,
+  );
+
+  server.close();
+});
+
+test('badly formatted superjson response', async () => {
+  const server = createServer(({ res }) => {
+    res.setHeader('content-type', 'application/json');
+    res.write(
+      JSON.stringify({
+        result: {},
+      }),
+    );
+    res.end();
   });
-  const ctx = konn()
-    .beforeEach(() => {
-      const opts = routerToServerAndClientNew(appRouter, {
-        client: {
-          transformer: superjson,
-        },
-      });
-
-      return opts;
-    })
-    .afterEach(async (ctx) => {
-      await ctx?.close?.();
-    })
-    .done();
-
-  test('works', async () => {
-    const result = await ctx.proxy.greeting.query();
-
-    expect(result.now).toBeInstanceOf(Date);
+  const client: any = createTRPCProxyClient({
+    links: [
+      httpLink({
+        url: server.url,
+        fetch: fetch as any,
+      }),
+    ],
+    transformer: superjson,
   });
+
+  const error = await waitError(client.test.query(), TRPCClientError);
+  expect(error).toMatchInlineSnapshot(
+    `[TRPCClientError: Cannot read properties of undefined (reading 'json')]`,
+  );
+
+  server.close();
+});
+
+test('bad link', async () => {
+  const server = createServer(({ res }) => {
+    res.setHeader('content-type', 'application/json');
+    res.write(
+      JSON.stringify({
+        result: {},
+      }),
+    );
+    res.end();
+  });
+  const client: any = createTRPCProxyClient({
+    links: [
+      () => (opts) => {
+        return observable((observer) => {
+          opts.next(opts.op).subscribe({
+            ...observer,
+            next() {
+              throw new Error('whoops');
+            },
+          });
+        });
+      },
+      httpLink({
+        url: server.url,
+        fetch: fetch as any,
+      }),
+    ],
+  });
+
+  const error = await waitError(client.test.query(), TRPCClientError);
+  expect(error).toMatchInlineSnapshot(`[TRPCClientError: whoops]`);
+
+  server.close();
 });
