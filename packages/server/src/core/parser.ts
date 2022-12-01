2252b268e983b49df303e199c1edf44120317a1c
santosmarco <eu@marco.rio.br>
2022-12-01 15:21:24 -0300

    Add iots esques

--- packages/server/src/core/parser.ts
+++ packages/server/src/core/parser.ts
@@ -18,16 +18,26 @@ export type ParserCustomValidatorEsque<TInput> = (
 export type ParserYupEsque<TInput> = {
   validateSync: (input: unknown) => TInput;
 };
+
+export type ParserIoTsEsque<TInput, TParsedInput> = {
+  _I: TInput;
+  _O: TParsedInput;
+};
+
+export type ParserMyIoTsEsque<TInput> = {
+  decode: (input: unknown) => { right: TInput };
+};
+
 export type ParserWithoutInput<TInput> =
   | ParserYupEsque<TInput>
   | ParserSuperstructEsque<TInput>
   | ParserCustomValidatorEsque<TInput>
-  | ParserMyZodEsque<TInput>;
+  | ParserMyZodEsque<TInput>
+  | ParserMyIoTsEsque<TInput>;
 
-export type ParserWithInputOutput<TInput, TParsedInput> = ParserZodEsque<
-  TInput,
-  TParsedInput
->;
+export type ParserWithInputOutput<TInput, TParsedInput> =
+  | ParserZodEsque<TInput, TParsedInput>
+  | ParserIoTsEsque<TInput, TParsedInput>;
 
 export type Parser = ParserWithoutInput<any> | ParserWithInputOutput<any, any>;
 
