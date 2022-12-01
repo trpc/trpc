3c69cc0643a6e131d87a5d277b3d0165828a6f04
Bennett Dams <dams.bennett@gmail.com>
2022-12-01 20:17:51 +0100

    test: Infer context for SSG helpers creation

--- packages/tests/server/react/ssg.test.ts
+++ packages/tests/server/react/ssg.test.ts
@@ -1,10 +1,30 @@
 import { InfiniteData } from '@tanstack/react-query';
 import { createProxySSGHelpers } from '@trpc/react-query/src/ssg/ssgProxy';
-import { initTRPC } from '@trpc/server/src';
+import { inferAsyncReturnType, initTRPC } from '@trpc/server/src';
 import { expectTypeOf } from 'expect-type';
+import { CreateNextContextOptions } from 'packages/server/src/adapters/next';
 import { z } from 'zod';
 
-const t = initTRPC.create();
+async function createContextInner() {
+  return {
+    inner: 'inner value',
+  };
+}
+
+async function createContext(opts: CreateNextContextOptions) {
+  const contextInner = await createContextInner();
+
+  return {
+    ...contextInner,
+    outer: 'outer value',
+    req: opts.req,
+    res: opts.res,
+  };
+}
+
+export type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
 
 const appRouter = t.router({
   post: t.router({
@@ -55,4 +75,23 @@ test('prefetchInfinite and dehydrate', async () => {
 
   const data = JSON.stringify(ssg.dehydrate());
   expect(data).toContain('__infResult');
+});
+
+describe('Creating SSG helpers infers the given context type', () => {
+  test('happy path', async () => {
+    createProxySSGHelpers({
+      router: appRouter,
+      ctx: await createContextInner(),
+    });
+  });
+
+  test("given context is not a subset of router's context", async () => {
+    createProxySSGHelpers({
+      router: appRouter,
+      // @ts-expect-error The given context is wrong on purpose.
+      ctx: {
+        wrong: 'on purpose',
+      },
+    });
+  });
 });
