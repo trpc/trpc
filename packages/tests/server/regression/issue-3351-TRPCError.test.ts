4c0172aaa07b1a7768eb13fb0c9b77ae0a7f5060
KATT <alexander@n1s.se>
2022-12-06 17:47:29 +0100

    fix test

--- packages/tests/server/regression/issue-3351-TRPCError.test.ts
+++ packages/tests/server/regression/issue-3351-TRPCError.test.ts
@@ -1,7 +1,7 @@
 import '../___testHelpers';
 import { TRPCError } from '@trpc/server';
 
-test('getErrorFromUnknown', async () => {
+test('TRPCError cause', async () => {
   const err = new TRPCError({
     code: 'INTERNAL_SERVER_ERROR',
   });
