c74c4e4e054f60762e017eb84c7528e9f42f3def
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-12 20:25:17 +0100

    fontish?

--- www/og-image/pages/api/vercel.tsx
+++ www/og-image/pages/api/vercel.tsx
@@ -6,12 +6,16 @@ export const config = {
 };
 
 // load proper font here
-// const font = fetch(new URL("../../assets/inter.ttf", import.meta.url)).then(
-//   (res) => res.arrayBuffer()
-// );
+const font = fetch(
+  new URL('../../assets/SFPRODISPLAYREGULAR.OTF', import.meta.url),
+).then((res) => res.arrayBuffer());
+const fontBold = fetch(
+  new URL('../../assets/SFPRODISPLAYBOLD.OTF', import.meta.url),
+).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
-  // const fontData = await font;
+  const fontData = await font;
+  const fontBoldData = await fontBold;
 
   const { searchParams } = new URL(req.url);
 
@@ -22,13 +26,13 @@ export default async function handler(req: NextRequest) {
       <div
         style={{
           height: '100%',
-          width: '100%',
           display: 'flex',
+          width: '100%',
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
           position: 'relative',
-          // fontFamily: "Inter",
+          fontFamily: 'SF Pro Display',
         }}
       >
         <img
@@ -42,14 +46,17 @@ export default async function handler(req: NextRequest) {
         <div tw="flex items-start h-full w-full py-8">
           {/* header */}
           <div tw="flex w-full justify-between">
-            <h1 tw="text-6xl font-bold text-gray-900 mx-8">{title}</h1>
+            <h1 tw="text-6xl text-gray-900 mx-8">{title}</h1>
             <div tw="flex flex-col items-center">
               <img
                 tw="h-48 w-48 mx-12"
                 src="https://assets.trpc.io/icons/svgs/blue-bg-rounded.svg"
                 alt=""
               />
-              <span tw="text-5xl font-bold" style={{ fontWeight: 900 }}>
+              <span
+                tw="text-5xl font-bold"
+                style={{ fontFamily: 'SF Pro Display Bold' }}
+              >
                 tRPC
               </span>
             </div>
@@ -63,14 +70,20 @@ export default async function handler(req: NextRequest) {
     {
       width: 1200,
       height: 600,
-      // fonts: [
-      //   {
-      //     name: "Inter",
-      //     data: fontData,
-      //     style: "normal",
-      //     weight: 900,
-      //   },
-      // ],
+      fonts: [
+        {
+          name: 'SF Pro Display',
+          data: fontData,
+          style: 'normal',
+          weight: 400,
+        },
+        {
+          name: 'SF Pro Display Bold',
+          data: fontBoldData,
+          style: 'normal',
+          weight: 700,
+        },
+      ],
     },
   );
 }
