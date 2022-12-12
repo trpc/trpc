da8030921aab626c882edd59b1a7523fd384d9d5
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-12 19:42:14 +0100

    link

--- www/og-image/pages/index.tsx
+++ www/og-image/pages/index.tsx
@@ -1,25 +1,21 @@
 import Head from 'next/head';
 
+function getBaseUrl() {
+  if (typeof window !== 'undefined') return '';
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://127.0.0.1:${process.env.PORT ?? 3000}`;
+}
+
 export default function Page() {
   return (
     <div>
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
-        <meta
-          name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
-          }
-        />
+        <meta name="og:image" content={`${getBaseUrl()}/api/vercel`} />
       </Head>
       <h1>A page with Open Graph Image.</h1>
+      <a href={getBaseUrl() + '/api/vercel'}>See preview</a>
     </div>
   );
 }
