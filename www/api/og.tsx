d37ecaea01f509da0f207680f900085595a18c06
KATT <alexander@n1s.se>
2022-12-12 13:02:28 +0100

    add api test

---
+++ www/api/og.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from '@vercel/og';
+import React from 'react';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default function () {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Hello world!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    },
+  );
+}
