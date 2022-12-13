03167b1d3faeff6388c567ef44f487622c4fd223
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-13 10:02:30 +0100

    try stuff

--- www/api/ogBlog.ts
+++ www/api/ogBlog.ts
@@ -1,4 +1,5 @@
 import { ImageResponse } from '@vercel/og';
+import { z } from 'zod';
 import { OGBlogComponent } from './ogComponents';
 
 export const config = {
@@ -9,38 +10,44 @@ const inter = fetch(new URL('../static/Inter.ttf', import.meta.url)).then(
   (res) => res.arrayBuffer(),
 );
 
-const alex = 'https://avatars.githubusercontent.com/u/459267?v=4';
+const searchParamsSchema = z.object({
+  title: z.string(),
+  date: z
+    .string()
+    .transform((val) => new Date(val))
+    .transform((date) =>
+      Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+      }).format(date),
+    ),
+  readingTime: z.string(),
+  authorName: z.string(),
+  authorTitle: z.string(),
+  authorImg: z.string(),
+});
 
 export default async (req: Request) => {
   const interData = await inter;
 
   const { searchParams } = new URL(req.url);
+  console.log(new URL(req.url));
   console.log({ searchParams });
 
-  const title = searchParams.get('title') ?? 'Default tRPC Title';
-
-  const date = Intl.DateTimeFormat('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  }).format(
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    searchParams.has('date') ? new Date(searchParams.get('date')!) : new Date(),
-  );
-
-  const readingTime = searchParams.get('readingTime') ?? '5.00';
-  const img = searchParams.get('authorImg') || alex;
-  const author = searchParams.get('authorName') ?? 'Alex';
-  const authorDesc = searchParams.get('authorTitle') ?? 'Creator of tRPC';
+  const parsed = searchParamsSchema.safeParse(searchParams);
+  if (!parsed.success) {
+    return new Response(parsed.error.toString(), { status: 400 });
+  }
 
   return new ImageResponse(
     OGBlogComponent({
-      title,
-      author,
-      authorDesc,
-      img,
-      date,
-      readingTime,
+      title: parsed.data.title,
+      author: parsed.data.authorName,
+      authorDesc: parsed.data.authorTitle,
+      img: parsed.data.authorImg,
+      date: parsed.data.date,
+      readingTime: parsed.data.readingTime,
     }),
     {
       width: 1200,
