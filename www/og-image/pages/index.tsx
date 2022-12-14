94b5a7c9ed4ad77278e9fb334c846b41d211d312
KATT <alexander@n1s.se>
2022-12-14 20:37:52 +0100

    what

--- www/og-image/pages/index.tsx
+++ www/og-image/pages/index.tsx
@@ -20,42 +20,37 @@ export default function Page() {
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
-        <meta
-          name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
-          }
-        />
       </Head>
-      <h1>A page with Open Graph Image.</h1>
-      <h2>Blog</h2>
-      <img
-        src={`/api/blog?${searchParams<typeof blogParamsSchema['_input']>({
-          authorImg: 'https://avatars.githubusercontent.com/u/459267',
-          authorName: 'Alex "KATT" Johansson',
-          authorTitle: 'Creator of tRPC',
-          date: '2021-08-01',
-          description: 'A blog post about trpc',
-          readingTime: '5',
-          title: 'Hello world',
-        })}&random=${Math.random()}`}
-      />
-      <h2>Docs</h2>
-      <img
-        src={`/api/docs?${searchParams<typeof docsParamsSchema['_input']>({
-          description: 'A blog post about trpc',
-          title: 'Hello world',
-        })}&random=${Math.random()}`}
-      />
+      <h1>tRPC Open Graph images</h1>
+      <main>
+        <section>
+          <h2>Blog</h2>
+          <img
+            src={`/api/blog?${searchParams<typeof blogParamsSchema['_input']>({
+              authorImg: 'https://avatars.githubusercontent.com/u/459267',
+              authorName: 'Alex "KATT" Johansson',
+              authorTitle: 'Creator of tRPC',
+              date: '2021-08-01',
+              description: 'A blog post about trpc',
+              readingTime: '5',
+              title: 'Hello world',
+            })}&random=${Math.random()}`}
+          />
+        </section>
+        <section>
+          <h2>Docs</h2>
+          <img
+            src={`/api/docs?${searchParams<typeof docsParamsSchema['_input']>({
+              description: 'A blog post about trpc',
+              title: 'Hello world',
+            })}&random=${Math.random()}`}
+          />
+        </section>
+      </main>
       {/* eslint-disable-next-line react/no-unknown-property */}
       <style jsx>{`
         img {
+          width: 600px;
           max-width: 100%;
         }
       `}</style>
