e1e42a8266dce5c777fa201e6eae1d7d12707295
KATT <alexander@n1s.se>
2022-12-14 19:53:15 +0100

    wha

--- www/og-image/pages/index.tsx
+++ www/og-image/pages/index.tsx
@@ -20,12 +20,11 @@ export default function Page() {
         />
       </Head>
       <h1>A page with Open Graph Image.</h1>
-
       <h2>Blog</h2>
       <img src="/api/blog?title=x&description=x&date=2020-01-01&readingTime=1&authorName=alex&authorTitle=o&authorImg=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F459267%3Fv%3D4" />
-
       <h2>Docs</h2>
       <img src="/api/docs?title=x&description=y" />
+      {/* eslint-disable-next-line react/no-unknown-property */}
       <style jsx>{`
         img {
           max-width: 100%;
