eb2fd7ae198ba0b60adae38dbd56aca6258bb086
KATT <alexander@n1s.se>
2022-12-14 20:32:49 +0100

    what

--- www/og-image/pages/index.tsx
+++ www/og-image/pages/index.tsx
@@ -1,4 +1,17 @@
 import Head from 'next/head';
+import { blogParamsSchema } from './api/blog';
+
+function searchParams<T extends Record<string, string | string[]>>(
+  obj: T,
+): string {
+  return Object.entries(obj)
+    .map(([key, value]) => {
+      const values = Array.isArray(value) ? value : [value];
+
+      return values.map((v) => `${key}=${encodeURIComponent(v)}`).join('&');
+    })
+    .join('&');
+}
 
 export default function Page() {
   return (
@@ -21,7 +34,17 @@ export default function Page() {
       </Head>
       <h1>A page with Open Graph Image.</h1>
       <h2>Blog</h2>
-      <img src="/api/blog?title=x&description=x&date=2020-01-01&readingTime=1&authorName=alex&authorTitle=o&authorImg=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F459267%3Fv%3D4" />
+      <img
+        src={`/api/blog?${searchParams<typeof blogParamsSchema['_input']>({
+          authorImg: 'https://avatars.githubusercontent.com/u/459267',
+          authorName: 'Alex "KATT" Johansson',
+          authorTitle: 'Creator of tRPC',
+          date: '2021-08-01',
+          description: 'A blog post about trpc',
+          readingTime: '5',
+          title: 'Hello world',
+        })}&random=${Math.random()}`}
+      />
       <h2>Docs</h2>
       <img src="/api/docs?title=x&description=y" />
       {/* eslint-disable-next-line react/no-unknown-property */}
