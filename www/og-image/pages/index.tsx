5810eb01205c4356222a2a270909e005ba43d6f2
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-16 11:32:24 +0100

    testy

--- www/og-image/pages/index.tsx
+++ www/og-image/pages/index.tsx
@@ -1,24 +1,18 @@
 import Head from 'next/head';
 import { blogParams, docsParams } from 'utils/zodParams';
 
+const ogImageUrl = `${
+  process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
+}/api/landing`;
+
 export default function Page() {
   return (
     <div>
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
-        <meta
-          name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/landing`
-          }
-        />
+        <meta name="og:image" content={ogImageUrl} />
+        <meta data-rh="true" name="twitter:image" content={ogImageUrl} />
       </Head>
       <h1>A page with Open Graph Image.</h1>
       <main className="grid grid-cols-2 grid-flow-row">
