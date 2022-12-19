11e778c184645d32492c7bac4ba9c48d95bb3e45
KATT <alexander@n1s.se>
2022-12-19 02:21:34 +0100

    fix

--- www/og-image/next.config.js
+++ www/og-image/next.config.js
@@ -5,7 +5,7 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
     // validate env vars
-    require('./src/utils/env');
+    require('./utils/env');
   }
   /**
    * @type {import('next').NextConfig}
