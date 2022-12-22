42f65f085be41ce28a0e07ffa4aee03c1529d0bc
KATT <alexander@n1s.se>
2022-12-22 23:15:15 +0100

    fix(server): `context` and `meta` should be able to be `interface`s

--- packages/server/src/core/internals/config.ts
+++ packages/server/src/core/internals/config.ts
@@ -5,8 +5,8 @@ import { ErrorFormatter } from '../../error/formatter';
  * @internal
  */
 export interface RootConfigTypes {
-  ctx: Record<string, unknown>;
-  meta: Record<string, unknown>;
+  ctx: Record<any, unknown>;
+  meta: Record<any, unknown>;
   errorShape: unknown;
   transformer: unknown;
 }
