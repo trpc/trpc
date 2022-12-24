93c933d8c2c1fdfceab3667ab622033ae908e345
KATT <alexander@n1s.se>
2022-12-24 23:27:46 +0100

    rm more interop

--- packages/react-query/src/index.ts
+++ packages/react-query/src/index.ts
@@ -1,5 +1,4 @@
 export * from '@trpc/client';
 
 export { createTRPCReact, type CreateTRPCReact } from './createTRPCReact';
-export { createReactQueryHooks } from './interop';
 export type { inferReactQueryProcedureOptions } from './utils/inferReactQueryProcedure';
