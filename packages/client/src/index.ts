25c0befb3eb4944efe0ac3caa79645bc4e92cac9
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-25 14:26:18 -0500

    Update packages/client/src/index.ts
    Co-authored-by: Alex / KATT <alexander@n1s.se>

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,6 +1,5 @@
 export * from './createTRPCClient';
 export * from './createTRPCClientProxy';
 export * from './getFetch';
-export * from './internals/types';
 export * from './TRPCClientError';
 export * from './links';
