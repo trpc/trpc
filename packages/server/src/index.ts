995d9c8dc27fbafdf4d22e5b87dd93a7b58d67f4
KATT <alexander@n1s.se>
2022-12-27 00:49:36 +0100

    cleanup server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,5 @@
 export * from './transformer';
 export * from './error/TRPCError';
 export * from './types';
-export { router } from './deprecated/router';
 export * from './core';
 export * from './internals';
