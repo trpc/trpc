a7f2b9578c490612d1a70bf83e4e41110f08c278
KATT <alexander@n1s.se>
2023-01-04 23:43:57 +0000

    chore:

--- packages/client/src/createTRPCClient.ts
+++ packages/client/src/createTRPCClient.ts
@@ -59,15 +59,6 @@ export interface TRPCClient<TRouter extends AnyRouter> {
 export function createTRPCClient<TRouter extends AnyRouter>(
   opts: CreateTRPCClientOptions<TRouter>,
 ) {
-  const getLinks = () => {
-    if ('links' in opts) {
-      return opts.links;
-    }
-    return [httpBatchLink(opts)];
-  };
-  const client = new TRPCUntypedClient({
-    ...opts,
-    links: getLinks(),
-  });
+  const client = new TRPCUntypedClient(opts);
   return client as TRPCClient<TRouter>;
 }
