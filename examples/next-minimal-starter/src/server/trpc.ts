e31a5c2f0748aee4494a2eafe8eadf0d47b6fd0d
AbderrahimGuerfi <112620903+AbderrahimGuerfi@users.noreply.github.com>
2023-01-05 15:11:52 +0100

    chore: trpc root configuration shorter export

--- examples/next-minimal-starter/src/server/trpc.ts
+++ examples/next-minimal-starter/src/server/trpc.ts
@@ -9,12 +9,9 @@
  */
 import { initTRPC } from '@trpc/server';
 
-const t = initTRPC.create();
+const { procedure: publicProcedure, router, middleware } = initTRPC.create();
 
 /**
  * Unprotected procedure
  **/
-export const publicProcedure = t.procedure;
-
-export const router = t.router;
-export const middleware = t.middleware;
+export { publicProcedure, router, middleware };
