a23b792597f213a692f71a55257b2d67c68a08bc
OrJDev <orjdeveloper@gmail.com>
2023-01-21 14:57:47 +0200

    more minimal

--- examples/solidstart-minimal-starter/src/routes/index.tsx
+++ examples/solidstart-minimal-starter/src/routes/index.tsx
@@ -1,10 +1,9 @@
-import styles from "./index.module.css";
-import { type VoidComponent } from "solid-js";
-import { A, Head, Title, Meta, Link } from "solid-start";
-import { trpc } from "~/utils/trpc";
+import { type VoidComponent } from 'solid-js';
+import { Head, Link, Meta, Title } from 'solid-start';
+import { trpc } from '~/utils/trpc';
 
 const Home: VoidComponent = () => {
-  const hello = trpc.example.hello.useQuery(() => ({ name: "from tRPC" }));
+  const hello = trpc.example.hello.useQuery(() => ({ name: 'from tRPC' }));
   return (
     <>
       <Head>
@@ -13,37 +12,7 @@ const Home: VoidComponent = () => {
         <Link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div class={styles.container}>
-          <h1 class={styles.title}>
-            Create <span class={styles.greenSpan}>JD</span> App
-          </h1>
-          <div class={styles.cardRow}>
-            <A
-              class={styles.card}
-              href="https://start.solidjs.com"
-              target="_blank"
-            >
-              <h3 class={styles.cardTitle}>Solid Start →</h3>
-              <div class={styles.cardText}>
-                Learn more about Solid Start and the basics.
-              </div>
-            </A>
-            <A
-              class={styles.card}
-              href="https://github.com/orjdev/create-jd-app"
-              target="_blank"
-            >
-              <h3 class={styles.cardTitle}>JD End →</h3>
-              <div class={styles.cardText}>
-                Learn more about Create JD App, the libraries it uses, and how
-                to deploy it
-              </div>
-            </A>
-          </div>
-          <p class="text-2xl text-white">
-            {hello.data ?? "Loading tRPC query"}
-          </p>
-        </div>
+        <p>{hello.data ?? 'Loading tRPC query'}</p>
       </main>
     </>
   );
