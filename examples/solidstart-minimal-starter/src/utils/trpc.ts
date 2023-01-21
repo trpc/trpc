cd47654a714ab1fc0078f8464459b146744813f1
OrJDev <orjdeveloper@gmail.com>
2023-01-21 15:04:39 +0200

    production url for solid-start trpc

--- examples/solidstart-minimal-starter/src/utils/trpc.ts
+++ examples/solidstart-minimal-starter/src/utils/trpc.ts
@@ -3,8 +3,16 @@ import { httpBatchLink } from '@trpc/client';
 import { createTRPCSolid } from 'solid-trpc';
 import { AppRouter } from '~/server/trpc';
 
+const PRODUCTION_URL = '';
+
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return '';
+  if (process.env.NODE_ENV === 'production') {
+    if (PRODUCTION_URL === '') {
+      console.error('PRODUCTION_URL is not set');
+    }
+    return PRODUCTION_URL;
+  }
   return `http://localhost:${process.env.PORT ?? 5173}`;
 };
 
