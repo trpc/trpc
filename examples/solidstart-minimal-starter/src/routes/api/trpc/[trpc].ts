2083ad5544d7bcbe39069215e2e9bb3f203e2f1b
OrJDev <orjdeveloper@gmail.com>
2023-01-21 14:55:59 +0200

    fix

--- examples/solidstart-minimal-starter/src/routes/api/trpc/[trpc].ts
+++ examples/solidstart-minimal-starter/src/routes/api/trpc/[trpc].ts
@@ -2,10 +2,7 @@ import { createSolidApiHandler } from '@trpc/server/adapters/solid';
 import { createContext } from '~/server/trpc/context';
 import { appRouter } from '~/server/trpc/router/_app';
 
-const handler = createSolidApiHandler({
+export const { GET, POST } = createSolidApiHandler({
   router: appRouter,
   createContext,
 });
-
-export const GET = handler;
-export const POST = handler;
