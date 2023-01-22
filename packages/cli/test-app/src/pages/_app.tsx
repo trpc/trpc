aa8251a76a93942f2c87464b4fd30fac2bb2f67c
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-22 16:05:59 +0100

    rm test-app

--- packages/cli/test-app/src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppType } from 'next/app';
-
-const App: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-export default App;
--- packages/cli/test-app/src/pages/api/trpc/[trpc].ts
+++
@@ -1,9 +0,0 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { appRouter } from "~/server/routers/_app";
-import { createContext } from "~/server/context";
-
-// export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-});
