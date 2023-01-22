4aa8bfe67f5087e5150deadbb9bea54ab7b66c17
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-22 16:09:41 +0100

    cleanup

--- packages/cli/src/pkgMgr.ts
+++ packages/cli/src/pkgMgr.ts
@@ -1,17 +1,9 @@
 export const getPkgMgr = () => {
-  // This environment variable is set by npm and yarn but pnpm seems less consistent
   const userAgent = process.env.npm_config_user_agent;
-
-  if (userAgent) {
-    if (userAgent.startsWith('yarn')) {
-      return 'yarn';
-    } else if (userAgent.startsWith('pnpm')) {
-      return 'pnpm';
-    } else {
-      return 'npm';
-    }
-  } else {
-    // If no user agent is set, assume npm
-    return 'npm';
+  if (userAgent?.startsWith('yarn')) {
+    return 'yarn';
+  } else if (userAgent?.startsWith('pnpm')) {
+    return 'pnpm';
   }
+  return 'npm';
 };
