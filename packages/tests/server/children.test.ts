ccb23f97085ecb6f85776b5e07fb74175de376f2
igalklebanov <igalklebanov@gmail.com>
2023-01-31 03:00:31 +0200

    add `namespaceDelimiter` test case @ children.test (to be continued).

--- packages/tests/server/children.test.ts
+++ packages/tests/server/children.test.ts
@@ -62,3 +62,57 @@ test('w/o children', async () => {
 
   expectTypeOf(router._def.procedures.foo).toEqualTypeOf(foo);
 });
+
+test('children, custom namespace delimiter', async () => {
+  const t = initTRPC.create({
+    namespaceDelimiter: '/',
+  });
+
+  const router = t.router({
+    foo: t.procedure.query(() => 'bar'),
+    child: t.router({
+      childQuery: t.procedure.query(() => 'asd'),
+      grandchild: t.router({
+        foo: t.procedure.query(() => 'grandchild' as const),
+        mut: t.procedure.mutation(() => 'mut'),
+      }),
+    }),
+  });
+
+  const { queries, mutations, subscriptions, procedures } = router._def;
+  expect({
+    queries,
+    mutations,
+    subscriptions,
+    procedures,
+  }).toMatchInlineSnapshot(`
+    Object {
+      "mutations": Object {
+        "child/grandchild/mut": [Function],
+      },
+      "procedures": Object {
+        "child/childQuery": [Function],
+        "child/grandchild/foo": [Function],
+        "child/grandchild/mut": [Function],
+        "foo": [Function],
+      },
+      "queries": Object {
+        "child/childQuery": [Function],
+        "child/grandchild/foo": [Function],
+        "foo": [Function],
+      },
+      "subscriptions": Object {},
+    }
+  `);
+
+  // TODO: fix this stuff...
+
+  // const { close, proxy } = routerToServerAndClientNew(router);
+
+  // expect(await proxy.foo.query()).toBe('bar');
+
+  // expect(await proxy.child.grandchild.foo.query()).toBe('grandchild');
+  // expect(await proxy.child.grandchild.mut.mutate()).toBe('mut');
+
+  // return close();
+});
