7f2b871d342ba2c8cab15d6a27df0128d55af847
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-07 19:17:21 +0100

    use root config, rm disables

--- www/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-
-module.exports = {
-  extends: '../.eslintrc',
-  parserOptions: {
-    project: path.resolve(__dirname, 'tsconfig.eslint.json'),
-  },
-};
