ec9bdff89e7667e951f53d861ebcf79c7fb6218a
Alok Vishwakarma <avalokkumar@gmail.com>
2023-02-14 07:05:35 +0530

    Directory Traversal Vulnerability Fix
    🎯 Changes
    What changes are made in this PR? Is it a feature or a bug fix?
    
    * Instead of using string concatenation to create the file path, path.join function is used to safely join the individual path segments into a valid file path. This helps to prevent directory traversal attacks.
    
    * Make the url parameter passed to createWsClient optionally be a function that returns a string. If the url that should be used is dynamic or changing over time this allows the wsClient to respect that when automatically reconnecting.
    
    ✅ Checklist
     - [x] I have followed the steps listed in the Contributing guide.
     - [x] If necessary, I have added documentation related to the changes made.
     - [ ] I have added or updated the tests related to the changes made.

--- scripts/generateBigLegacyRouter.ts
+++ scripts/generateBigLegacyRouter.ts
@@ -3,10 +3,10 @@ import path from 'path';
 
 const NUM_PROCEDURES_TO_GENERATE = 200;
 
-const TEST_DIR =
-  __dirname + '/../packages/tests/server/__generated__/bigLegacyRouter';
+// Fix Directory Traversal Vulnerability by using path.join to join the directories
+const TEST_DIR = path.join(__dirname, '..', 'packages', 'tests', 'server', '__generated__', 'bigLegacyRouter');
 
-// Big F̶u̶c̶ Fantastic Router
+// Big Fantastic Router
 function getBFR() {
   const str = [`trpc.router()`];
   for (let num = 1; num <= NUM_PROCEDURES_TO_GENERATE; num++) {
@@ -20,10 +20,13 @@ function getBFR() {
 const contents = `
 /* eslint-disable */
 import * as trpc from '@trpc/server';
-
 export const bigRouter = ${getBFR()}
   .flat();
 `.trim();
 
-fs.mkdirSync(TEST_DIR, { recursive: true });
+// Ensure that the directory exists before trying to write to it
+if (!fs.existsSync(TEST_DIR)) {
+  fs.mkdirSync(TEST_DIR, { recursive: true });
+}
+
 fs.writeFileSync(path.join(TEST_DIR, 'bigRouter.ts'), contents);
