bb4184964565f53d8922df54d015a179c8a8e0cd
KATT <alexander@n1s.se>
2023-02-18 15:33:19 +0100

    add client test

--- examples/.test/internal-types-export/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { initTRPC } from '@trpc/server';
-
-export const t = initTRPC.create();
--- packages/server/src/internals.ts
+++ packages/server/src/internals.ts
@@ -4,6 +4,9 @@
 export type { DefaultErrorShape } from './error/formatter';
 export type { mergeRouters } from './core/internals/mergeRouters';
 export type { RootConfig, AnyRootConfig } from './core/internals/config';
-export type { ProcedureBuilder, BuildProcedure } from './core/internals/procedureBuilder';
+export type {
+  ProcedureBuilder,
+  BuildProcedure,
+} from './core/internals/procedureBuilder';
 export type { unsetMarker } from './core/internals/utils';
 export type { MiddlewareFunction, MiddlewareBuilder } from './core/middleware';
