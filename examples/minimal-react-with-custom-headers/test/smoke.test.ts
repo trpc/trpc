a75586fd48bdc45885559665c123f2cec92e18c5
Nick Schmidt <nick@fintory.com>
2023-02-22 12:09:33 +0100

    docs: added example and tests

---
+++ examples/minimal-react-with-custom-headers/test/smoke.test.ts
@@ -0,0 +1,13 @@
+import { test } from '@playwright/test';
+
+test.setTimeout(35e3);
+
+test('go to /', async ({ page }) => {
+  await page.goto('/');
+
+  await page.waitForSelector(`text=hello user with token undefined`);
+
+  await page.click('button')
+
+  await page.waitForSelector(`text=hello user with token custom-token`);
+});
