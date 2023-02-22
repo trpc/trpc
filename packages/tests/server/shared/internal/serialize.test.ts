3b70c842abf2b40a5637d4fb35c504675d85101a
Joep Roebroek <j.roebroek@gmail.com>
2023-02-22 11:28:28 +0100

    chore: fix lint issue

--- packages/tests/server/shared/internal/serialize.test.ts
+++ packages/tests/server/shared/internal/serialize.test.ts
@@ -1,11 +1,13 @@
-import { expectTypeOf } from 'expect-type'
 import { Serialize } from '@trpc/server/shared/internal/serialize';
+import { expectTypeOf } from 'expect-type';
 
 interface MixedKnownAndUnknownKeys {
-    name: string;
-    [x: string]: unknown;
+  name: string;
+  [x: string]: unknown;
 }
 
 test('it serializes objects with known and unknown keys', () => {
-    expectTypeOf<MixedKnownAndUnknownKeys>().toMatchTypeOf<Serialize<MixedKnownAndUnknownKeys>>()
-})
+  expectTypeOf<MixedKnownAndUnknownKeys>().toMatchTypeOf<
+    Serialize<MixedKnownAndUnknownKeys>
+  >();
+});
