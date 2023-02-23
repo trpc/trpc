99fd151bcde0b4000e95f0fd0ed6c92704df6a9c
Alex / KATT <alexander@n1s.se>
2023-02-23 16:45:29 +0100

    Update fetchHelpers.ts

--- packages/client/src/internals/fetchHelpers.ts
+++ packages/client/src/internals/fetchHelpers.ts
@@ -2,10 +2,13 @@ import { Maybe } from '@trpc/server';
 import { AbortControllerEsque } from './types';
 
 export function getWindow() {
+  if (typeof globalThis !== 'undefined') {
+    return globalThis; 
+  }
   if (typeof window !== 'undefined') {
     return window;
   }
-  return globalThis;
+  throw new Error('Could not resolve "globalThis"');
 }
 export function getAbortController(
   ac: Maybe<AbortControllerEsque>,
