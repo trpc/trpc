be18f700c40abdbaf78fe9d7cd7b03236e1850f2
Sachin Raja <me@s4n.land>
2023-02-25 07:47:42 -0800

    chore(examples): move some examples to esm

--- examples/lambda-api-gateway/src/client.ts
+++ examples/lambda-api-gateway/src/client.ts
@@ -1,11 +1,8 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import fetch from 'node-fetch';
 import type { AppRouter } from './server';
 
-global.fetch = fetch as any;
-
 const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: 'http://127.0.0.1:4050' })],
+  links: [httpBatchLink({ url: 'http://localhost:4050' })],
 });
 
 (async () => {
