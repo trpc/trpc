993ef9c721e65c32bd27f46902b1bf39bc180f94
KATT <alexander@n1s.se>
2023-02-26 16:20:45 +0100

    export formDataLink

--- packages/client/src/links/index.ts
+++ packages/client/src/links/index.ts
@@ -1,9 +1,11 @@
+export * from './types';
+
 export * from './httpBatchLink';
 export * from './httpLink';
 export * from './loggerLink';
 export * from './splitLink';
 export * from './wsLink';
-export * from './types';
+export * from './formDataLink';
 
 // These are not public (yet) as we get this functionality from tanstack query
 // export * from './retryLink';
