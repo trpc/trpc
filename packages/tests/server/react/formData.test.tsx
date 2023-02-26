317edba2ac2f341c2a0f33ebb2942534a84eed5f
KATT <alexander@n1s.se>
2023-02-26 22:35:28 +0100

    tweaky

--- packages/tests/server/react/formData.test.tsx
+++ packages/tests/server/react/formData.test.tsx
@@ -3,6 +3,7 @@ import { createQueryClient } from '../__queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { waitFor } from '@testing-library/dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { createTRPCClientProxy, loggerLink } from '@trpc/client';
 import { unstable_formDataLink } from '@trpc/client/links/formDataLink';
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
@@ -36,26 +37,30 @@ const zodFile = zodFileObject.transform(
   },
 );
 
-const createUserSchema = z.object({
-  name: z.string(),
-  age: z.string().transform(Number).pipe(z.number()),
-  image: zodFile.optional(),
-});
 const ctx = konn()
   .beforeEach(() => {
     const t = initTRPC.create();
 
     const appRouter = t.router({
-      createUser: t.procedure.input(createUserSchema).mutation(({ input }) => {
-        return {
-          ...input,
-          image: input.image && {
-            filename: input.image.name,
-            size: input.image.size,
-            type: input.image.type,
-          },
-        };
-      }),
+      createUser: t.procedure
+        .input(
+          z.object({
+            name: z.string(),
+            age: z.coerce.number(),
+            image: zodFile.optional(),
+          }),
+        )
+        .mutation(({ input }) => {
+          return {
+            ...input,
+            image: input.image && {
+              //          ^?
+              filename: input.image.name,
+              size: input.image.size,
+              type: input.image.type,
+            },
+          };
+        }),
       uploadFile: t.procedure
         .input(
           z.object({
@@ -137,8 +142,9 @@ const ctx = konn()
   })
   .done();
 
-test('react basic', async () => {
+test('react', async () => {
   const { proxy, App } = ctx;
+
   function MyComponent() {
     const createUserMutation = proxy.createUser.useMutation();
 
@@ -154,6 +160,7 @@ test('react basic', async () => {
         >
           <input name="name" defaultValue="bob" />
           <input name="age" defaultValue={42} />
+          <input name="image" type="file" data-testid="image-input" />
           <button type="submit">Submit</button>
         </form>
         {createUserMutation.data && (
@@ -165,24 +172,98 @@ test('react basic', async () => {
     );
   }
 
-  const utils = render(
-    <App>
-      <MyComponent />
-    </App>,
-  );
+  {
+    //upload w/o file
+    const utils = render(
+      <App>
+        <MyComponent />
+      </App>,
+    );
 
-  utils.getByText('Submit').click();
+    utils.getByText('Submit').click();
 
-  await waitFor(() => {
-    utils.getByTestId('result');
-  });
+    await waitFor(() => {
+      utils.getByTestId('result');
+    });
 
-  expect(utils.getByTestId('result').textContent).toMatchInlineSnapshot(`
-    "{
-        \\"name\\": \\"bob\\",
-        \\"age\\": 42
-    }"
-  `);
+    expect(utils.getByTestId('result').textContent).toMatchInlineSnapshot(`
+      "{
+          \\"name\\": \\"bob\\",
+          \\"age\\": 42,
+          \\"image\\": {
+              \\"filename\\": \\"\\",
+              \\"size\\": 13,
+              \\"type\\": \\"application/octet-stream\\"
+          }
+      }"
+    `);
+    utils.unmount();
+  }
+});
+
+test('react upload file', async () => {
+  const { proxy, App } = ctx;
+
+  function MyComponent() {
+    const createUserMutation = proxy.createUser.useMutation();
+
+    return (
+      <div>
+        <form
+          onSubmit={(e) => {
+            const formData = new FormData(e.currentTarget);
+            createUserMutation.mutate(formData as any);
+
+            e.preventDefault();
+          }}
+        >
+          <input name="name" defaultValue="bob" />
+          <input name="age" defaultValue={42} />
+          <input name="image" type="file" data-testid="image-input" />
+          <button type="submit">Submit</button>
+        </form>
+        {createUserMutation.data && (
+          <pre data-testid={'result'}>
+            {JSON.stringify(createUserMutation.data, null, 4)}
+          </pre>
+        )}
+      </div>
+    );
+  }
+  {
+    // upload with file
+    const utils = render(
+      <App>
+        <MyComponent />
+      </App>,
+    );
+    // get the upload button
+    const uploader = utils.getByTestId('image-input');
+
+    const file = new File(['hi bob'], 'bob.txt', {
+      type: 'text/plain',
+    });
+
+    await userEvent.upload(uploader, file);
+
+    utils.getByText('Submit').click();
+
+    await waitFor(() => {
+      utils.getByTestId('result');
+    });
+
+    expect(utils.getByTestId('result').textContent).toMatchInlineSnapshot(`
+      "{
+          \\"name\\": \\"bob\\",
+          \\"age\\": 42,
+          \\"image\\": {
+              \\"filename\\": \\"\\",
+              \\"size\\": 13,
+              \\"type\\": \\"application/octet-stream\\"
+          }
+      }"
+    `);
+  }
 });
 
 test('upload file', async () => {
