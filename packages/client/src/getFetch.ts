aae49c733f461abe5f40f2f447e5aabb7b2f8f20
KATT <alexander@n1s.se>
2023-03-03 10:04:38 +0100

    maybe

--- packages/client/src/getFetch.ts
+++ packages/client/src/getFetch.ts
@@ -1,5 +1,14 @@
 import { FetchEsque, NativeFetchEsque } from './internals/types';
 
+type AnyFn = (...args: any[]) => unknown;
+
+const isFunction = (fn: unknown): fn is AnyFn => typeof fn === 'function';
+
+function _bind(fn: AnyFn, thisArg: unknown): AnyFn {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  return isFunction(fn.bind) ? fn.bind(thisArg) : fn;
+}
+
 export function getFetch(
   customFetchImpl?: FetchEsque | NativeFetchEsque,
 ): FetchEsque {
@@ -7,12 +16,12 @@ export function getFetch(
     return customFetchImpl as FetchEsque;
   }
 
-  if (typeof window !== 'undefined' && window.fetch) {
-    return window.fetch.bind(window) as FetchEsque;
+  if (typeof window !== 'undefined' && isFunction(window.fetch)) {
+    return _bind(window.fetch, window) as FetchEsque;
   }
 
-  if (typeof globalThis !== 'undefined' && typeof globalThis.fetch) {
-    return globalThis.fetch as FetchEsque;
+  if (typeof globalThis !== 'undefined' && isFunction(globalThis.fetch)) {
+    return _bind(globalThis.fetch, globalThis) as FetchEsque;
   }
 
   throw new Error('No fetch implementation found');
