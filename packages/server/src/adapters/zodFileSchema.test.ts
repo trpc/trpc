6752a54266b144c42485aca58fb08336bbe12962
KATT <alexander@n1s.se>
2023-03-03 18:21:58 +0100

    cool

---
+++ packages/server/src/adapters/zodFileSchema.test.ts
@@ -0,0 +1,39 @@
+import { zodFileSchema } from './zodFileSchema';
+
+type Input = typeof zodFileSchema['_input'];
+
+type FileListEsque = {
+  item(item: number): File | null;
+};
+
+test('File', async () => {
+  const input: Input = new File([], 'test.png');
+  await zodFileSchema.parseAsync(input);
+});
+
+test('stream', async () => {
+  // covered by other tests
+});
+
+test('FileList', async () => {
+  const fileListEsque: FileListEsque = {
+    item(_item: number) {
+      return new File([], 'test.png');
+    },
+  };
+
+  await zodFileSchema.parseAsync(fileListEsque);
+});
+
+test('optional', async () => {
+  const optional = zodFileSchema.optional();
+
+  const fileListEsque: FileListEsque = {
+    item() {
+      return null;
+    },
+  };
+
+  await optional.parseAsync(undefined);
+  await optional.parseAsync(fileListEsque);
+});
