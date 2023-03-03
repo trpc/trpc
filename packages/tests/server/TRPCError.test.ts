4a32a23e7c6fc4bfcd38006c57f20d44e068d514
KATT <alexander@n1s.se>
2023-03-03 10:38:13 +0100

    rm unnecessary closure

--- packages/tests/server/TRPCError.test.ts
+++ packages/tests/server/TRPCError.test.ts
@@ -1,77 +1,75 @@
 import { TRPCError, getTRPCErrorFromUnknown } from '@trpc/server/src';
 
-describe('TRPCError', () => {
-  it('should extend original Error class', () => {
-    const trpcError = new TRPCError({ code: 'FORBIDDEN' });
-    expect(trpcError).toBeInstanceOf(TRPCError);
-    expect(trpcError).toBeInstanceOf(Error);
-  });
+test('should extend original Error class', () => {
+  const trpcError = new TRPCError({ code: 'FORBIDDEN' });
+  expect(trpcError).toBeInstanceOf(TRPCError);
+  expect(trpcError).toBeInstanceOf(Error);
+});
 
-  it('should populate name field using the class name', () => {
-    const trpcError = new TRPCError({ code: 'FORBIDDEN' });
-    expect(trpcError.name).toEqual('TRPCError');
-  });
+test('should populate name field using the class name', () => {
+  const trpcError = new TRPCError({ code: 'FORBIDDEN' });
+  expect(trpcError.name).toEqual('TRPCError');
+});
 
-  it('should use message when one is provided', () => {
-    const trpcError = new TRPCError({ code: 'FORBIDDEN', message: 'wat' });
-    expect(trpcError.message).toEqual('wat');
-  });
+test('should use message when one is provided', () => {
+  const trpcError = new TRPCError({ code: 'FORBIDDEN', message: 'wat' });
+  expect(trpcError.message).toEqual('wat');
+});
 
-  it('should fallback to using code as a message when one is not provided', () => {
-    const trpcError = new TRPCError({ code: 'FORBIDDEN' });
-    expect(trpcError.message).toEqual('FORBIDDEN');
-  });
+test('should fallback to using code as a message when one is not provided', () => {
+  const trpcError = new TRPCError({ code: 'FORBIDDEN' });
+  expect(trpcError.message).toEqual('FORBIDDEN');
+});
 
-  it('should correctly assign the cause when error instance is provided', () => {
-    const originalError = new Error('morty');
-    const trpcError = new TRPCError({
-      code: 'FORBIDDEN',
-      cause: originalError,
-    });
-    expect(trpcError.cause).toBe(originalError);
+test('should correctly assign the cause when error instance is provided', () => {
+  const originalError = new Error('morty');
+  const trpcError = new TRPCError({
+    code: 'FORBIDDEN',
+    cause: originalError,
   });
+  expect(trpcError.cause).toBe(originalError);
+});
 
-  it('should be able to create synthetic cause from string', () => {
-    const trpcError = new TRPCError({ code: 'FORBIDDEN', cause: 'rick' });
-    expect(trpcError.cause).toBeInstanceOf(Error);
-    expect(trpcError.cause?.message).toEqual('rick');
-  });
+test('should be able to create synthetic cause from string', () => {
+  const trpcError = new TRPCError({ code: 'FORBIDDEN', cause: 'rick' });
+  expect(trpcError.cause).toBeInstanceOf(Error);
+  expect(trpcError.cause?.message).toEqual('rick');
+});
 
-  it('should skip creating the cause if one is not provided', () => {
-    const trpcError = new TRPCError({ code: 'FORBIDDEN' });
-    expect(trpcError.cause).toBeUndefined();
-  });
+test('should skip creating the cause if one is not provided', () => {
+  const trpcError = new TRPCError({ code: 'FORBIDDEN' });
+  expect(trpcError.cause).toBeUndefined();
+});
 });
 
 describe('getTRPCErrorFromUnknown', () => {
-  it('should return same error if its already TRPCError instance', () => {
-    const originalError = new TRPCError({ code: 'FORBIDDEN' });
-    const trpcError = getTRPCErrorFromUnknown(originalError);
-    expect(trpcError).toBe(originalError);
-  });
+test('should return same error if its already TRPCError instance', () => {
+  const originalError = new TRPCError({ code: 'FORBIDDEN' });
+  const trpcError = getTRPCErrorFromUnknown(originalError);
+  expect(trpcError).toBe(originalError);
+});
 
-  it('should create new instance of TRPCError with `INTERNAL_SERVER_ERROR` code and same message for non-errors', () => {
-    const originalError = 'rick';
-    const trpcError = getTRPCErrorFromUnknown(originalError);
-    expect(trpcError).toBeInstanceOf(TRPCError);
-    expect(trpcError.message).toEqual('rick');
-    expect(trpcError.cause).toBeInstanceOf(Error);
-    expect(trpcError.cause?.message).toEqual('rick');
-  });
+test('should create new instance of TRPCError with `INTERNAL_SERVER_ERROR` code and same message for non-errors', () => {
+  const originalError = 'rick';
+  const trpcError = getTRPCErrorFromUnknown(originalError);
+  expect(trpcError).toBeInstanceOf(TRPCError);
+  expect(trpcError.message).toEqual('rick');
+  expect(trpcError.cause).toBeInstanceOf(Error);
+  expect(trpcError.cause?.message).toEqual('rick');
+});
 
-  it('should create new instance of TRPCError with `INTERNAL_SERVER_ERROR` code and proper cause for errors', () => {
-    const originalError = new Error('morty');
-    const trpcError = getTRPCErrorFromUnknown(originalError);
-    expect(trpcError).toBeInstanceOf(TRPCError);
-    expect(trpcError.message).toEqual('morty');
-    expect(trpcError.cause).toBe(originalError);
-    expect(trpcError.cause?.message).toEqual('morty');
-  });
+test('should create new instance of TRPCError with `INTERNAL_SERVER_ERROR` code and proper cause for errors', () => {
+  const originalError = new Error('morty');
+  const trpcError = getTRPCErrorFromUnknown(originalError);
+  expect(trpcError).toBeInstanceOf(TRPCError);
+  expect(trpcError.message).toEqual('morty');
+  expect(trpcError.cause).toBe(originalError);
+  expect(trpcError.cause?.message).toEqual('morty');
+});
 
-  it('should preserve original stack in case new instance of TRPCError is created', () => {
-    const originalError = new Error('picklerick');
-    originalError.stack = 'meeseeks';
-    const trpcError = getTRPCErrorFromUnknown(originalError);
-    expect(trpcError.stack).toEqual('meeseeks');
-  });
+test('should preserve original stack in case new instance of TRPCError is created', () => {
+  const originalError = new Error('picklerick');
+  originalError.stack = 'meeseeks';
+  const trpcError = getTRPCErrorFromUnknown(originalError);
+  expect(trpcError.stack).toEqual('meeseeks');
 });
