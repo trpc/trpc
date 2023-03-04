e0d6a2d4e2463047f4cd1a7eeb2e79d5c499fc6d
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-04 18:32:00 +0100

    fix url

--- examples/next-app-router/src/components/Greeting.tsx
+++ examples/next-app-router/src/components/Greeting.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { cache, use } from 'react';
+import { use } from 'react';
 import { api } from 'trpc-api';
 
-const greetingCache = cache(() => api.greeting.query({ text: 'from client' }));
-
 export const Greeting = () => {
-  const greeting = use(greetingCache());
+  const greeting = use(api.greeting.query({ text: 'from client' }));
 
   return <>{greeting}</>;
 };
--- examples/next-app-router/src/trpc/client.ts
+++ examples/next-app-router/src/trpc/client.ts
@@ -2,12 +2,18 @@ import { httpBatchLink } from '@trpc/client';
 import { createTRPCNextAppRouter } from '@trpc/next-app-router/client';
 import { AppRouter } from '~/server/router';
 
+function getBaseUrl() {
+  if (typeof window !== 'undefined') return '';
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return 'http://localhost:3000';
+}
+
 export const api = createTRPCNextAppRouter<AppRouter>({
   config() {
     return {
       links: [
         httpBatchLink({
-          url: '/api/trpc',
+          url: getBaseUrl() + '/api/trpc',
         }),
       ],
     };
