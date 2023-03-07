532f4a0830336635f7f4c3b42533f30e56153d7d
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-07 12:40:18 +0100

    add streaming example

--- examples/next-app-router/src/app/page.tsx
+++ examples/next-app-router/src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Suspense } from 'react';
 import { api } from 'trpc-api';
-import { Greeting } from '~/components/Greeting';
+import { Greeting } from './client-greeting';
 
 export default async function Home() {
   const [result, result2] = await Promise.all([
     api.greeting.query({ text: 'from server' }),
     api.greeting.query({ text: 'from server2' }),
   ]);
+
+  const promise = new Promise(async (res) => {
+    await new Promise((r) => setTimeout(r, 1000)); // wait for demo purposes
+    res(api.greeting.query({ text: 'streamed server data' }));
+  });
 
   return (
     <main>
@@ -15,6 +20,16 @@ export default async function Home() {
       </Suspense>
       <div>{result}</div>
       <div>{result2}</div>
+      <Suspense fallback={<>Loading stream...</>}>
+        {/** @ts-expect-error - Async Server Component */}
+        <StreamedSC promise={promise} />
+      </Suspense>
     </main>
   );
+}
+
+async function StreamedSC(props: { promise: Promise<string> }) {
+  const data = await props.promise;
+
+  return <div>{data}</div>;
 }
