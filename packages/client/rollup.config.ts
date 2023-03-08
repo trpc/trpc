5f1cee01926a9e30c1481f30ca77cfe2f68ea8ea
Sachin Raja <me@s4n.land>
2023-03-08 08:31:23 -0800

    revert client package changes

--- packages/client/rollup.config.ts
+++ packages/client/rollup.config.ts
@@ -8,7 +8,6 @@ export const input = [
   'src/links/splitLink.ts',
   'src/links/loggerLink.ts',
   'src/links/wsLink.ts',
-  'src/links/dedupeLink.ts',
 ];
 
 export default function rollup(): RollupOptions[] {
