8b7a99db14ea7203c6dfaedcb4c6032f58283aab
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-03-09 16:16:33 +0000

    adds some more test

--- packages/tests/server/router.test.ts
+++ packages/tests/server/router.test.ts
@@ -1,5 +1,6 @@
 import './___packages';
 import { initTRPC } from '@trpc/server/src/core';
+import { z } from 'zod';
 
 const t = initTRPC.create();
 
@@ -85,5 +86,76 @@ describe('router', () => {
     const caller = router.createCaller({});
     const result = await caller.foo.bar.foo.bar.foo.bar();
     expect(result).toBe('Hello I am recursive');
+  });
+
+  test('combined router object should be accessible', async () => {
+    const bar = {
+      foo: t.procedure.query(() => 'Hello I am recursive'),
+    };
+
+    const router = t.router({
+      foo: {
+        bar,
+      },
+    });
+
+    const caller = router.createCaller({});
+
+    const result = await caller.foo.bar.foo();
+    expect(result).toBe('Hello I am recursive');
+  });
+
+  test('realistic nested router should be accessible', async () => {
+    const posts = [
+      {
+        id: '1',
+        title: 'Post 1',
+      },
+      {
+        id: '2',
+        title: 'Post 2',
+      },
+      {
+        id: '3',
+        title: 'Post 3',
+      },
+    ];
+
+    const router = t.router({
+      post: {
+        find: {
+          all: t.procedure.query(() => posts),
+          byId: t.procedure
+            .input(z.object({ id: z.string() }))
+            .query(({ input }) => {
+              const post = posts.find((post) => post.id === input.id);
+              return post;
+            }),
+        },
+        create: {
+          one: t.procedure
+            .input(z.object({ title: z.string() }))
+            .mutation(({ input }) => {
+              const newPost = {
+                id: String(posts.length + 1),
+                title: input.title,
+              };
+
+              posts.push(newPost);
+
+              return newPost;
+            }),
+        },
+      },
+    });
+
+    const caller = router.createCaller({});
+
+    expect(await caller.post.find.all()).toEqual(posts);
+    expect(await caller.post.find.byId({ id: '1' })).toEqual(posts[0]);
+    expect(await caller.post.create.one({ title: 'Post 4' })).toEqual({
+      id: '4',
+      title: 'Post 4',
+    });
   });
 });
