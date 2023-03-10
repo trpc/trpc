2f14a0a5adbeeac9e0e8656dae820f816180b42f
KATT <alexander@n1s.se>
2023-03-10 18:08:27 +0100

    tweak

--- packages/server/src/shared/index.ts
+++ packages/server/src/shared/index.ts
@@ -2,4 +2,4 @@ export * from './createProxy';
 export * from './jsonify';
 
 // For `.d.ts` files https://github.com/trpc/trpc/issues/3943
-export { SerializeObject } from './internal/serialize';
+export type { SerializeObject } from './internal/serialize';
