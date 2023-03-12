0b642c0af560f2cf9eb1c16b18b43a2178cc97f1
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-03-12 12:03:23 -0500

    little refactor

--- examples/next-app-router/src/app/page.tsx
+++ examples/next-app-router/src/app/page.tsx
@@ -3,9 +3,9 @@ import { api } from 'trpc-api';
 import { Greeting } from './client-greeting';
 
 export default async function Home() {
-  const [result, result2] = await Promise.all([
-    api.greeting.query({ text: 'from server' }),
-    api.greeting.query({ text: 'from server2' }),
+  const results = await Promise.all([
+    api.greeting.query({ id: 1, text: 'from server' }),
+    api.greeting.query({ id: 2, text: 'from server2' }),
   ]);
 
   const promise = new Promise(async (res) => {
@@ -18,8 +18,9 @@ export default async function Home() {
       <Suspense fallback={<>Loading client...</>}>
         <Greeting />
       </Suspense>
-      <div>{result}</div>
-      <div>{result2}</div>
+      {results.map((result) => (
+        <div key={result.id}>{result}</div>
+      ))}
       <Suspense fallback={<>Loading stream...</>}>
         {/** @ts-expect-error - Async Server Component */}
         <StreamedSC promise={promise} />
