fb895d8353fca4798d06afea2f71f0cb0ea8ecf6
KATT <alexander@n1s.se>
2023-03-16 18:21:41 +0100

    tweak

--- examples/next-prisma-starter/src/utils/transformer.ts
+++ examples/next-prisma-starter/src/utils/transformer.ts
@@ -1,5 +1,6 @@
 /**
  * If you need to add transformers for special data types like `Temporal.Instant` or `Temporal.Date`, `Decimal.js`, etc you can do so here.
+ * Make sure to import this file rather than `superjson` directly.
  * @see https://github.com/blitz-js/superjson#recipes
  */
 import superjson from 'superjson';
