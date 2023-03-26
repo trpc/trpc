ace2c47c3582f8ec4520df5168e94034161197f3
KATT <alexander@n1s.se>
2023-03-26 21:45:53 +0200

    clean

--- examples/next-app-router/src/app/page.tsx
+++ examples/next-app-router/src/app/page.tsx
@@ -4,11 +4,6 @@ import { ClientGreeting } from './client-greeting';
 import { ServerGreeting } from './server-greeting';
 
 export default async function Home() {
-  const [result, result2] = await Promise.all([
-    api.greeting.query({ text: 'from server' }),
-    api.greeting.query({ text: 'from server2' }),
-  ]);
-
   const promise = new Promise(async (resolve) => {
     await new Promise((r) => setTimeout(r, 1000)); // wait for demo purposes
     resolve(api.greeting.query({ text: 'streamed server data' }));
