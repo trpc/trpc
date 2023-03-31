fd3f47cf579522109bd209582a4f6f939b5c57d5
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-31 20:15:24 +0200

    rm unused

--- examples/next-13-starter/src/trpc/client.ts
+++ examples/next-13-starter/src/trpc/client.ts
@@ -1,7 +1,6 @@
 import { httpBatchLink, httpLink, loggerLink, splitLink } from '@trpc/client';
 import { createTRPCNextAppRouterClient } from '@trpc/next-app-router/client';
 import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
-// import { headers } from 'next/headers';
 import { AppRouter } from '~/server/api/router';
 import { getUrl, transformer } from './shared';
 
@@ -19,22 +18,10 @@ export const api = createTRPCNextAppRouterClient<AppRouter>({
           true: httpLink({
             url: getUrl(),
             // fetch: (url, opts) => fetch(url, { ...opts, cache: 'no-store' }),
-            // headers: () => {
-            //   const { connection: _, ...h } = Object.fromEntries(
-            //     Object.entries(headers() || {}),
-            //   );
-            //   return h;
-            // },
           }),
           false: httpBatchLink({
             url: getUrl(),
             // fetch: (url, opts) => fetch(url, { ...opts, cache: 'no-store' }),
-            // headers: () => {
-            //   const { connection: _, ...h } = Object.fromEntries(
-            //     Object.entries(headers() || {}),
-            //   );
-            //   return h;
-            // },
           }),
         }),
       ],
