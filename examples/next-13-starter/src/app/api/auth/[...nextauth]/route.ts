211952877be4424f3a08634c4d5074192372193b
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-04-10 17:01:08 +0200

    route handlers

--- examples/next-13-starter/src/app/api/auth/[...nextauth]/route.ts
+++ examples/next-13-starter/src/app/api/auth/[...nextauth]/route.ts
@@ -24,4 +24,5 @@ export const authOptions: NextAuthOptions = {
   },
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
