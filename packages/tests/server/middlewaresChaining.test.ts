5996c8b7db9bd0c19cd23485d1b5403217cf7565
Nick Lucas <me@nicklucas.co.uk>
2023-04-17 22:36:51 +0100

    Add inline test case

--- packages/tests/server/middlewaresChaining.test.ts
+++ packages/tests/server/middlewaresChaining.test.ts
@@ -16,18 +16,70 @@ test('middleware', () => {
   });
 
   t.procedure
-    .extend(experimentalExtension)
     .input(z.object({ name: z.string() }))
+    .extend(experimentalExtension)
+    .input(z.object({ name2: z.string() }))
     .query((opts) => {
-      // @ts-expect-error number can't be a boolean
-      const orgId = opts.input.orgId as boolean;
-
+      opts.input.name;
       // @ts-expect-error string can't be a boolean
       const name = opts.input.name as boolean;
 
+      opts.input.orgId;
+      // @ts-expect-error number can't be a boolean
+      const orgId = opts.input.orgId as boolean;
+
+      opts.input.name2;
+      // @ts-expect-error string can't be a boolean
+      const name2 = opts.input.name2 as boolean;
+
+      opts.ctx.orgPermitted;
       // @ts-expect-error boolean can't be a string
       const orgOk = opts.ctx.orgPermitted as string;
 
+      opts.ctx.userId;
+      // @ts-expect-error number can't be a boolean
+      const userId = opts.ctx.userId as boolean;
+
+      if (!orgOk) {
+        throw new TRPCError({
+          code: 'FORBIDDEN',
+          message: `User ${userId} does not have access to ${orgId}`,
+        });
+      }
+
+      return `Hello, ${name} from org ${orgId}`;
+    });
+
+  t.procedure
+    .input(z.object({ name: z.string() }))
+    .extend((proc) => {
+      return proc.input(z.object({ orgId: z.number() })).use(({ next }) => {
+        return next({
+          ctx: {
+            orgPermitted: true,
+          },
+        });
+      });
+    })
+    .input(z.object({ name2: z.string() }))
+    .query((opts) => {
+      opts.input.name;
+      // @ts-expect-error string can't be a boolean
+      const name = opts.input.name as boolean;
+
+      opts.input.orgId;
+      // @ts-expect-error number can't be a boolean
+      const orgId = opts.input.orgId as boolean;
+
+      opts.input.name2;
+      // @ts-expect-error string can't be a boolean
+      const name2 = opts.input.name2 as boolean;
+
+      opts.ctx.orgPermitted;
+      // @ts-expect-error boolean can't be a string
+      const orgOk = opts.ctx.orgPermitted as string;
+
+      opts.ctx.userId;
       // @ts-expect-error number can't be a boolean
       const userId = opts.ctx.userId as boolean;
 
