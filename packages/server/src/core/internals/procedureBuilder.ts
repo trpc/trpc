2940349c2f05589b96fd18effc48590062c315c0
Nick Lucas <me@nicklucas.co.uk>
2023-04-19 22:13:36 +0100

    Remove dead code

--- packages/server/src/core/internals/procedureBuilder.ts
+++ packages/server/src/core/internals/procedureBuilder.ts
@@ -156,21 +156,9 @@ export interface ProcedureBuilder<TParams extends ProcedureParams> {
   /**
    * Add a procedure extension, which may append freely to the ProcedureBuilder instance.
    */
-  // extend<$NextBuilder extends AnyProcedureBuilder>(
-  //   extender: (procedure: ProcedureBuilder<TParams>) => $NextBuilder,
-  // ): $NextBuilder;
-  // extend<$ProcedureBuilder extends AnyProcedureBuilder>(
-  //   proc: (base: AnyProcedureBuilder) => $ProcedureBuilder,
-  // ): $ProcedureBuilder extends ProcedureBuilder<infer $TParams>
-  //   ? CreateProcedureReturnInput2<TParams, $TParams>
-  //   : never;
   extend<$Params extends ProcedureParams>(
     proc: (base: AnyProcedureBuilder) => ProcedureBuilder<$Params>,
   ): CreateProcedureReturnInput2<TParams, $Params>;
-  // extend<$Params extends TParams>(
-  //   fn: (procedure: ProcedureBuilder<TParams>) => ProcedureBuilder<$Params>,
-  // ): ProcedureBuilder<$Params>;
-
   /**
    * Extend the procedure with another procedure.
    * @warning The TypeScript inference fails when chaining concatenated procedures.
