6f1da9ba0cab81f6253d1aef26fa438fd0ed2d2c
Alex / KATT <alexander@n1s.se>
2023-04-26 11:48:04 +0200

    docs: remove announcement bar
    v10 is hardly new anymore

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -48,14 +48,14 @@ module.exports = {
       // contextualSearch: true,
       // searchParameters: {},
     },
-    announcementBar: {
-      id: 'v10',
-      content:
-        "🚀 You are looking at tRPC <strong>version 10</strong>! Read the <a href='/blog/announcing-trpc-10'>announcement post</a> or see the <a href='/docs/migrate-from-v9-to-v10'>migration guide</a> if you're currently using tRPC v9",
-      backgroundColor: 'var(--ifm-color-primary-dark)',
-      textColor: '#ffffff',
-      isCloseable: true,
-    },
+    //     announcementBar: {
+    //       id: 'v10',
+    //       content:
+    //         "🚀 You are looking at tRPC <strong>version 10</strong>! Read the <a href='/blog/announcing-trpc-10'>announcement post</a> or see the <a href='/docs/migrate-from-v9-to-v10'>migration guide</a> if you're currently using tRPC v9",
+    //       backgroundColor: 'var(--ifm-color-primary-dark)',
+    //       textColor: '#ffffff',
+    //       isCloseable: true,
+    //     },
     navbar: {
       title: 'tRPC',
       logo: {
