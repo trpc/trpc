74d7ef849de3853312996a7715e0a6a969849a54
KATT <alexander@n1s.se>
2023-04-28 16:35:46 +0200

    fix

--- packages/tests/server/react/deprecated/overrides.test.tsx
+++ packages/tests/server/react/deprecated/overrides.test.tsx
@@ -1,4 +1,4 @@
-import { routerToServerAndClientNew } from '../___testHelpers';
+import { routerToServerAndClientNew } from '../../___testHelpers';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
