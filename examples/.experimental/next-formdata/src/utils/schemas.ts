c3d7a022f3b0c7acdc6f81b416b7dcc6dcff73b3
KATT <alexander@n1s.se>
2023-04-28 16:30:52 +0200

    lint

--- examples/.experimental/next-formdata/src/utils/schemas.ts
+++ examples/.experimental/next-formdata/src/utils/schemas.ts
@@ -1,8 +1,9 @@
 import { zfd } from 'zod-form-data';
 
 if (typeof window === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const undici = require('undici');
-  globalThis.File = undici.File as any;
+  globalThis.File = undici.File;
 }
 
 export const uploadFileSchema = zfd.formData({
