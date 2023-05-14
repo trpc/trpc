e5d1f595be7c77e6c5617b4082074ea2741c6a7c
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-05-14 12:37:06 -0500

    fixes

--- examples/next-prisma-starter/tailwind.config.ts
+++ examples/next-prisma-starter/tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} as Config;
