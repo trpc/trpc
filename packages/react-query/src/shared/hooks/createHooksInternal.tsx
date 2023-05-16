4a55fa7667270b2e310e645b4d0c5c7d3a0fd982
Haaxor1689 <betkomaros@gmail.com>
2023-05-16 22:20:03 +0200

    Remove spread of defaultOpts in useMutation wrapper since they should already come from the context

--- packages/react-query/src/shared/hooks/createHooksInternal.tsx
+++ packages/react-query/src/shared/hooks/createHooksInternal.tsx
@@ -342,7 +342,6 @@ export function createRootHooks<
     ]);
 
     const hook = __useMutation({
-      ...defaultOpts,
       ...opts,
       mutationKey: [actualPath.split('.')],
       mutationFn: (input) => {
