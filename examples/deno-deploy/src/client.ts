8b2bfc34e4e5b0da156b77f105c24b5903135191
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-21 17:27:37 +1000

    fix

--- examples/deno-deploy/src/client.ts
+++ examples/deno-deploy/src/client.ts
@@ -1,13 +1,13 @@
-import { delay } from "https://deno.land/std@0.188.0/async/delay.ts";
+import { delay } from 'https://deno.land/std@0.188.0/async/delay.ts';
 import {
   createTRPCProxyClient,
   httpBatchLink,
   loggerLink,
-} from "npm:@trpc/client";
-import type { AppRouter } from "./router.ts";
+} from 'npm:@trpc/client';
+import type { AppRouter } from './router.ts';
 
 async function main() {
-  const url = "http://127.0.0.1:8000/trpc";
+  const url = 'http://127.0.0.1:8000/trpc';
 
   const proxy = createTRPCProxyClient<AppRouter>({
     links: [loggerLink(), httpBatchLink({ url })],
@@ -19,21 +19,21 @@ async function main() {
   await Promise.all([
     //
     proxy.hello.query(),
-    proxy.hello.query("client"),
+    proxy.hello.query('client'),
   ]);
   await delay(100);
 
   const postCreate = await proxy.post.createPost.mutate({
-    title: "hello client",
+    title: 'hello client',
   });
-  console.log("created post", postCreate.title);
+  console.log('created post', postCreate.title);
   await delay(100);
 
   const postList = await proxy.post.listPosts.query();
-  console.log("has posts", postList, "first:", postList[0].title);
+  console.log('has posts', postList, 'first:', postList[0].title);
   await delay(100);
 
-  console.log("👌 should be a clean exit if everything is working right");
+  console.log('👌 should be a clean exit if everything is working right');
 }
 
 main();
