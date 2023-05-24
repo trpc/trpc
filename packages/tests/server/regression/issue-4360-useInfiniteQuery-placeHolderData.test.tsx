619eefa7b997caa1c4acea775775e37dd57b808e
Alex / KATT <alexander@n1s.se>
2023-05-24 22:42:34 +0200

    Update packages/tests/server/regression/issue-4360-useInfiniteQuery-placeHolderData.test.tsx

--- packages/tests/server/regression/issue-4360-useInfiniteQuery-placeHolderData.test.tsx
+++ packages/tests/server/regression/issue-4360-useInfiniteQuery-placeHolderData.test.tsx
@@ -1,6 +1,6 @@
 import { ignoreErrors } from '../___testHelpers';
 import { getServerAndReactClient } from '../react/__reactHelpers';
-import { initTRPC } from '@trpc/server/src';
+import { initTRPC } from '@trpc/server';
 import { konn } from 'konn';
 import { z } from 'zod';
 
