804819d029aa20d4c81602e79980447b5cb82219
fallenfreq <michael@catsdog.co.uk>
2023-05-25 02:46:42 +0100

    Add query as unknonw type

--- packages/server/src/adapters/node-http/types.ts
+++ packages/server/src/adapters/node-http/types.ts
@@ -6,6 +6,7 @@ import { NodeHTTPContentTypeHandler } from './internals/contentType';
 
 export type NodeHTTPRequest = IncomingMessage & {
   body?: unknown;
+  query?: unknown;
 };
 export type NodeHTTPResponse = ServerResponse;
 
