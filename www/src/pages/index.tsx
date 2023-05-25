e56db385086f111e4ab4dc9398d851cd79f57fa4
Alex / KATT <alexander@n1s.se>
2023-05-25 11:51:12 +0200

    Update www/src/pages/index.tsx
    Co-authored-by: Christopher Ehrlich <ehrlich.christopher@gmail.com>

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -72,7 +72,7 @@ function ErrorOverlay(props: { url: string; dismiss: () => void }) {
           Heads up!
         </div>
         <div className="w-full max-w-xl text-lg font-medium text-zinc-700 dark:text-zinc-200">
-          WebContainers require modern Web APIs that likely isn&apos;t supported
+          WebContainers require modern Web APIs that likely aren&apos;t supported
           by your browser. Either open this page in a different browser, or
           download this sandbox from Stackblitz and run it locally.
         </div>
