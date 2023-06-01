2148aeb7c70c416df7ef792e471c299d071b8041
KATT <alexander@n1s.se>
2023-06-01 16:18:54 +0200

    cool

--- examples/.experimental/next-app-dir/src/app/stream/page.tsx
+++ examples/.experimental/next-app-dir/src/app/stream/page.tsx
@@ -18,10 +18,29 @@ async function wait(ms: number) {
 
   return `waited ${ms}ms` as const;
 }
+
+function isBrowser() {
+  return typeof window !== 'undefined';
+}
+
+const clientCache: Record<string, Shape> = {};
+
 function MyComponent(props: { wait: number }) {
+  const cacheKey = JSON.stringify(props);
+
   const ctx = use(context);
-  const data = use(wait(props.wait));
-  ctx.stream.push({ key: `${props.wait}`, data });
+  // This should be lib code:
+  let data: Awaited<ReturnType<typeof wait>>;
+
+  if (isBrowser() && clientCache[cacheKey]) {
+    console.log('browser read from cache!!!!!!!!', { cacheKey });
+    data = clientCache[cacheKey].data as Awaited<ReturnType<typeof wait>>;
+  } else {
+    console.error('no cache :(');
+    data = use(wait(props.wait));
+  }
+
+  ctx.stream.push({ key: cacheKey, data });
 
   return <div>{data}</div>;
 }
@@ -30,17 +49,20 @@ export default function DefaultPage() {
     <HydrationStreamProvider
       onEntries={(entries) => {
         console.log('received entries', entries, entries.length);
+        for (const entry of entries) {
+          clientCache[entry.key] = entry;
+        }
       }}
     >
-      <Suspense fallback={<div>waiting for 3</div>}>
+      <Suspense fallback={<div>waiting for 3 entries...</div>}>
         <MyComponent wait={100} />
         <MyComponent wait={100} />
         <MyComponent wait={100} />
       </Suspense>
-      <Suspense fallback={<div>waiting for 1</div>}>
+      <Suspense fallback={<div>waiting for 1 entry....</div>}>
         <MyComponent wait={1000} />
       </Suspense>
-      <Suspense fallback={<div>waiting for 2</div>}>
+      <Suspense fallback={<div>waiting for 2 entries....</div>}>
         <MyComponent wait={2000} />
         <MyComponent wait={3000} />
       </Suspense>
