51e8efa3851096ab1bd935a78e8f81ed426cf38b
KATT <alexander@n1s.se>
2023-06-01 20:03:57 +0200

    cleanup

--- examples/.experimental/next-app-dir-react-query/src/server/context.ts
+++
@@ -1,9 +0,0 @@
-import { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
-
-export function createContext(opts?: FetchCreateContextFnOptions) {
-  return {
-    headers: opts && Object.fromEntries(opts.req.headers),
-  };
-}
-
-export type Context = Awaited<ReturnType<typeof createContext>>;
--- examples/.experimental/next-app-dir-react-query/src/trpc/shared.ts
+++
@@ -1,9 +0,0 @@
-function getBaseUrl() {
-  if (typeof window !== 'undefined') return '';
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return 'http://localhost:3000';
-}
-
-export function getUrl() {
-  return getBaseUrl() + '/api/trpc';
-}
