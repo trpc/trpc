9a9364ca38840509c5ac6cc7f34c9541baf9bfb5
juliusmarminge <julius0216@outlook.com>
2023-06-08 11:25:53 +0200

    patch

---
+++ packages/tests/server/react/regression/issue-4486-initialData-types.test.tsx
@@ -0,0 +1,113 @@
+import { ignoreErrors } from '../../___testHelpers';
+import { getServerAndReactClient } from '../__reactHelpers';
+import { initTRPC } from '@trpc/server/src';
+import { konn } from 'konn';
+
+/**
+ * For reference,
+ * @see https://github.com/trpc/trpc/issues/4486
+ */
+
+const fixtureData = ['1', '2'];
+
+const ctx = konn()
+  .beforeEach(() => {
+    const t = initTRPC.create();
+
+    const appRouter = t.router({
+      post: t.router({
+        list: t.procedure.query(() => ({
+          posts: fixtureData,
+          foo: 'bar' as const,
+        })),
+      }),
+    });
+
+    return getServerAndReactClient(appRouter);
+  })
+  .afterEach(async (ctx) => {
+    await ctx?.close?.();
+  })
+  .done();
+
+describe('placeholderData', async () => {
+  test('invalid placeholderData should typeerror', () => {
+    const { proxy } = ctx;
+
+    ignoreErrors(() => {
+      proxy.post.list.useQuery(undefined, {
+        // @ts-expect-error can't return page data that doesn't match the output type
+        placeholderData() {
+          return {
+            barbaz: null,
+          };
+        },
+      });
+    });
+
+    ignoreErrors(() => {
+      proxy.post.list.useQuery(undefined, {
+        // @ts-expect-error can't return page data that doesn't match the output type
+        placeholderData: {
+          barbaz: null,
+        },
+      });
+    });
+  });
+
+  test('good placeholderData does not typeerror', () => {
+    const { proxy } = ctx;
+
+    ignoreErrors(() => {
+      proxy.post.list.useQuery(undefined, {
+        placeholderData() {
+          return {
+            posts: [],
+            foo: 'bar',
+          };
+        },
+      });
+    });
+  });
+});
+
+describe('initialData', async () => {
+  test('invalid initialData should typeerror', () => {
+    const { proxy } = ctx;
+
+    ignoreErrors(() => {
+      proxy.post.list.useQuery(undefined, {
+        // @ts-expect-error can't return page data that doesn't match the output type
+        initialData() {
+          return {
+            barbaz: null,
+          };
+        },
+      });
+    });
+
+    ignoreErrors(() => {
+      proxy.post.list.useQuery(undefined, {
+        // @ts-expect-error can't return page data that doesn't match the output type
+        initialData: {
+          barbaz: null,
+        },
+      });
+    });
+  });
+
+  test('good initialData does not typeerror', () => {
+    const { proxy } = ctx;
+
+    ignoreErrors(() => {
+      proxy.post.list.useQuery(undefined, {
+        initialData() {
+          return {
+            posts: [],
+            foo: 'bar',
+          };
+        },
+      });
+    });
+  });
+});
