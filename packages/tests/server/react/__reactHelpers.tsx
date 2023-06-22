ebd06266ae49bd2ed99b1257c1561811d7bf8a9a
KATT <alexander@n1s.se>
2023-06-22 13:08:07 +0200

    cool

--- packages/tests/server/react/__reactHelpers.tsx
+++ packages/tests/server/react/__reactHelpers.tsx
@@ -42,7 +42,7 @@ export function getServerAndReactClient<TRouter extends AnyRouter>(
   const proxy = createTRPCReact<TRouter, unknown, ''>();
   const baseProxy = proxy as CreateTRPCReactBase<TRouter, unknown>;
 
-  const { client } = opts;
+  const client = opts.client;
 
   function App(props: { children: ReactNode }) {
     return (
