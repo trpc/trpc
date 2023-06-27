2f116c94be95ca96afe75ecc6f5597a04b33bbdc
juliusmarminge <julius0216@outlook.com>
2023-06-27 10:56:57 +0200

    comment why edge is disabled

--- examples/.experimental/next-app-dir/src/app/api/auth/[...nextauth]/route.ts
+++ examples/.experimental/next-app-dir/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { options } from '~/auth';
 import NextAuth from 'next-auth';
 
+// Add back once NextAuth v5 is released
 // export const runtime = 'edge';
 
 const handlers = NextAuth(options);
