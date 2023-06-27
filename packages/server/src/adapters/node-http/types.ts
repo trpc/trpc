794a2c8259853609ecf0a8b4f7f106fc2f6d74a2
juliusmarminge <julius0216@outlook.com>
2023-06-27 09:42:11 +0200

    huh

--- packages/server/src/adapters/node-http/types.ts
+++ packages/server/src/adapters/node-http/types.ts
@@ -54,40 +54,42 @@ export type NodeHTTPHandlerOptions<
   TRouter extends AnyRouter,
   TRequest extends NodeHTTPRequest,
   TResponse extends NodeHTTPResponse,
-> = HTTPBaseHandlerOptions<TRouter, TRequest> & NodeHTTPCreateContextOption<TRouter, TRequest, TResponse> & {
-  /**
-   * By default, http `OPTIONS` requests are not handled, and CORS headers are not returned.
-   *
-   * This can be used to handle them manually or via the `cors` npm package: https://www.npmjs.com/package/cors
-   *
-   * ```ts
-   * import cors from 'cors'
-   *
-   * nodeHTTPRequestHandler({
-   *   cors: cors()
-   * })
-   * ```
-   *
-   * You can also use it for other needs which a connect/node.js compatible middleware can solve,
-   *  though you might wish to consider an alternative solution like the Express adapter if your needs are complex.
-   */
-  middleware?: ConnectMiddleware;
-  maxBodySize?: number;
-  experimental_contentTypeHandlers?: NodeHTTPContentTypeHandler<
-    TRequest,
-    TResponse
-  >[];
-};
+> = HTTPBaseHandlerOptions<TRouter, TRequest> &
+  NodeHTTPCreateContextOption<TRouter, TRequest, TResponse> & {
+    /**
+     * By default, http `OPTIONS` requests are not handled, and CORS headers are not returned.
+     *
+     * This can be used to handle them manually or via the `cors` npm package: https://www.npmjs.com/package/cors
+     *
+     * ```ts
+     * import cors from 'cors'
+     *
+     * nodeHTTPRequestHandler({
+     *   cors: cors()
+     * })
+     * ```
+     *
+     * You can also use it for other needs which a connect/node.js compatible middleware can solve,
+     *  though you might wish to consider an alternative solution like the Express adapter if your needs are complex.
+     */
+    middleware?: ConnectMiddleware;
+    maxBodySize?: number;
+    experimental_contentTypeHandlers?: NodeHTTPContentTypeHandler<
+      TRequest,
+      TResponse
+    >[];
+  };
 
 export type NodeHTTPRequestHandlerOptions<
   TRouter extends AnyRouter,
   TRequest extends NodeHTTPRequest,
   TResponse extends NodeHTTPResponse,
-> = NodeHTTPHandlerOptions<TRouter, TRequest, TResponse> & {
+  // eslint-disable-next-line @typescript-eslint/sort-type-constituents
+> = {
   req: TRequest;
   res: TResponse;
   path: string;
-};
+} & NodeHTTPHandlerOptions<TRouter, TRequest, TResponse>;
 
 export type NodeHTTPCreateContextFnOptions<TRequest, TResponse> = {
   req: TRequest;
