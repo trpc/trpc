419882db8b50fac7674dd8effd66015776a4e9bb
juliusmarminge <julius0216@outlook.com>
2023-06-29 14:24:23 +0200

    fix

--- examples/.experimental/next-app-dir/src/app/post-example/page.tsx
+++ examples/.experimental/next-app-dir/src/app/post-example/page.tsx
@@ -1,6 +1,11 @@
+import { readFile } from 'node:fs/promises';
 import { Button } from '~/components/button';
+import { CodeBlock } from '~/components/codeblocks';
+import { CollapsiblePreview } from '~/components/collapsible-preview';
 import { Input } from '~/components/input';
+import { JsonPreTag } from '~/components/json-pretag';
 import { api } from '~/trpc/server-http';
+import { Suspense } from 'react';
 
 async function action(fd: FormData) {
   'use server';
@@ -19,17 +24,39 @@ export default async function PostPage() {
   const latestPost = await api.getLatestPost.query();
 
   return (
-    <div>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <span>Title: {latestPost.title}</span>
-        <span>Content: {latestPost.content}</span>
-        <span>Created At: {latestPost.createdAt.toISOString()}</span>
+    <div className="space-y-4">
+      <div className="prose">
+        <h1>Full Stack Revalidation</h1>
       </div>
+
+      <JsonPreTag object={latestPost} />
+
       <form className="max-w-sm space-y-2" action={action}>
         <Input name="title" placeholder="title" />
         <Input name="content" placeholder="content" />
         <Button type="submit">Create Post!</Button>
       </form>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-bold">Code</h2>
+        <Suspense fallback={'Reading page source...'}>
+          <h3 className="font-semibold">page.tsx</h3>
+          <ComponentCode path="./page.tsx" />
+        </Suspense>
+      </div>
     </div>
+  );
+}
+
+async function ComponentCode(props: { path: string; expandText?: string }) {
+  const fileContent = await readFile(
+    new URL(props.path, import.meta.url),
+    'utf-8',
+  );
+
+  return (
+    <CollapsiblePreview expandButtonTitle={props.expandText}>
+      <CodeBlock code={fileContent} lang="tsx" />
+    </CollapsiblePreview>
   );
 }
