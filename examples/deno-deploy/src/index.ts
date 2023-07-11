451020c4dde63d27ad8929df60f2773425e202b6
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-11 15:43:57 +1000

    lint

--- examples/deno-deploy/src/index.ts
+++ examples/deno-deploy/src/index.ts
@@ -1,15 +1,15 @@
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "./router.ts";
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from './router.ts';
 
 function handler(request: Request) {
   // Only used for start-server-and-test package that
   // expects a 200 OK to start testing the server
-  if (request.method === "HEAD") {
+  if (request.method === 'HEAD') {
     return new Response();
   }
 
   return fetchRequestHandler({
-    endpoint: "/trpc",
+    endpoint: '/trpc',
     req: request,
     router: appRouter,
     createContext: () => ({}),
