9a8085baf8018301dbf953150e755f493a57b396
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-11 15:39:55 +1000

    refactor: use `Deno.serve()`

--- examples/deno-deploy/src/index.ts
+++ examples/deno-deploy/src/index.ts
@@ -1,20 +1,19 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { serve } from 'std/http/server.ts';
-import { appRouter } from './router.ts';
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "./router.ts";
 
 function handler(request: Request) {
   // Only used for start-server-and-test package that
   // expects a 200 OK to start testing the server
-  if (request.method === 'HEAD') {
+  if (request.method === "HEAD") {
     return new Response();
   }
 
   return fetchRequestHandler({
-    endpoint: '/trpc',
+    endpoint: "/trpc",
     req: request,
     router: appRouter,
     createContext: () => ({}),
   });
 }
 
-serve(handler);
+Deno.serve(handler);
