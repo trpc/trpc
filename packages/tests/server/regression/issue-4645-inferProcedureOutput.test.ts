022aa2d8ae7902ca46c63b3dbe5178214f622230
KATT <alexander@n1s.se>
2023-08-11 11:05:38 +0200

    add more

--- packages/tests/server/regression/issue-4645-inferProcedureOutput.test.ts
+++ packages/tests/server/regression/issue-4645-inferProcedureOutput.test.ts
@@ -1,10 +1,11 @@
 import { inferProcedureOutput, initTRPC } from '@trpc/server';
 import { inferTransformedProcedureOutput } from '@trpc/server/shared';
+import SuperJSON from 'superjson';
 import { z } from 'zod';
 
 test('infer json-esque', async () => {
   const t = initTRPC.create();
-  const helloProcedure = t.procedure.input(z.string()).query((opts) => {
+  const helloProcedure = t.procedure.input(z.string()).query(() => {
     return {
       hello: Math.random() > 0.5 ? 'hello' : undefined,
     };
@@ -20,5 +21,27 @@ test('infer json-esque', async () => {
     // Example: JSON.stringify({ hello: undefined }) === '{}'
     type Inferred = inferTransformedProcedureOutput<typeof helloProcedure>;
     expectTypeOf<Inferred>().toEqualTypeOf<{ hello?: string }>();
+  }
+});
+
+test('infer with superjson', async () => {
+  const t = initTRPC.create({
+    transformer: SuperJSON,
+  });
+  const helloProcedure = t.procedure.input(z.string()).query((opts) => {
+    return {
+      hello: Math.random() > 0.5 ? 'hello' : undefined,
+    };
+  });
+
+  {
+    type Inferred = inferProcedureOutput<typeof helloProcedure>;
+    expectTypeOf<Inferred>().toEqualTypeOf<{ hello: string | undefined }>();
+  }
+  {
+    // This type is what the client will receive
+    // Here, we use a transformer which will handle preservation of undefined
+    type Inferred = inferTransformedProcedureOutput<typeof helloProcedure>;
+    expectTypeOf<Inferred>().toEqualTypeOf<{ hello: string | undefined }>();
   }
 });
