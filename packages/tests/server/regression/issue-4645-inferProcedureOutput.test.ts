f29efb34c055c42414dc3100dc68387babec96ca
KATT <alexander@n1s.se>
2023-08-11 11:04:09 +0200

    chore: add test for #4645

---
+++ packages/tests/server/regression/issue-4645-inferProcedureOutput.test.ts
@@ -0,0 +1,24 @@
+import { inferProcedureOutput, initTRPC } from '@trpc/server';
+import { inferTransformedProcedureOutput } from '@trpc/server/shared';
+import { z } from 'zod';
+
+test('infer json-esque', async () => {
+  const t = initTRPC.create();
+  const helloProcedure = t.procedure.input(z.string()).query((opts) => {
+    return {
+      hello: Math.random() > 0.5 ? 'hello' : undefined,
+    };
+  });
+
+  {
+    type Inferred = inferProcedureOutput<typeof helloProcedure>;
+    expectTypeOf<Inferred>().toEqualTypeOf<{ hello: string | undefined }>();
+  }
+  {
+    // This type is what the client will receive
+    // Because it will be sent as JSON, the undefined will be stripped by `JSON.stringify`
+    // Example: JSON.stringify({ hello: undefined }) === '{}'
+    type Inferred = inferTransformedProcedureOutput<typeof helloProcedure>;
+    expectTypeOf<Inferred>().toEqualTypeOf<{ hello?: string }>();
+  }
+});
