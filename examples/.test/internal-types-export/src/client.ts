5a0431a4ee02cc64704379aa1262b8775e4125de
KATT <alexander@n1s.se>
2023-08-24 06:20:44 +0200

    repro attempt

--- examples/.test/internal-types-export/src/client.ts
+++ examples/.test/internal-types-export/src/client.ts
@@ -1,5 +1,6 @@
 import { createTRPCProxyClient, httpLink } from '@trpc/client';
 import type { AppRouter } from './server.js';
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 
 export const client = createTRPCProxyClient<AppRouter>({
   links: [
@@ -8,3 +9,6 @@ export const client = createTRPCProxyClient<AppRouter>({
     })
   ]
 });
+
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
