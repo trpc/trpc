23ddd6768fd25799a5a7bb8625c847e59da9d935
KATT <alexander@n1s.se>
2023-09-11 19:15:55 +0200

    add repro

---
+++ packages/tests/server/regression/issue-4750-no-cancel.test.ts
@@ -0,0 +1,59 @@
+import { routerToServerAndClientNew, waitMs } from '../___testHelpers';
+import { httpBatchLink, httpLink } from '@trpc/client';
+import { initTRPC } from '@trpc/server/src/core';
+
+const t = initTRPC.create();
+
+const router = t.router({
+  q: t.procedure.query(() => {
+    throw 'hello';
+  }),
+});
+
+test('httpLink', async () => {
+  const abortController = new AbortController();
+
+  const { close, proxy } = routerToServerAndClientNew(router, {
+    client({ httpUrl }) {
+      return {
+        links: [httpLink({ url: httpUrl })],
+      };
+    },
+  });
+
+  await proxy.q
+    .query(undefined, {
+      signal: abortController.signal,
+    })
+    .catch(() => {
+      /// ..
+    });
+
+  expect(abortController.signal.aborted).toBe(false);
+
+  await close();
+});
+
+test('httpBatchLink', async () => {
+  const abortController = new AbortController();
+
+  const { close, proxy } = routerToServerAndClientNew(router, {
+    client({ httpUrl }) {
+      return {
+        links: [httpBatchLink({ url: httpUrl })],
+      };
+    },
+  });
+
+  await proxy.q
+    .query(undefined, {
+      signal: abortController.signal,
+    })
+    .catch(() => {
+      /// ..
+    });
+
+  expect(abortController.signal).toBe(false);
+
+  await close();
+});
