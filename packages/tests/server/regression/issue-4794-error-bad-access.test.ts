04bfb26db91ffd877004914d9b344bad8fa16f18
juliusmarminge <julius0216@outlook.com>
2023-09-19 11:38:46 +0200

    Merge branch 'main' into next-merge-2023-09-19

---
+++ packages/tests/server/regression/issue-4794-error-bad-access.test.ts
@@ -0,0 +1,18 @@
+import { TRPCClientError } from '@trpc/client';
+import '../___packages';
+
+test('test passing non error like object to TRPCClientError.from', () => {
+  const cause = null;
+
+  expect(TRPCClientError.from(cause as any)).toMatchInlineSnapshot(
+    '[TRPCClientError: Unknown error]',
+  );
+});
+
+test('empty obj', () => {
+  const cause = {};
+
+  expect(TRPCClientError.from(cause as any)).toMatchInlineSnapshot(
+    '[TRPCClientError: Unknown error]',
+  );
+});
