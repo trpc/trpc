771024484fbc631250db3ce855b273a9b44d5349
KATT <alexander@n1s.se>
2023-09-26 12:08:23 +0200

    chore: make big router more complex

--- examples/.test/big-router-declaration/src/server/context.ts
+++ examples/.test/big-router-declaration/src/server/context.ts
@@ -1,16 +1,23 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 
-interface CreateContextOptions {
-  // session: Session | null
+export interface Session {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+interface CreateContextOptions extends Partial<trpcNext.CreateNextContextOptions> {
+  session: Session | null
 }
 
 /**
  * Inner function for `createContext` where we create the context.
  * This is useful for testing when we don't want to mock Next.js' request/response
  */
-export async function createContextInner(_opts: CreateContextOptions) {
-  return {};
+export async function createContextInner(opts: CreateContextOptions) {
+  return {
+    ...opts
+  };
 }
 
 export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
@@ -22,5 +29,8 @@ export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
 export async function createContext(
   opts: trpcNext.CreateNextContextOptions,
 ): Promise<Context> {
-  return await createContextInner({});
+  return await createContextInner({
+    ...opts,
+    session: null,
+  });
 }
