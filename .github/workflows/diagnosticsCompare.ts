af0ffcf932f4c81c63f301fe20de4613f9a2e754
KATT <alexander@n1s.se>
2023-09-27 02:39:55 +0200

    fix table

--- .github/workflows/diagnosticsCompare.ts
+++ .github/workflows/diagnosticsCompare.ts
@@ -58,8 +58,7 @@ function printTable(
   }
 
   commentBody += '| Metric | PR | `next` | `main` |\n';
-  commentBody +=
-    '|--------|------------------|-------------------|------------------|------------------|------------------|\n';
+  commentBody += '| --- | --- | --- | --- |\n';
 
   // Loop through the metrics and build the comment body
   for (const [metric, currentPrValue] of Object.entries(data)) {
