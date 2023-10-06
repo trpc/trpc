2646f7bf35d8bbcda7de1a14c89e4d54b3558507
KATT <alexander@n1s.se>
2023-10-06 16:50:29 +0200

    fix

--- packages/server/src/adapters/fastify/fastifyTRPCPlugin.ts
+++ packages/server/src/adapters/fastify/fastifyTRPCPlugin.ts
@@ -22,15 +22,14 @@ export function fastifyTRPCPlugin<TRouter extends AnyRouter>(
   opts: FastifyTRPCPluginOptions<TRouter>,
   done: (err?: Error) => void,
 ) {
-  if (!fastify.hasContentTypeParser('application/json')) {
-    fastify.addContentTypeParser(
-      'application/json',
-      { parseAs: 'string' },
-      function (_, body, _done) {
-        _done(null, body);
-      },
-    );
-  }
+  fastify.removeContentTypeParser('application/json');
+  fastify.addContentTypeParser(
+    'application/json',
+    { parseAs: 'string' },
+    function (_, body, _done) {
+      _done(null, body);
+    },
+  );
 
   let prefix = opts.prefix ?? '';
 
