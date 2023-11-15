trpc/trpc

commit f27d50c774908059e263bbbf25e20a782f6faa89

fix(server): inference fix for inputs with middleware (#5017)
* wip

* wip

* ok

* nah

* revert

* col

* rename

* types/utils.ts/Overwrite: only overwrite keys of objects

Overwrite<string,string> resulted in a mangled object type
because the existing implementation iterated over the string
type's keys (e.g. 'charCodeAt')

This commit changes the implementation so that keys are only
iterated and overwritten in the case of objects. Otherwise,
a non-never TWith type will entirely replace TType, e.g.
Overwrite<string, number> results in 'number'.

---------

Co-authored-by: jussisaurio <jussi.saurio@gmail.com>

packages/server/src/core/internals/utils.ts
@@ -3,18 +3,32 @@ import { ProcedureParams } from '../procedure';
 
 /**
  * @internal
+ * Overwrite properties in `TType` with properties in `TWith`
+ * Only overwrites properties when both types are objects
+ * Otherwise it will overwrite the entire TType with TWith,
+ * unless TWith is never.
  */
-export type Overwrite<TType, TWith> = TType extends any
-  ? TWith extends any
-    ? {
+export type Overwrite<TType, TWith> = TType extends object
+  ? TWith extends object
+    ? // Both TType and TWith are objects: overwrite key-by-key
+      {
         [K in keyof TType | keyof TWith]: K extends keyof TWith
           ? TWith[K]
           : K extends keyof TType
           ? TType[K]
           : never;
       }
+    : TWith extends any
+    ? // TWith is not an object but some non-never type, so fully overwrite TType
+      TWith
     : never
+  : TType extends any
+  ? TWith extends any
+    ? // Same as above: just overwrite TType with TWith
+      TWith
+    : TType
   : never;
+
 /**
  * @internal
  */

packages/tests/server/regression/issue-5020-inference-middleware.test.ts
@@ -1,0 +1,40 @@
+import { inferRouterInputs, inferRouterOutputs, initTRPC } from '@trpc/server';
+import { z } from 'zod';
+
+const t = initTRPC.create();
+const appRouter = t.router({
+  str: t.procedure.input(z.string()).query(({ input }) => input),
+  strWithMiddleware: t.procedure
+    // ^?
+    .input(z.string())
+    .use((opts) => opts.next())
+    .query(({ input }) => input),
+
+  voidWithMiddleware: t.procedure
+    .use((opts) => opts.next())
+    .query(() => {
+      // ..
+    }),
+});
+type AppRouter = typeof appRouter;
+
+describe('inferRouterInputs', () => {
+  type AppRouterInputs = inferRouterInputs<AppRouter>;
+  type AppRouterOutputs = inferRouterOutputs<AppRouter>;
+
+  test('string', async () => {
+    {
+      type Input = AppRouterInputs['str'];
+      type Output = AppRouterOutputs['str'];
+      expectTypeOf<Input>().toBeString();
+      expectTypeOf<Output>().toBeString();
+    }
+    {
+      type Input = AppRouterInputs['strWithMiddleware'];
+      //   ^?
+      type Output = AppRouterOutputs['strWithMiddleware'];
+      expectTypeOf<Input>().toBeString();
+      expectTypeOf<Output>().toBeString();
+    }
+  });
+});

commit c63604e70737e6c838fe00190a12c1f5a2f11aab

fix(server): fix regression introduced by #5017

packages/server/src/core/internals/utils.ts
@@ -4,14 +4,12 @@ import { ProcedureParams } from '../procedure';
 /**
  * @internal
  * Overwrite properties in `TType` with properties in `TWith`
- * Only overwrites properties when both types are objects
- * Otherwise it will overwrite the entire TType with TWith,
- * unless TWith is never.
+ * Only overwrites properties when the type to be overwritten
+ * is an object. Otherwise it will just use the type from `TWith`.
  */
-export type Overwrite<TType, TWith> = TType extends object
-  ? TWith extends object
-    ? // Both TType and TWith are objects: overwrite key-by-key
-      {
+export type Overwrite<TType, TWith> = TWith extends any
+  ? TType extends object
+    ? {
         [K in  // Exclude index signature from keys
           | keyof WithoutIndexSignature<TType>
           | keyof WithoutIndexSignature<TWith>]: K extends keyof TWith
@@ -24,15 +22,7 @@ export type Overwrite<TType, TWith> = TType extends object
         : number extends keyof TWith
         ? { [key: number]: TWith[number] }
         : object)
-    : TWith extends any
-    ? // TWith is not an object but some non-never type, so fully overwrite TType
-      TWith
-    : never
-  : TType extends any
-  ? TWith extends any
-    ? // Same as above: just overwrite TType with TWith
-      TWith
-    : TType
+    : TWith
   : never;
 
 /**

packages/server/src/types.ts
@@ -123,7 +123,7 @@ export type DeepPartial<TObject> = TObject extends object
  * type information about the keys and only the index signature will remain.
  * @internal
  */
-export type WithoutIndexSignature<TObj extends object> = {
+export type WithoutIndexSignature<TObj> = {
   [K in keyof TObj as string extends K
     ? never
     : number extends K

packages/tests/server/regression/issue-5037-context-inference.test.ts
@@ -1,0 +1,69 @@
+import { inferRouterOutputs, initTRPC } from '@trpc/server';
+import { z } from 'zod';
+
+/**
+ * See @sentry/node trpc middleware:
+ * https://github.com/getsentry/sentry-javascript/blob/6d424571e3cd5e99991b711f4e23d773e321e294/packages/node/src/handlers.ts#L328
+ */
+interface TrpcMiddlewareArguments<T> {
+  path: string;
+  type: string;
+  next: () => T;
+  rawInput: unknown;
+}
+
+/**
+ * See @sentry/node trpc middleware:
+ * https://github.com/getsentry/sentry-javascript/blob/6d424571e3cd5e99991b711f4e23d773e321e294/packages/node/src/handlers.ts#L328
+ */
+function sentryTrpcMiddleware(_options: any) {
+  return function <T>({
+    path,
+    type,
+    next,
+    rawInput,
+  }: TrpcMiddlewareArguments<T>): T {
+    path;
+    type;
+    next;
+    rawInput;
+
+    // This function is effectively what @sentry/node does to provide its trpc middleware.
+    return null as any as T;
+  };
+}
+
+type Context = {
+  some: 'prop';
+};
+
+describe('inferRouterInputs', () => {
+  test('string', async () => {
+    const t = initTRPC.context<Context>().create();
+
+    const baseMiddleware = t.middleware(sentryTrpcMiddleware({ foo: 'bar' }));
+
+    const someMiddleware = t.middleware(async (opts) => {
+      return opts.next({
+        ctx: {
+          object: {
+            a: 'b',
+          },
+        },
+      });
+    });
+
+    const baseProcedure = t.procedure.use(baseMiddleware);
+    const bazQuxProcedure = baseProcedure.use(someMiddleware);
+
+    const appRouter = t.router({
+      foo: bazQuxProcedure.input(z.string()).query(({ ctx }) => ctx),
+    });
+    type Output = inferRouterOutputs<typeof appRouter>['foo'];
+
+    expectTypeOf<Output>().toEqualTypeOf<{
+      object: { a: string };
+      some: 'prop';
+    }>();
+  });
+});