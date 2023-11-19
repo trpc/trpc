e02dc5d9aae98f33c05a917a3a1db527306f0efe
juliusmarminge <julius0216@outlook.com>
2023-11-19 14:29:43 +0100

    rm deprecated

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,15 +9,6 @@ export * from './core';
 export { type DefaultErrorShape } from './error/formatter';
 export type { RootConfig, AnyRootConfig } from './core/internals/config';
 
-/**
- * ⚠️ ⚠️ ⚠️ Danger zone ⚠️ ⚠️ ⚠️
- * @remark
- * Do not use things from this export as they are subject to change without notice. They only exists to support `.d.ts`-files
- * If you need something from here, please open an issue and we'll see if we can expose it in a stable way.
- * @deprecated
- */
-export * as unstableExternalsExport from './unstableInternalsExport';
-
 export type {
   /**
    * @deprecated
