4464b01452a42ff17e49b283790a53e8f26bb846
maddsua <maddsuadev@proton.me>
2023-11-21 01:16:07 +0200

    Made prettier happy here too

--- packages/server/src/adapters/fetch/fetchRequestHandler.ts
+++ packages/server/src/adapters/fetch/fetchRequestHandler.ts
@@ -20,7 +20,9 @@ export async function fetchRequestHandler<TRouter extends AnyRouter>(
   };
 
   const url = new URL(opts.req.url);
-  const path = url.pathname.slice(opts.endpoint === '/' ? 1 : opts.endpoint.length + 1);
+  const path = url.pathname.slice(
+    opts.endpoint === '/' ? 1 : opts.endpoint.length + 1,
+  );
   const req: HTTPRequest = {
     query: url.searchParams,
     method: opts.req.method,
