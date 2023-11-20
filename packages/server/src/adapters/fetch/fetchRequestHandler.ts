74b72e6846e4c90c4b69f210d1f8d6dae2c881e2
KATT <alexander@n1s.se>
2023-11-20 11:08:34 +0100

    trim leading slash
    # Conflicts:
    #	packages/server/src/adapters/fetch/fetchRequestHandler.ts

--- packages/server/src/adapters/fetch/fetchRequestHandler.ts
+++ packages/server/src/adapters/fetch/fetchRequestHandler.ts
@@ -10,6 +10,9 @@ export type FetchHandlerRequestOptions<TRouter extends AnyRouter> =
     endpoint: string;
   };
 
+const trimLeadingSlash = (str: string) =>
+  str.startsWith('/') ? str.substring(1) : str;
+
 export async function fetchRequestHandler<TRouter extends AnyRouter>(
   opts: FetchHandlerRequestOptions<TRouter>,
 ): Promise<Response> {
@@ -20,7 +23,10 @@ export async function fetchRequestHandler<TRouter extends AnyRouter>(
   };
 
   const url = new URL(opts.req.url);
-  const path = url.pathname.slice(opts.endpoint === '/' ? 1 : opts.endpoint.length + 1);
+  const path = trimLeadingSlash(url.pathname).slice(
+    trimLeadingSlash(opts.endpoint).length,
+  );
+
   const req: HTTPRequest = {
     query: url.searchParams,
     method: opts.req.method,
