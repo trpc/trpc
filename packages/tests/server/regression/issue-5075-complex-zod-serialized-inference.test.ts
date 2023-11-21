ae5979959d7804e71fb270b17de06c6b1e1c03e5
Jussi Saurio <jussi.saurio@gmail.com>
2023-11-21 17:19:25 +0200

    Add zod kitchen sink serialization inference test

--- packages/tests/server/regression/issue-5075-complex-zod-serialized-inference.test.ts
+++ packages/tests/server/regression/issue-5075-complex-zod-serialized-inference.test.ts
@@ -40,3 +40,84 @@ describe('Non-records should not erroneously be inferred as Records in serialize
     expectTypeOf<SerializedOutputNoExplicitOutput>().toEqualTypeOf<Change>();
   });
 });
+
+describe('Zod schema serialization kitchen sink', () => {
+  test('Test serialization of different zod schemas against z.infer', () => {
+    const zObjectZArrayZRecordZTupleZUnionZIntersectionZLazyZPromiseZFunctionZMapZSetZEnumZNativeEnumZUnknownZNullableZOptionalZLiteralZBooleanZStringZNumberZBigintZDateZUndefinedZAny =
+      z.object({
+        zArray: z.array(z.string()),
+        zRecord: z.record(z.string()),
+        zTuple: z.tuple([z.string(), z.number()]),
+        zUnion: z.union([z.string(), z.number()]),
+        zIntersection: z.intersection(
+          z.object({ name: z.string() }),
+          z.object({ age: z.number() }),
+        ),
+        zLazy: z.lazy(() => z.string()),
+        zPromise: z.promise(z.string()),
+        zFunction: z.function(),
+        zMap: z.map(z.string(), z.number()),
+        zSet: z.set(z.string()),
+        zEnum: z.enum(['foo', 'bar']),
+        zNativeEnum: z.nativeEnum({ foo: 1, bar: 2 }),
+        zUnknown: z.unknown(),
+        zNullable: z.nullable(z.string()),
+        zOptional: z.optional(z.string()),
+        zLiteral: z.literal('foo'),
+        zBoolean: z.boolean(),
+        zString: z.string(),
+        zNumber: z.number(),
+        zBigint: z.bigint(),
+        zDate: z.date(),
+        zUndefined: z.undefined(),
+        zAny: z.any(),
+        zArrayOptional: z.array(z.string()).optional(),
+        zArrayOrRecord: z.union([z.array(z.string()), z.record(z.string())]),
+      });
+
+    const t = initTRPC.create();
+
+    const router = t.router({
+      createProject: t.procedure
+        .output(
+          zObjectZArrayZRecordZTupleZUnionZIntersectionZLazyZPromiseZFunctionZMapZSetZEnumZNativeEnumZUnknownZNullableZOptionalZLiteralZBooleanZStringZNumberZBigintZDateZUndefinedZAny,
+        )
+        .query(() => {
+          return zObjectZArrayZRecordZTupleZUnionZIntersectionZLazyZPromiseZFunctionZMapZSetZEnumZNativeEnumZUnknownZNullableZOptionalZLiteralZBooleanZStringZNumberZBigintZDateZUndefinedZAny.parse(
+            null as any,
+          );
+        }),
+    });
+
+    type SerializedOutput = inferRouterOutputs<typeof router>['createProject'];
+
+    expectTypeOf<SerializedOutput>().toEqualTypeOf<{
+      zArray: string[];
+      zRecord: Record<string, string>;
+      zTuple: [string, number];
+      zUnion: string | number;
+      zIntersection: { name: string; age: number };
+      zLazy: string;
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      zPromise: {};
+      // zFunction: (...args: any[]) => any; <-- not serialized, OK.
+      zMap: object;
+      zSet: object;
+      zEnum: 'foo' | 'bar';
+      zNativeEnum: number;
+      zUnknown?: unknown; // <-- why is this optional?
+      zNullable: string | null;
+      zOptional?: string | undefined;
+      zLiteral: 'foo';
+      zBoolean: boolean;
+      zString: string;
+      zNumber: number;
+      zBigint: never; // <-- should this be never or omitted?
+      zDate: string;
+      // zUndefined: undefined; <-- not serialized, OK.
+      zAny?: any; // <-- why is this optional?
+      zArrayOptional?: string[] | undefined;
+      zArrayOrRecord: string[] | Record<string, string>;
+    }>();
+  });
+});
