6ae6d61b34aac31902e88efa0b1dd7b379a5c01a
KATT <alexander@n1s.se>
2023-11-21 11:34:12 +0100

    weaks

--- packages/client/src/TRPCClientError.ts
+++ packages/client/src/TRPCClientError.ts
@@ -99,7 +99,7 @@ export class TRPCClientError<TRouterOrProcedure extends ErrorInferrable>
       }
       return cause;
     }
-    if (isTRPCErrorResponse(cause)) {
+    if (isTRPCErrorResponse(cause)) {             
       return new TRPCClientError(cause.error.message, {
         ...opts,
         result: cause,
