1061eb32e7c00a1c0de80ebf232a43394e09789e
Daniel Reed <danieel.reed@gmail.com>
2023-12-12 09:49:34 +0100

    refactor: moving company logos map to its own file

--- www/src/components/CompaniesUsing.tsx
+++ www/src/components/CompaniesUsing.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
+import { companyLogos } from './companies';
 import { SectionTitle } from './SectionTitle';
 
 const animationRow = Array(2).fill(0) as [number, number];
-
-const companyLogos: Record<string, string> = {
-  facebook: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  disney: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  airbnb: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  a: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  b: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  c: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  d: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  e: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-  f: 'https://cruip-tutorials.vercel.app/logo-carousel/disney.svg',
-};
 
 /**
  * A logo carousel to show companies using tRPC
