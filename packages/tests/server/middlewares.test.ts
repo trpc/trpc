c04490be758001c50ac3a68e5b6e447c6115f514
KATT <alexander@n1s.se>
2023-12-24 02:38:41 +0100

    rm some mess

--- packages/tests/server/middlewares.test.ts
+++ packages/tests/server/middlewares.test.ts
@@ -173,264 +173,40 @@ describe('standalone middleware', () => {
       }>();
     });
   });
-});
 
-test('standalone middlewares that define the ctx/input they require and can be used in different tRPC instances', () => {
-  type Human = {
-    id: string;
-    name: string;
-  };
-  type HumanContext = {
-    user: Human;
-  };
-  const tHuman = initTRPC.context<HumanContext>().create();
-
-  type Alien = {
-    id: string;
-    name: Buffer; // aliens have weird names
-  };
-  type AlienContext = {
-    user: Alien;
-    planet: 'mars' | 'venus';
-  };
-  const tAlien = initTRPC.context<AlienContext>().create();
-
-  const addFooToCtxMiddleware = experimental_standaloneMiddleware().create(
-    (opts) => {
-      expectTypeOf(opts.ctx).toEqualTypeOf<object>();
-      return opts.next({
-        ctx: {
-          foo: 'foo' as const,
-        },
-      });
-    },
-  );
-
-  const addUserNameLengthToCtxMiddleware = experimental_standaloneMiddleware<{
-    ctx: { user: Human | Alien };
-  }>().create((opts) => {
-    expectTypeOf(opts.ctx).toEqualTypeOf<{
-      user: Human | Alien;
-    }>();
-    return opts.next({
-      ctx: {
-        nameLength: opts.ctx.user.name.length,
-      },
-    });
-  });
-
-  const determineIfUserNameIsLongMiddleware =
-    experimental_standaloneMiddleware<{
-      ctx: { nameLength: number };
+  test('in pipe', () => {
+    const needsUser = experimental_standaloneMiddleware<{
+      ctx: { user: User };
     }>().create((opts) => {
-      expectTypeOf(opts.ctx).toEqualTypeOf<{
-        nameLength: number;
-      }>();
-
+      opts.ctx.user.id;
+      //    ^?
       return opts.next({
         ctx: {
-          nameIsLong: opts.ctx.nameLength > 10,
+          bar: 'bar' as const,
         },
       });
     });
 
-  const mapUserToUserTypeMiddleware = experimental_standaloneMiddleware<{
-    ctx: { user: Human | Alien };
-  }>().create((opts) => {
-    expectTypeOf(opts.ctx).toEqualTypeOf<{
-      user: Human | Alien;
-    }>();
-    return opts.next({
-      ctx: {
-        user:
-          typeof opts.ctx.user.name === 'string'
-            ? ('human' as const)
-            : ('alien' as const),
-      },
-    });
-  });
-
-  // This is not OK because determineIfUserNameIsLongMiddleware requires { nameLength: number } which is not in either context:
-  tHuman.procedure.use(
-    // @ts-expect-error: no user in context
-    determineIfUserNameIsLongMiddleware,
-  );
-  tAlien.procedure.use(
-    // @ts-expect-error: no user in context
-    determineIfUserNameIsLongMiddleware,
-  );
-
-  // This is not OK because determineIfUserNameIsLongMiddleware requires { nameLength: number } which is not in HumanContext & { foo: string }
-  tHuman.procedure
-    .use(addFooToCtxMiddleware)
-    // @ts-expect-error: no nameLength in context
-    .use(determineIfUserNameIsLongMiddleware);
-
-  // This is OK because the context provides { user: Human } or { user: Alien } which is what addUserNameLengthToCtx requires
-  tHuman.procedure
-    .use(addFooToCtxMiddleware)
-    .use(addUserNameLengthToCtxMiddleware)
-    // determineIfUserNameIsLongMiddleware only needs { nameLength: number }, so overwriting user is fine
-    .use(mapUserToUserTypeMiddleware)
-    .use(determineIfUserNameIsLongMiddleware)
-    .query(({ ctx }) => {
-      expectTypeOf(ctx).toEqualTypeOf<{
-        user: 'human' | 'alien';
-        nameLength: number;
-        nameIsLong: boolean;
-        foo: 'foo';
-      }>();
-    });
-
-  tAlien.procedure
-    .use(addFooToCtxMiddleware)
-    .use(addUserNameLengthToCtxMiddleware)
-    // determineIfUserNameIsLongMiddleware only needs { nameLength: number }, so overwriting user is fine
-    .use(mapUserToUserTypeMiddleware)
-    .use(determineIfUserNameIsLongMiddleware)
-    .query(({ ctx }) => {
-      expectTypeOf(ctx).toEqualTypeOf<{
-        user: 'human' | 'alien';
-        nameLength: number;
-        nameIsLong: boolean;
-        planet: 'mars' | 'venus';
-        foo: 'foo';
-      }>();
-    });
-
-  addFooToCtxMiddleware
-    // This is not OK because the requirements of the later middlewares are not met
-    // @ts-expect-error: No user in context at this point
-    .unstable_pipe(addUserNameLengthToCtxMiddleware)
-    .unstable_pipe(mapUserToUserTypeMiddleware)
-    .unstable_pipe(determineIfUserNameIsLongMiddleware);
-
-  const requireUserAndAddFooToCtxMiddleware =
-    experimental_standaloneMiddleware<{
-      ctx: { user: Human | Alien };
-    }>().create((opts) => {
-      expectTypeOf(opts.ctx).toEqualTypeOf<{
-        user: Human | Alien;
-      }>();
-      return opts.next({
-        ctx: {
-          foo: 'foo' as const,
-        },
-      });
-    });
-
-  const validPipedVersion = requireUserAndAddFooToCtxMiddleware
-    .unstable_pipe(addUserNameLengthToCtxMiddleware)
-    .unstable_pipe(mapUserToUserTypeMiddleware)
-    .unstable_pipe(determineIfUserNameIsLongMiddleware);
-
-  tHuman.procedure.use(validPipedVersion).query(({ ctx }) => {
-    expectTypeOf(ctx).toEqualTypeOf<{
-      user: 'human' | 'alien';
-      nameLength: number;
-      nameIsLong: boolean;
-      foo: 'foo';
-    }>();
-  });
-
-  tAlien.procedure.use(validPipedVersion).query(({ ctx }) => {
-    expectTypeOf(ctx).toEqualTypeOf<{
-      user: 'human' | 'alien';
-      nameLength: number;
-      nameIsLong: boolean;
-      planet: 'mars' | 'venus';
-      foo: 'foo';
-    }>();
-  });
-
-  // Middleware chain using standalone middleware that requires a particular 'input' shape
-  const ensureMagicNumberIsNotLongerThanNameLength =
-    experimental_standaloneMiddleware<{
-      ctx: { nameLength: number };
-      input: { magicNumber: number };
-    }>().create((opts) => {
-      expectTypeOf(opts.ctx).toEqualTypeOf<{
-        nameLength: number;
-      }>();
-      expectTypeOf(opts.input).toEqualTypeOf<{
-        magicNumber: number;
-      }>();
-
-      if (opts.input.magicNumber > opts.ctx.nameLength) {
-        throw new TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'magicNumber is too high',
-        });
-      }
-
-      return opts.next();
-    });
-
-  // This is not OK because the input is not compatible with the middleware (magicNumber must always be number)
-  tHuman.procedure
-    .input(z.object({ magicNumber: z.number().optional() }))
-    .use(addUserNameLengthToCtxMiddleware)
-    // @ts-expect-error: magicNumber is required
-    .use(ensureMagicNumberIsNotLongerThanNameLength);
-
-  // This is OK because the input is compatible with the middleware
-  tHuman.procedure
-    .input(z.object({ magicNumber: z.number() }))
-    .use(addUserNameLengthToCtxMiddleware)
-    .use(ensureMagicNumberIsNotLongerThanNameLength)
-    .query(({ input, ctx }) => {
-      expectTypeOf(ctx).toEqualTypeOf<{
-        user: Human;
-        nameLength: number;
-      }>();
-      expectTypeOf(input).toEqualTypeOf<{
-        magicNumber: number;
-      }>();
-    });
-
-  // Middleware that defines a particular 'meta' shape
-  const shamefullyLogIfProcedureIsNotCoolMiddleware =
-    experimental_standaloneMiddleware<{
-      meta: {
-        cool: boolean;
+    const withUser = t.middleware((opts) => {
+      const user: User = {
+        id: 'id',
       };
-    }>().create((opts) => {
-      expectTypeOf(opts.meta).toEqualTypeOf<
-        | {
-            cool: boolean;
-          }
-        | undefined
-      >();
-
-      if (!opts.meta?.cool) {
-        globalThis.console.log('This procedure is not cool');
-      }
-
-      return opts.next();
+      return opts.next({
+        ctx: {
+          user,
+        },
+      });
     });
 
-  // This is not OK because the meta is not compatible with the middleware (cool must always be boolean)
-  const tHumanWithWrongMeta = initTRPC
-    .context<HumanContext>()
-    .meta<{ cool: string }>()
-    .create();
-  tHumanWithWrongMeta.procedure
-    .meta({ cool: 'true' })
-    // @ts-expect-error: cool must be boolean
-    .use(shamefullyLogIfProcedureIsNotCoolMiddleware);
-
-  // This is OK because the meta is compatible with the middleware
-  const tHumanWithMeta = initTRPC
-    .context<HumanContext>()
-    .meta<{ cool: boolean }>()
-    .create();
-
-  tHumanWithMeta.procedure
-    .meta({ cool: false })
-    .use(shamefullyLogIfProcedureIsNotCoolMiddleware);
-
-  // Works without the .meta() addition as well, since Meta is always a union with undefined
-  tHumanWithMeta.procedure.use(shamefullyLogIfProcedureIsNotCoolMiddleware);
+    withUser.unstable_pipe(needsUser).unstable_pipe((opts) => {
+      expectTypeOf(opts.ctx).toEqualTypeOf<{
+        foo: 'foo';
+        user: User;
+        bar: 'bar';
+      }>();
+      return opts.next();
+    });
+  });
 });
 
 test('pipe middlewares - inlined', async () => {
