50cf164b6734161c86663ec34b25416365892a35
Alex / KATT <alexander@n1s.se>
2023-12-27 00:15:28 +0100

    feat(server): deprecate `.createCaller()` in favor of `t.createCallerFactory()` (#5213)

--- examples/next-prisma-websockets-starter/src/server/trpc.ts
+++ examples/next-prisma-websockets-starter/src/server/trpc.ts
@@ -4,8 +4,8 @@
  * - We export only the functionality that we use so we can enforce which base procedures should be used
  *
  * Learn how to create protected base procedures and other things below:
- * @see https://trpc.io/docs/v10/router
- * @see https://trpc.io/docs/v10/procedures
+ * @see https://trpc.io/docs/router
+ * @see https://trpc.io/docs/procedures
  */
 
 import { Context } from './context';
@@ -14,11 +14,11 @@ import superjson from 'superjson';
 
 const t = initTRPC.context<Context>().create({
   /**
-   * @see https://trpc.io/docs/v10/data-transformers
+   * @see https://trpc.io/docs/data-transformers
    */
   transformer: superjson,
   /**
-   * @see https://trpc.io/docs/v10/error-formatting
+   * @see https://trpc.io/docs/error-formatting
    */
   errorFormatter({ shape }) {
     return shape;
@@ -27,23 +27,23 @@ const t = initTRPC.context<Context>().create({
 
 /**
  * Create a router
- * @see https://trpc.io/docs/v10/router
+ * @see https://trpc.io/docs/router
  */
 export const router = t.router;
 
 /**
  * Create an unprotected procedure
- * @see https://trpc.io/docs/v10/procedures
+ * @see https://trpc.io/docs/procedures
  **/
 export const publicProcedure = t.procedure;
 
 /**
- * @see https://trpc.io/docs/v10/middlewares
+ * @see https://trpc.io/docs/middlewares
  */
 export const middleware = t.middleware;
 
 /**
- * @see https://trpc.io/docs/v10/merging-routers
+ * @see https://trpc.io/docs/merging-routers
  */
 export const mergeRouters = t.mergeRouters;
 
