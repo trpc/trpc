402f7b08b8c6f2f61574fcddf2c6fcefe9e3535e
juliusmarminge <julius0216@outlook.com>
2023-12-29 15:59:36 +0100

    rm unnecessary

--- packages/client/src/links/types.ts
+++ packages/client/src/links/types.ts
@@ -39,7 +39,6 @@ export type Operation<TInput = unknown> = {
 
 interface HeadersInitEsque {
   [Symbol.iterator](): IterableIterator<[string, string]>;
-  entries(): IterableIterator<[string, string]>;
 }
 
 /**
