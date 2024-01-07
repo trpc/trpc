0cabe00c786db3f74d898cb0f512dafacb652e9a
juliusmarminge <julius0216@outlook.com>
2024-01-07 16:41:37 +0100

    reorder a bit

--- packages/core/src/http/types.ts
+++ packages/core/src/http/types.ts
@@ -18,6 +18,11 @@ export interface HTTPResponse {
 
 export type ResponseChunk = [procedureIndex: number, responseBody: string];
 
+export interface ResponseMeta {
+  status?: number;
+  headers?: HTTPHeaders;
+}
+
 /**
  * @internal
  */
@@ -58,11 +63,6 @@ export interface HTTPBaseHandlerOptions<TRouter extends AnyRouter, TRequest>
   responseMeta?: ResponseMetaFn<TRouter>;
 }
 
-export interface ResponseMeta {
-  status?: number;
-  headers?: HTTPHeaders;
-}
-
 /** @internal */
 export type ProcedureCall = {
   type: ProcedureType;
@@ -85,22 +85,10 @@ export type TRPCRequestInfo = {
  */
 export type ResolveHTTPRequestOptionsContextFn<TRouter extends AnyRouter> =
   (opts: { info: TRPCRequestInfo }) => Promise<inferRouterContext<TRouter>>;
-/**
- * Base interface for any response handler
- * @internal
- */
 
-export interface BaseHandlerOptions<TRouter extends AnyRouter, TRequest> {
-  onError?: OnErrorFunction<TRouter, TRequest>;
-  batching?: {
-    enabled: boolean;
-  };
-  router: TRouter;
-}
 /**
  * @internal
  */
-
 export type OnErrorFunction<TRouter extends AnyRouter, TRequest> = (opts: {
   error: TRPCError;
   type: ProcedureType | 'unknown';
@@ -109,3 +97,15 @@ export type OnErrorFunction<TRouter extends AnyRouter, TRequest> = (opts: {
   input: unknown;
   ctx: inferRouterContext<TRouter> | undefined;
 }) => void;
+
+/**
+ * Base interface for any response handler
+ * @internal
+ */
+export interface BaseHandlerOptions<TRouter extends AnyRouter, TRequest> {
+  onError?: OnErrorFunction<TRouter, TRequest>;
+  batching?: {
+    enabled: boolean;
+  };
+  router: TRouter;
+}
