0bf2ec0640b837e065485a456568218202ae6b6a
juliusmarminge <julius0216@outlook.com>
2024-01-08 00:21:11 +0100

    cleanup exports in observable

--- packages/core/src/observable/index.ts
+++ packages/core/src/observable/index.ts
@@ -1,14 +1,9 @@
 export { isObservable, observable } from './observable';
 export type { inferObservableValue } from './observable';
 export type {
-  MonoTypeOperatorFunction,
   Observable,
   Observer,
-  OperatorFunction,
-  Subscribable,
-  SubscriptionLike,
   TeardownLogic,
-  UnaryFunction,
   Unsubscribable,
   UnsubscribeFn,
 } from './types';
