1ae10055c39136b268fdaf16baeb4339b6540398
KATT <alexander@n1s.se>
2024-01-08 20:47:58 +0100

    cool

--- www/sidebars.js
+++ www/sidebars.js
@@ -170,6 +170,10 @@ module.exports = {
     },
     {
       type: 'doc',
+      id: 'migration/migrate-from-v10-to-v11',
+    },
+    {
+      type: 'doc',
       id: 'migration/migrate-from-v9-to-v10',
     },
     {
