9dc76e2cb385a3805c9964df8449b73102788e64
juliusmarminge <julius0216@outlook.com>
2024-01-08 01:24:54 +0100

    sort main entrypoint exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,81 +3,6 @@
  */
 
 export type {
-  CombinedDataTransformer,
-  CombinedDataTransformerClient,
-  DataTransformer,
-  DataTransformerOptions,
-  DefaultDataTransformer,
-} from './core/transformer';
-export {
-  defaultTransformer,
-  getDataTransformer,
-  transformResult,
-  transformTRPCResponse,
-} from './core/transformer';
-export {
-  TRPCError,
-  getCauseFromUnknown,
-  getTRPCErrorFromUnknown,
-} from './error/TRPCError';
-export type {
-  AnyRouter,
-  ProcedureRecord,
-  ProcedureRouterRecord,
-  CreateRouterInner,
-  Router,
-  RouterCaller,
-  AnyRouterDef,
-} from './core/router';
-export {
-  callProcedure,
-  createCallerFactory,
-  mergeRouters,
-} from './core/router';
-export type {
-  Procedure,
-  ProcedureType,
-  AnyProcedure,
-  AnyQueryProcedure,
-  AnyMutationProcedure,
-  AnySubscriptionProcedure,
-  ProcedureArgs,
-  ProcedureOptions,
-  MutationProcedure,
-  QueryProcedure,
-  SubscriptionProcedure,
-} from './core/procedure';
-export { procedureTypes } from './core/procedure';
-export type { inferParser } from './core/parser';
-export {
-  createInputMiddleware,
-  createOutputMiddleware,
-  experimental_standaloneMiddleware,
-  middlewareMarker,
-} from './core/middleware';
-export type {
-  MiddlewareFunction,
-  MiddlewareBuilder,
-  MiddlewareMarker,
-} from './core/middleware';
-export { initTRPC } from './core/initTRPC';
-export type {
-  DeepPartial,
-  Dict,
-  DistributiveOmit,
-  Filter,
-  FilterKeys,
-  Maybe,
-  MaybePromise,
-  Simplify,
-  Unwrap,
-  WithoutIndexSignature,
-  Overwrite,
-  PickFirstDefined,
-  ValidateShape,
-} from './utilityTypes';
-export type { IntersectionError, ProtectedIntersection } from './types';
-export type {
   inferHandlerInput,
   inferProcedureInput,
   inferProcedureOutput,
@@ -95,8 +20,29 @@ export type {
   inferConfig,
   inferErrorShape,
 } from './core/inference';
-export type { DefaultErrorShape } from './error/formatter';
-export { unsetMarker } from './core/procedureBuilder';
+export { initTRPC } from './core/initTRPC';
+export type { MiddlewareFunction, MiddlewareBuilder } from './core/middleware';
+export {
+  createInputMiddleware,
+  createOutputMiddleware,
+  experimental_standaloneMiddleware,
+  middlewareMarker,
+} from './core/middleware';
+export type { inferParser } from './core/parser';
+export type {
+  Procedure,
+  ProcedureType,
+  AnyProcedure,
+  AnyQueryProcedure,
+  AnyMutationProcedure,
+  AnySubscriptionProcedure,
+  ProcedureArgs,
+  ProcedureOptions,
+  MutationProcedure,
+  QueryProcedure,
+  SubscriptionProcedure,
+} from './core/procedure';
+export { procedureTypes } from './core/procedure';
 export type {
   AnyProcedureBuilderDef,
   ProcedureBuilder,
@@ -104,9 +50,7 @@ export type {
   ProcedureBuilderResolver,
   ProcedureCallOptions,
 } from './core/procedureBuilder';
-export { createBuilder } from './core/procedureBuilder';
-export type { GetRawInputFn } from './types';
-export { isObject } from './utilityFunctions';
+export { unsetMarker, createBuilder } from './core/procedureBuilder';
 export type {
   AnyRootConfig,
   CreateRootConfigTypes,
@@ -115,10 +59,66 @@ export type {
   RuntimeConfig,
 } from './core/rootConfig';
 export { isServerDefault } from './core/rootConfig';
+export type {
+  AnyRouter,
+  ProcedureRecord,
+  ProcedureRouterRecord,
+  CreateRouterInner,
+  Router,
+  RouterCaller,
+  AnyRouterDef,
+} from './core/router';
+export {
+  callProcedure,
+  createCallerFactory,
+  mergeRouters,
+} from './core/router';
+export type {
+  CombinedDataTransformer,
+  CombinedDataTransformerClient,
+  DataTransformer,
+  DataTransformerOptions,
+  DefaultDataTransformer,
+} from './core/transformer';
+export {
+  defaultTransformer,
+  getDataTransformer,
+  transformResult,
+  transformTRPCResponse,
+} from './core/transformer';
+
+export { getErrorShape } from './error/getErrorShape';
+export type { DefaultErrorShape } from './error/formatter';
+export {
+  TRPCError,
+  getCauseFromUnknown,
+  getTRPCErrorFromUnknown,
+} from './error/TRPCError';
 
 export { createFlatProxy, createRecursiveProxy } from './createProxy';
 
 // For `.d.ts` files https://github.com/trpc/trpc/issues/3943
 export type { SerializeObject, Serialize } from './serialize';
 
-export { getErrorShape } from './error/getErrorShape';
+export type {
+  IntersectionError,
+  ProtectedIntersection,
+  GetRawInputFn,
+} from './types';
+
+export { isObject } from './utilityFunctions';
+export type {
+  DeepPartial,
+  Dict,
+  DistributiveOmit,
+  Filter,
+  FilterKeys,
+  Maybe,
+  MaybePromise,
+  Simplify,
+  Unwrap,
+  WithoutIndexSignature,
+  Overwrite,
+  PickFirstDefined,
+  ValidateShape,
+} from './utilityTypes';
