9b8be53c59c3690f79c860787a86063f4a8fc78c
Alex / KATT <alexander@n1s.se>
2024-01-11 13:29:44 +0100

    chore: `reportUnusedDisableDirectives: true` (#5311)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ const config = {
     'plugin:react-hooks/recommended',
     'prettier',
   ],
+  reportUnusedDisableDirectives: true,
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of import
