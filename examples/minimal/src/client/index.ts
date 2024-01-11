cf729c826917463879ea0469ba812e5dc4227021
KATT <alexander@n1s.se>
2024-01-11 21:08:42 +0100

    minimal

--- examples/minimal/src/client/index.ts
+++ examples/minimal/src/client/index.ts
@@ -9,21 +9,17 @@ const trpc = createTRPCClient<AppRouter>({
   ],
 });
 
-async function main() {
-  /**
-   * Inferring types
-   */
-  const users = await trpc.userList.query();
-  //    ^?
-  console.log('Users:', users);
+/**
+ * Inferring types
+ */
+const users = await trpc.userList.query();
+//    ^?
+console.log('Users:', users);
 
-  const createdUser = await trpc.userCreate.mutate({ name: 'sachinraja' });
-  //    ^?
-  console.log('Created user:', createdUser);
+const createdUser = await trpc.userCreate.mutate({ name: 'sachinraja' });
+//    ^?
+console.log('Created user:', createdUser);
 
-  const user = await trpc.userById.query('1');
-  //    ^?
-  console.log('User 1:', user);
-}
-
-main().catch(console.error);
+const user = await trpc.userById.query('1');
+//    ^?
+console.log('User 1:', user);
