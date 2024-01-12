d3164d659ca75e99191c79e1452d7eb9bd2fa28c
KATT <alexander@n1s.se>
2024-01-12 14:19:35 +0100

    cool

--- packages/core/src/procedureBuilder.ts
+++ packages/core/src/procedureBuilder.ts
@@ -32,7 +32,7 @@ type IntersectIfDefined<TType, TWith> = UnsetMarker extends TType
 type ErrorMessage<TMessage extends string> = TMessage;
 
 /** @internal */
-const unsetMarker = Symbol('unsetMarker');
+export const unsetMarker = Symbol('unsetMarker');
 export type UnsetMarker = typeof unsetMarker;
 type DefaultValue<TValue, TFallback> = TValue extends UnsetMarker
   ? TFallback
