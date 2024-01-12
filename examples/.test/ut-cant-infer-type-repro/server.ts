ff34e522a71b9b7015389c9a55461d61a3a76356
juliusmarminge <julius0216@outlook.com>
2024-01-12 17:03:39 +0100

    weird...

---
+++ examples/.test/ut-cant-infer-type-repro/server.ts
@@ -0,0 +1,15 @@
+import { initTRPC } from "@trpc/server";
+import {  ZodError } from "zod";
+
+export const t = initTRPC.create({
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      // data: {
+        // ...shape.data,
+        // zodError:
+        //   error.cause instanceof ZodError ? error.cause.flatten() : null,
+      // },
+    };
+  },
+});
