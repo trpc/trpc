86050dd3b641ab665ff7e7513df82552180b27e4
KATT <alexander@n1s.se>
2024-01-14 18:36:15 +0100

    Revert "fix `httpLink`"
    This reverts commit 0496d8d4176ff77e719614368e9c342d1070fc5f.

--- packages/client/src/links/httpLink.ts
+++ packages/client/src/links/httpLink.ts
@@ -1,6 +1,6 @@
 import type { AnyRouter } from '@trpc/core';
 import { transformResult } from '@trpc/core';
-import { observable } from '@trpc/server/observable';
+import { observable } from '@trpc/core/observable';
 import { TRPCClientError } from '../TRPCClientError';
 import type {
   HTTPLinkBaseOptions,
