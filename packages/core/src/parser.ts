1fffe789f81a7059ee591379b9b5fb7e01e85cb3
Nick Lucas <me@nicklucas.co.uk>
2024-01-14 21:59:25 +0000

    Strip out all parsers except for zod

--- packages/core/src/parser.ts
+++ packages/core/src/parser.ts
@@ -4,50 +4,13 @@ export type ParserZodEsque<TInput, TParsedInput> = {
   _output: TParsedInput;
 };
 
-export type ParserMyZodEsque<TInput> = {
-  parse: (input: any) => TInput;
-};
-
-export type ParserSuperstructEsque<TInput> = {
-  create: (input: unknown) => TInput;
-};
-
-export type ParserCustomValidatorEsque<TInput> = (
-  input: unknown,
-) => Promise<TInput> | TInput;
-
-export type ParserYupEsque<TInput> = {
-  validateSync: (input: unknown) => TInput;
-};
-
-export type ParserScaleEsque<TInput> = {
-  assert(value: unknown): asserts value is TInput;
-};
-
-export type ParserWithoutInput<TInput> =
-  | ParserCustomValidatorEsque<TInput>
-  | ParserMyZodEsque<TInput>
-  | ParserScaleEsque<TInput>
-  | ParserSuperstructEsque<TInput>
-  | ParserYupEsque<TInput>;
-
-export type ParserWithInputOutput<TInput, TParsedInput> = ParserZodEsque<
-  TInput,
-  TParsedInput
->;
-
-export type Parser = ParserWithInputOutput<any, any> | ParserWithoutInput<any>;
+export type Parser = ParserZodEsque<any, any>;
 
 export type inferParser<TParser extends Parser> =
-  TParser extends ParserWithInputOutput<infer $TIn, infer $TOut>
+  TParser extends ParserZodEsque<infer $TIn, infer $TOut>
     ? {
         in: $TIn;
         out: $TOut;
-      }
-    : TParser extends ParserWithoutInput<infer $InOut>
-    ? {
-        in: $InOut;
-        out: $InOut;
       }
     : never;
 
