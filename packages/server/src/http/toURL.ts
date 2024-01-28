433e7a395de9224d8963891b370c61fc04664106
KATT <alexander@n1s.se>
2024-01-28 16:11:36 +0100

    fix

--- packages/server/src/http/toURL.ts
+++ packages/server/src/http/toURL.ts
@@ -1,6 +1,7 @@
 export function toURL(urlOrPathname: string): URL {
-  if (URL.canParse(urlOrPathname)) {
-    return new URL(urlOrPathname);
-  }
-  return new URL(urlOrPathname, 'http://127.0.0.1');
+  const url = urlOrPathname.startsWith('/')
+    ? `http://127.0.0.1${urlOrPathname}`
+    : urlOrPathname;
+
+  return new URL(url);
 }
