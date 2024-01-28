68c96be4dd369c170692168ef7c8ddf46a2cd31b
Alex / KATT <alexander@n1s.se>
2024-01-28 15:16:43 +0100

    chore: cleanup big router tests (#5409)

--- examples/next-big-router/src/server/trpc.ts
+++ examples/next-big-router/src/server/trpc.ts
@@ -2,3 +2,7 @@ import { initTRPC } from '@trpc/server';
 import type { Context } from './context';
 
 export const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+
+export const publicProcedure = t.procedure;
