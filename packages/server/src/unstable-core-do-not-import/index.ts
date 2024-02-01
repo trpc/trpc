45d9f1b13d486490883a5680c59ff5d1126a7774
KATT <alexander@n1s.se>
2024-02-01 13:22:16 +0100

    meow

--- packages/server/src/unstable-core-do-not-import/index.ts
+++ packages/server/src/unstable-core-do-not-import/index.ts
@@ -8,93 +8,23 @@
  *
  * If you seem to need to import anything from here, please open an issue at https://github.com/trpc/trpc/issues
  */
-export {
-  TRPCError,
-  getCauseFromUnknown,
-  getTRPCErrorFromUnknown,
-} from './error/TRPCError';
-export type { DefaultErrorData, DefaultErrorShape } from './error/formatter';
-export { getErrorShape } from './error/getErrorShape';
-
-export { initTRPC } from './initTRPC';
-export {
-  createInputMiddleware,
-  createOutputMiddleware,
-  experimental_standaloneMiddleware,
-  middlewareMarker,
-} from './middleware';
-export type {
-  AnyMiddlewareFunction,
-  MiddlewareBuilder,
-  MiddlewareFunction,
-} from './middleware';
-export type { inferParser } from './parser';
-export { procedureTypes } from './procedure';
-export type {
-  AnyMutationProcedure,
-  AnyProcedure,
-  AnyQueryProcedure,
-  AnySubscriptionProcedure,
-  MutationProcedure,
-  Procedure,
-  ProcedureOptions,
-  ProcedureType,
-  QueryProcedure,
-  SubscriptionProcedure,
-} from './procedure';
-export { createBuilder, unsetMarker } from './procedureBuilder';
-export type { ProcedureBuilder } from './procedureBuilder';
-export * from './rootConfig';
-export type {
-  AnyRouter,
-  Router,
-  RouterCaller,
-  createRouterFactory,
-  inferRouterContext,
-  inferRouterError,
-  inferRouterMeta,
-} from './router';
-
 export * from './clientish/inference';
 export * from './clientish/inferrable';
 export * from './clientish/serialize';
-
-export * from './router';
-export { transformResult, transformTRPCResponse } from './transformer';
-export type {
-  CombinedDataTransformer,
-  CombinedDataTransformerClient,
-  DataTransformer,
-  DataTransformerOptions,
-} from './transformer';
-
-export { createFlatProxy, createRecursiveProxy } from './createProxy';
-
-// For `.d.ts` files https://github.com/trpc/trpc/issues/3943
-export type { Serialize, SerializeObject } from './clientish/serialize';
-
-export type {
-  DeepPartial,
-  Dict,
-  DistributiveOmit,
-  Filter,
-  FilterKeys,
-  IntersectionError,
-  Maybe,
-  MaybePromise,
-  Overwrite,
-  PickFirstDefined,
-  ProtectedIntersection,
-  Simplify,
-  Unwrap,
-  ValidateShape,
-  WithoutIndexSignature,
-  TypeError,
-} from './types';
-export { isObject } from './utils';
-
-/**
- * These should be re-exported from separate entrypoints in server package
- */
+export * from './createProxy';
+export * from './error/formatter';
+export * from './error/getErrorShape';
+export * from './error/TRPCError';
 export * from './http';
+export * from './initTRPC';
+export * from './middleware';
+export * from './parser';
+export * from './procedure';
+export * from './procedureBuilder';
+export * from './rootConfig';
+export * from './router';
+export * from './router';
 export * from './rpc';
+export * from './transformer';
+export * from './types';
+export * from './utils';
