181e34c577b399da19525f4b3bd045c7c63c2f68
KATT <alexander@n1s.se>
2024-02-02 13:16:02 +0100

    rm ssr

--- examples/next-prisma-starter/playwright/smoke.test.ts
+++ examples/next-prisma-starter/playwright/smoke.test.ts
@@ -8,11 +8,6 @@ test('go to /', async ({ page }) => {
   await page.waitForSelector(`text=Starter`);
 });
 
-test('test 404', async ({ page }) => {
-  const res = await page.goto('/post/not-found');
-  expect(res?.status()).toBe(404);
-});
-
 test('add a post', async ({ page, browser }) => {
   const nonce = `${Math.random()}`;
 
