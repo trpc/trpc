22e9fef4d79479a0d658db165c5cd1d87871dfdf
KATT <alexander@n1s.se>
2024-02-09 16:07:01 +0100

    tweak

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -124,9 +124,7 @@ function TryItOut() {
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
-        className={clsx(
-          'z-10 my-0 h-[800px] w-full overflow-hidden rounded-xl md:my-4 lg:my-8',
-        )}
+        className={clsx('relative z-10 my-0 w-full md:my-4 lg:my-8')}
       >
         <div className="h-full">
           <div className="flex w-full justify-center">
@@ -155,7 +153,7 @@ function TryItOut() {
             <div
               key={sandbox.id}
               className={cn(
-                'relative h-full overflow-hidden rounded-md border border-zinc-200 dark:border-zinc-700',
+                'relative h-[800px] overflow-hidden rounded-md border border-zinc-200 dark:border-zinc-700',
                 selectedId !== sandbox.id && 'hidden',
               )}
             >
@@ -171,7 +169,7 @@ function TryItOut() {
               </div>
               {!deviceSupported && (
                 <ErrorOverlay
-                  url={`https://stackblitz.com/github/trpc/examples-${sandbox.id}/tree/main`}
+                  url={`https://stackblitz.com/github/trpc/trpc/tree/next/examples/${sandbox.id}`}
                   dismiss={() => {
                     setIsSupported(true);
                   }}
@@ -179,7 +177,7 @@ function TryItOut() {
               )}
               <Iframe
                 src={
-                  `https://stackblitz.com/github/trpc/trpc/examples-${sandbox.id}/tree/main?` +
+                  `https://stackblitz.com/github/trpc/trpc/tree/next/examples/${sandbox.id}?` +
                   searchParams({
                     embed: '1',
                     file: sandbox.files,
