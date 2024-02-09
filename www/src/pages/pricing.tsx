2977aa3f97b98c1a56aa1a8f328642dbe2445eef
KATT <alexander@n1s.se>
2024-02-09 13:15:18 +0100

    pricing page

---
+++ www/src/pages/pricing.tsx
@@ -0,0 +1,220 @@
+import Head from '@docusaurus/Head';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { RadioGroup } from '@headlessui/react';
+import { CheckIcon } from '@heroicons/react/20/solid';
+import Layout from '@theme/Layout';
+import React, { useState } from 'react';
+
+const frequencies = [
+  { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
+  { value: 'annually', label: 'Annually', priceSuffix: '/year' },
+] as const satisfies { value: string; label: string; priceSuffix: string }[];
+
+const tiers = [
+  {
+    name: 'Freelancer',
+    id: 'tier-freelancer',
+    href: '#',
+    price: { monthly: '$15', annually: '$144' },
+    description: 'The essentials to provide your best work for clients.',
+    features: [
+      '5 products',
+      'Up to 1,000 subscribers',
+      'Basic analytics',
+      '48-hour support response time',
+    ],
+    featured: false,
+    cta: 'Buy plan',
+  },
+  {
+    name: 'Startup',
+    id: 'tier-startup',
+    href: '#',
+    price: { monthly: '$30', annually: '$288' },
+    description: 'A plan that scales with your rapidly growing business.',
+    features: [
+      '25 products',
+      'Up to 10,000 subscribers',
+      'Advanced analytics',
+      '48-hour support response time',
+      'Up to 2 hour consulting each calendar month (worth $500)',
+    ],
+    featured: false,
+    cta: 'Buy plan',
+  },
+  {
+    name: 'Enterprise',
+    id: 'tier-enterprise',
+    href: '#',
+    price: 'Custom',
+    description: 'Dedicated support and infrastructure for your company.',
+    features: [
+      'Unlimited products',
+      'Unlimited subscribers',
+      'Advanced analytics',
+      '1-hour, dedicated support response time',
+      'Marketing automations',
+      'Custom reporting tools',
+    ],
+    featured: true,
+    cta: 'Contact sales',
+  },
+] as const satisfies {
+  name: string;
+  id: string;
+  href: string;
+  price: string | { monthly: string; annually: string };
+  description: string;
+  features: string[];
+  featured: boolean;
+  cta: string;
+}[];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+function Pricing() {
+  const [frequency, setFrequency] = useState(frequencies[0]);
+
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-4xl text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+            Pricing
+          </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Pricing plans for teams of all sizes
+          </p>
+        </div>
+        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+          Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
+          quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
+        </p>
+        <div className="mt-16 flex justify-center">
+          <RadioGroup
+            value={frequency}
+            onChange={setFrequency}
+            className="grid grid-cols-2 gap-x-1 rounded-full p-1 text-center text-xs font-semibold leading-5 ring-1 ring-inset ring-gray-200"
+          >
+            <RadioGroup.Label className="sr-only">
+              Payment frequency
+            </RadioGroup.Label>
+            {frequencies.map((option) => (
+              <RadioGroup.Option
+                key={option.value}
+                value={option}
+                className={({ checked }) =>
+                  classNames(
+                    checked ? 'bg-indigo-600 text-white' : 'text-gray-500',
+                    'cursor-pointer rounded-full px-2.5 py-1',
+                  )
+                }
+              >
+                <span>{option.label}</span>
+              </RadioGroup.Option>
+            ))}
+          </RadioGroup>
+        </div>
+        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {tiers.map((tier) => (
+            <div
+              key={tier.id}
+              className={classNames(
+                tier.featured ? 'bg-gray-900 ring-gray-900' : 'ring-gray-200',
+                'rounded-3xl p-8 ring-1 xl:p-10',
+              )}
+            >
+              <h3
+                id={tier.id}
+                className={classNames(
+                  tier.featured ? 'text-white' : 'text-gray-900',
+                  'text-lg font-semibold leading-8',
+                )}
+              >
+                {tier.name}
+              </h3>
+              <p
+                className={classNames(
+                  tier.featured ? 'text-gray-300' : 'text-gray-600',
+                  'mt-4 text-sm leading-6',
+                )}
+              >
+                {tier.description}
+              </p>
+              <p className="mt-6 flex items-baseline gap-x-1">
+                <span
+                  className={classNames(
+                    tier.featured ? 'text-white' : 'text-gray-900',
+                    'text-4xl font-bold tracking-tight',
+                  )}
+                >
+                  {typeof tier.price === 'string'
+                    ? tier.price
+                    : tier.price[frequency.value]}
+                </span>
+                {typeof tier.price !== 'string' ? (
+                  <span
+                    className={classNames(
+                      tier.featured ? 'text-gray-300' : 'text-gray-600',
+                      'text-sm font-semibold leading-6',
+                    )}
+                  >
+                    {frequency.priceSuffix}
+                  </span>
+                ) : null}
+              </p>
+              <a
+                href={tier.href}
+                aria-describedby={tier.id}
+                className={classNames(
+                  tier.featured
+                    ? 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white'
+                    : 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-indigo-600',
+                  'mt-6 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
+                )}
+              >
+                {tier.cta}
+              </a>
+              <ul
+                role="list"
+                className={classNames(
+                  tier.featured ? 'text-gray-300' : 'text-gray-600',
+                  'mt-8 space-y-3 text-sm leading-6 xl:mt-10',
+                )}
+              >
+                {tier.features.map((feature) => (
+                  <li key={feature} className="flex gap-x-3">
+                    <CheckIcon
+                      className={classNames(
+                        tier.featured ? 'text-white' : 'text-indigo-600',
+                        'h-6 w-5 flex-none',
+                      )}
+                      aria-hidden="true"
+                    />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <Layout
+      title={`Pricing 💸`}
+      description="End-to-end typesafe APIs made easy. Automatic typesafety & autocompletion inferred from your API-paths, their input data, &amp; outputs 🧙‍♂️"
+    >
+      <Head>
+        <body className="pricing" />
+      </Head>
+      <Pricing />
+    </Layout>
+  );
+}
