82c5f44d165262e265b5ae3e022f967478824e87
Alex / KATT <alexander@n1s.se>
2024-02-15 16:28:53 +0100

    chore: tweak the minimal example a bit (#5483)

--- examples/minimal/src/client/index.ts
+++ examples/minimal/src/client/index.ts
@@ -1,6 +1,13 @@
+/**
+ * This is the client-side code that uses the inferred types from the server
+ */
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
+/**
+ * We only import the `AppRouter` type from the server - this is not available at runtime
+ */
 import type { AppRouter } from '../server/index.js';
 
+// Initialize the tRPC client
 const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
@@ -9,17 +16,21 @@ const trpc = createTRPCClient<AppRouter>({
   ],
 });
 
-/**
- * Inferring types
- */
-const users = await trpc.userList.query();
+// Call procedure functions
+
+// 💡 Tip, try to:
+// - hover any types below to see the inferred types
+// - Cmd/Ctrl+click on any function to jump to the definition
+// - Rename any variable and see it reflected across both frontend and backend
+
+const users = await trpc.user.list.query();
 //    ^?
 console.log('Users:', users);
 
-const createdUser = await trpc.userCreate.mutate({ name: 'sachinraja' });
+const createdUser = await trpc.user.create.mutate({ name: 'sachinraja' });
 //    ^?
 console.log('Created user:', createdUser);
 
-const user = await trpc.userById.query('1');
+const user = await trpc.user.byId.query('1');
 //    ^?
 console.log('User 1:', user);
