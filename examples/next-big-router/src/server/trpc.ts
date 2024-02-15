2dd71005cb1d654f8ef69e7570fa06ca6407bfb2
KATT <alexander@n1s.se>
2024-02-15 14:50:10 +0100

    revert

--- examples/next-big-router/src/server/trpc.ts
+++ examples/next-big-router/src/server/trpc.ts
@@ -1,4 +1,3 @@
-import type { inferProcedureBuilderContext } from '@trpc/server';
 import { initTRPC } from '@trpc/server';
 import type { Context } from './context';
 
@@ -7,17 +6,3 @@ export const t = initTRPC.context<Context>().create();
 export const router = t.router;
 
 export const publicProcedure = t.procedure;
-
-export const somethingProcedure = t.procedure.use(({ ctx, next }) => {
-  return next({
-    ctx: {
-      ...ctx,
-      something:
-        'Hi, I am something that came from the somethingProcedure ctx!' as const,
-    },
-  });
-});
-
-export type SomethingProcedureCtx = inferProcedureBuilderContext<
-  typeof somethingProcedure
->;
