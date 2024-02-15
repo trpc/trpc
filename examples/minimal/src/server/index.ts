8a06e065bf70d01e529ac751981e48205b2d4d9e
KATT <alexander@n1s.se>
2024-02-15 16:19:52 +0100

    tweaks

--- examples/minimal/src/server/index.ts
+++ examples/minimal/src/server/index.ts
@@ -1,3 +1,6 @@
+/**
+ * This a minimal tRPC server
+ */
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import { z } from 'zod';
 import { db } from './db.js';
@@ -31,8 +34,7 @@ const appRouter = router({
   },
 });
 
-// Export type router type signature,
-// NOT the router itself.
+// Export type router type signature, this is used by the client.
 export type AppRouter = typeof appRouter;
 
 const server = createHTTPServer({
