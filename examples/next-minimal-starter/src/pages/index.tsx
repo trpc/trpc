1f32201c0750bae6ad4042e6b56f8f148431b135
KATT <alexander@n1s.se>
2024-03-06 17:38:36 +0100

    revert `examples/next-minimal-starter/src/pages/index.tsx`

--- examples/next-minimal-starter/src/pages/index.tsx
+++ examples/next-minimal-starter/src/pages/index.tsx
@@ -1,43 +1,19 @@
 /**
  * This is a Next.js page.
  */
-import { skipToken } from '@tanstack/react-query';
-import { useState } from 'react';
 import { trpc } from '../utils/trpc';
 
 export default function IndexPage() {
-  const [name, setName] = useState<string | undefined>();
-
   // 💡 Tip: CMD+Click (or CTRL+Click) on `greeting` to go to the server definition
-  const result = trpc.greeting.useQuery(name ? { name: name } : skipToken);
-
-  const textInput = (
-    <input
-      onChange={(e) => {
-        setName(e.currentTarget.value);
-      }}
-      value={name ?? ''}
-    />
-  );
-
-  if (result.isLoading) {
-    return (
-      <div style={styles}>
-        <h1>Loading...</h1>
-        {textInput}
-      </div>
-    );
-  }
+  const result = trpc.greeting.useQuery({ name: 'client' });
 
   if (!result.data) {
     return (
       <div style={styles}>
-        <h1>Type your name below</h1>
-        {textInput}
+        <h1>Loading...</h1>
       </div>
     );
   }
-
   return (
     <div style={styles}>
       {/**
@@ -47,7 +23,6 @@ export default function IndexPage() {
        * 💡 Tip: Secondary click on `text` and "Rename Symbol" to rename it both on the client & server
        */}
       <h1>{result.data.text}</h1>
-      {textInput}
     </div>
   );
 }
@@ -58,5 +33,4 @@ const styles = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  flexDirection: 'column',
-} as const;
+};
