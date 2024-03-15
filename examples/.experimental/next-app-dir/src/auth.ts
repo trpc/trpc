ac919664563e8e4bb789dbb741aae62d134be410
KATT <alexander@n1s.se>
2024-03-15 14:23:04 +0100

    mkay

--- examples/.experimental/next-app-dir/src/auth.ts
+++ examples/.experimental/next-app-dir/src/auth.ts
@@ -1,5 +1,6 @@
 import type { DefaultSession, NextAuthOptions } from 'next-auth';
 import { getServerSession } from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import GitHub from 'next-auth/providers/github';
 
 declare module 'next-auth' {
@@ -17,6 +18,36 @@ export const options = {
       clientId: process.env.GITHUB_ID!,
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
+
+        if (credentials) {
+          // Any object returned will be saved in `user` property of the JWT
+          return {
+            id: credentials.username,
+            name: credentials.username,
+            email: credentials.username,
+          };
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
   callbacks: {
