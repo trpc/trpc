468fb98ca283f624daf37ec89d87024aff060b87
Nick Lucas <me@nicklucas.co.uk>
2024-04-14 14:11:51 +0100

    Fix client types for File->Readable

--- examples/minimal-content-types/server/index.ts
+++ examples/minimal-content-types/server/index.ts
@@ -6,21 +6,26 @@ import type { Readable } from 'stream';
 import { initTRPC } from '@trpc/server';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import cors from 'cors';
-import { z } from 'zod';
+import type { z } from 'zod';
 
 const t = initTRPC.create();
 
 const publicProcedure = t.procedure;
 const router = t.router;
 
-function asType<TOut>(input: unknown): TOut {
-  console.log('asType', input);
-  return input as TOut;
+// A fake type parser which just expects the appropriate type to be passed through
+function asType<TOut, TIn = unknown>(): z.ZodType<TOut, any, TIn> {
+  return {
+    parse: (input: unknown) => {
+      console.log('parser received', input);
+      return input as TOut;
+    },
+  } as z.ZodType<TOut, any, TIn>;
 }
 
 const appRouter = router({
   // Input parsers set! (should expect the input to be loaded into memory)
-  formData: publicProcedure.input(asType<FormData>).mutation(({ input }) => {
+  formData: publicProcedure.input(asType<FormData>()).mutation(({ input }) => {
     const object = {} as Record<string, unknown>;
     input.forEach((value, key) => (object[key] = value));
 
@@ -31,20 +36,22 @@ const appRouter = router({
       data: object,
     };
   }),
-  file: publicProcedure.input(asType<Readable>).mutation(async ({ input }) => {
-    const chunks = [];
-    for await (const chunk of input) {
-      chunks.push(Buffer.from(chunk));
-    }
-    const content = Buffer.concat(chunks).toString('utf-8');
+  file: publicProcedure
+    .input(asType<Readable, File>())
+    .mutation(async ({ input }) => {
+      const chunks = [];
+      for await (const chunk of input) {
+        chunks.push(Buffer.from(chunk));
+      }
+      const content = Buffer.concat(chunks).toString('utf-8');
 
-    console.log('File: ', content);
+      console.log('File: ', content);
 
-    return {
-      text: 'ACK',
-      data: content,
-    };
-  }),
+      return {
+        text: 'ACK',
+        data: content,
+      };
+    }),
 
   // No input parser set! (should expect the input never gets loaded into memory)
   // formData: publicProcedure.mutation(({ input }) => {
