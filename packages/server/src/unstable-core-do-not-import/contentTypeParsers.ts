cbf505ba29b32c5856d88ff49820ec682b0b2c28
Nick Lucas <me@nicklucas.co.uk>
2024-04-19 13:55:54 +0100

    Add FileLike to support node 18

--- packages/server/src/unstable-core-do-not-import/contentTypeParsers.ts
+++ packages/server/src/unstable-core-do-not-import/contentTypeParsers.ts
@@ -9,10 +9,17 @@ export type UtilityParser<TInput, TOutput> = ParserZodEsque<TInput, TOutput> & {
 // Should be the same possible types as packages/client/src/links/internals/contentTypes.ts isOctetType
 
 /**
+ * File is only available from Node19+ but it always extends Blob so we can use that as a type until we eventually drop Node18
+ */
+interface FileLike extends Blob {
+  readonly name: string;
+}
+
+/**
  * When expecting a supported octet type to be passed from the frontend, this parser may be used to validate the type for your procedure. Note: the output is always a `Readable` stream.
  */
 export function parseOctetInput<
-  TInput extends Blob | Uint8Array,
+  TInput extends Blob | Uint8Array | FileLike,
 >(): UtilityParser<TInput, Readable> {
   return {
     _input: null as any as TInput,
@@ -24,7 +31,7 @@ export function parseOctetInput<
 
       throw new TRPCError({
         code: 'INTERNAL_SERVER_ERROR',
-        cause: `'Parsed input was expected to be a Readable but was: input'`,
+        cause: `Parsed input was expected to be a Readable but was: ${typeof input}`,
       });
     },
   };
