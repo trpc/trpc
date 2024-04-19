41ad83b95c09d188444c071839d44ebc456f4976
Nick Lucas <me@nicklucas.co.uk>
2024-04-19 14:36:55 +0100

    Fix experiment next-formdata example

--- examples/.experimental/next-formdata/src/pages/api/trpc/[trpc].ts
+++ examples/.experimental/next-formdata/src/pages/api/trpc/[trpc].ts
@@ -3,8 +3,6 @@
  * On a bigger app, you will probably want to split this file up into multiple files.
  */
 import * as trpcNext from '@trpc/server/adapters/next';
-import { nodeHTTPFormDataContentTypeHandler } from '@trpc/server/adapters/node-http/content-type/form-data';
-import { nodeHTTPJSONContentTypeHandler } from '@trpc/server/adapters/node-http/content-type/json';
 import { roomRouter } from '~/server/routers/room';
 import { createContext, router } from '~/server/trpc';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -20,10 +18,6 @@ export type AppRouter = typeof appRouter;
 const handler = trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
-  experimental_contentTypeHandlers: [
-    nodeHTTPFormDataContentTypeHandler(),
-    nodeHTTPJSONContentTypeHandler(),
-  ],
 });
 
 // export API handler
