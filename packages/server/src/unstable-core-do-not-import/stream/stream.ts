a33fd16ef749213746e34a2900077d900e05927a
KATT <alexander@n1s.se>
2024-05-05 18:10:57 +0200

    mv

--- packages/server/src/unstable-core-do-not-import/stream/stream.ts
+++ packages/server/src/unstable-core-do-not-import/stream/stream.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { isObject } from '../unstable-core-do-not-import/utils';
+import { isObject } from '../utils';
 
 // ---------- utils
 function isAsyncIterable<TValue>(
