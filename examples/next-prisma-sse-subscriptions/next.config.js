2b0c45222fb93ae03cf9b09647a067208df6bd01
KATT <alexander@n1s.se>
2024-05-12 15:29:44 +0200

    maybe

--- examples/next-prisma-sse-subscriptions/next.config.js
+++ examples/next-prisma-sse-subscriptions/next.config.js
@@ -10,7 +10,6 @@ const config = {
   publicRuntimeConfig: {
     // Will be available on both server and client
     APP_URL: process.env.APP_URL,
-    WS_URL: process.env.WS_URL,
   },
   /** We run eslint as a separate task in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
