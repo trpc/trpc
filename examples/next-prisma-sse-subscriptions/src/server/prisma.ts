63108ec6057087e23128076974e3532717c10154
KATT <alexander@n1s.se>
2024-05-13 15:26:00 +0200

    lint fix

--- examples/next-prisma-sse-subscriptions/src/server/prisma.ts
+++ examples/next-prisma-sse-subscriptions/src/server/prisma.ts
@@ -9,7 +9,7 @@ const prismaGlobal = global as typeof global & {
 };
 
 export const prisma: PrismaClient =
-  prismaGlobal.prisma ||
+  prismaGlobal.prisma ??
   new PrismaClient({
     log:
       process.env.NODE_ENV === 'development'
