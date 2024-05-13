8b3d9a5a3af1a2ec7882e5e4e721e296aa38c1d4
KATT <alexander@n1s.se>
2024-05-13 10:41:08 +0200

    rev moar

--- examples/next-prisma-sse-subscriptions/src/server/context.ts
+++ examples/next-prisma-sse-subscriptions/src/server/context.ts
@@ -6,7 +6,6 @@ import { getSession } from 'next-auth/react';
  * @link https://trpc.io/docs/v11/context
  */
 export const createContext = async (opts: FetchCreateContextFnOptions) => {
-  console.log('---------------');
   const session = await getSession({
     req: {
       headers: Object.fromEntries(opts.req.headers),
