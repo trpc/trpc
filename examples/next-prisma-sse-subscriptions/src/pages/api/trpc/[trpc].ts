af889a5e7a66f084384cf9da044b2d15bfb9571d
KATT <alexander@n1s.se>
2024-05-15 16:13:30 +0200

    wip - reverting the edge stuff

--- examples/next-prisma-sse-subscriptions/src/pages/api/trpc/[trpc].ts
+++ examples/next-prisma-sse-subscriptions/src/pages/api/trpc/[trpc].ts
@@ -1,33 +1,25 @@
 /**
  * This file contains the tRPC http response handler and context creation for Next.js
  */
+import * as trpcNext from '@trpc/server/adapters/next';
+
 import { createContext } from '~/server/context';
+import type { AppRouter } from '~/server/routers/_app';
 import { appRouter } from '~/server/routers/_app';
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import type { NextRequest } from 'next/server';
 
-export const runtime = 'nodejs';
-// This is required to enable streaming
-export const dynamic = 'force-dynamic';
-
-// export API handler
-export default async function handler(req: NextRequest) {
-  return fetchRequestHandler({
-    endpoint: '/api/trpc',
-    router: appRouter,
-    req,
-    /**
-     * @link https://trpc.io/docs/v11/context
-     */
-    createContext,
-    /**
-     * @link https://trpc.io/docs/v11/error-handling
-     */
-    onError({ error }) {
-      if (error.code === 'INTERNAL_SERVER_ERROR') {
-        // send to bug reporting
-        console.error('Something went wrong', error);
-      }
-    },
-  });
-}
+export default trpcNext.createNextApiHandler<AppRouter>({
+  router: appRouter,
+  /**
+   * @link https://trpc.io/docs/v11/context
+   */
+  createContext,
+  /**
+   * @link https://trpc.io/docs/v11/error-handling
+   */
+  onError({ error }) {
+    if (error.code === 'INTERNAL_SERVER_ERROR') {
+      // send to bug reporting
+      console.error('Something went wrong', error);
+    }
+  },
+});
--- examples/next-prisma-sse-subscriptions/src/server/context.ts
+++ examples/next-prisma-sse-subscriptions/src/server/context.ts
@@ -1,16 +1,11 @@
-import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
+import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
 import { getSession } from 'next-auth/react';
-
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/v11/context
  */
-export const createContext = async (opts: FetchCreateContextFnOptions) => {
-  const session = await getSession({
-    req: {
-      headers: Object.fromEntries(opts.req.headers),
-    },
-  });
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const session = await getSession(opts);
 
   console.log('createContext for', session?.user?.name ?? 'unknown user');
 
