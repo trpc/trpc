4a672ddd64db2dafccecb0ef86a6443942afd917
Julius Marminge <julius0216@outlook.com>
2024-05-26 09:47:54 +0200

    blog: trpc server actions (#5735)
    * initial draft
    
    * chore: apply lint and formatting fixes
    
    * path extactor helper
    
    * link to github
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex / KATT <alexander@n1s.se>
    
    * { meta }
    
    * finalizing
    
    * example rate limited action
    
    * reorder
    
    * console.log
    
    * banner
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Alex / KATT <alexander@n1s.se>

--- packages/server/src/adapters/next-app-dir.test.ts
+++ packages/server/src/adapters/next-app-dir.test.ts
@@ -1,4 +1,4 @@
-import { initTRPC } from '../@trpc/server';
+import { experimental_trpcMiddleware, initTRPC } from '../@trpc/server';
 import { nextAppDirCaller } from './next-app-dir/nextAppDirCaller';
 
 test('experimental caller', async () => {
@@ -47,4 +47,47 @@ test('with context', async () => {
       {},
     ),
   );
+});
+
+test('with path extractor', async () => {
+  interface Meta {
+    span: string;
+  }
+  const t = initTRPC.meta<Meta>().create();
+
+  // Some external middleware that relies on procedure paths
+  const loggerMiddleware = experimental_trpcMiddleware().create(
+    async (opts) => {
+      const start = new Date().getTime();
+      const result = await opts.next();
+      const duration = new Date().getTime() - start;
+
+      // eslint-disable-next-line no-console
+      console.log(`${opts.path} took ${duration}ms`);
+
+      return result;
+    },
+  );
+
+  const base = t.procedure
+    .experimental_caller(
+      nextAppDirCaller({
+        pathExtractor: ({ meta }) => (meta as Meta).span,
+      }),
+    )
+    .use(loggerMiddleware);
+
+  const loggerSpy = vi.spyOn(console, 'log').mockImplementation(() => {
+    // no-op
+  });
+
+  {
+    const proc = base.meta({ span: 'hello' }).query(async () => 'hello');
+    const result = await proc();
+    expect(result).toBe('hello');
+
+    expect(loggerSpy).toHaveBeenCalledWith(
+      expect.stringContaining('hello took'),
+    );
+  }
 });
--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -48,14 +48,14 @@ module.exports = {
       // contextualSearch: true,
       // searchParameters: {},
     },
-    // announcementBar: {
-    //   id: 'drift',
-    //   content:
-    //     "🚀 We've just released a beta version of <strong>tRPC Drift</strong> which helps you keep track of changes in your tRPC API. Check it out at <a href='https://drift.trpc.io'><strong>drift.trpc.io</strong></a>.",
-    //   backgroundColor: 'var(--ifm-color-primary-dark)',
-    //   textColor: '#ffffff',
-    //   isCloseable: true,
-    // },
+    announcementBar: {
+      id: 'actions',
+      content:
+        "🚀 New blog post live! Check out how to use Server Actions with tRPC <a href='/blog/trpc-actions'><strong>here</strong></a>.",
+      backgroundColor: 'var(--ifm-color-primary-dark)',
+      textColor: '#ffffff',
+      isCloseable: true,
+    },
     navbar: {
       title: 'tRPC',
       logo: {
