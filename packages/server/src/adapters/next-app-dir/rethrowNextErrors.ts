a4f5d854168964a872610d94d4aa359789db760b
juliusmarminge <julius0216@outlook.com>
2024-06-02 10:21:51 +0200

    comment

--- packages/server/src/adapters/next-app-dir/rethrowNextErrors.ts
+++ packages/server/src/adapters/next-app-dir/rethrowNextErrors.ts
@@ -5,6 +5,7 @@ import { TRPCRedirectError } from './redirect';
 /**
  * @remarks The helpers from `next/dist/client/components/*` has been removed in Next.js 15.
  * Inlining them here instead...
+ * @see https://github.com/vercel/next.js/blob/5ae286ffd664e5c76841ed64f6e2da85a0835922/packages/next/src/client/components/redirect.ts#L97-L123
  */
 const REDIRECT_ERROR_CODE = 'NEXT_REDIRECT';
 function isRedirectError(error: unknown) {
@@ -26,6 +27,11 @@ function isRedirectError(error: unknown) {
   );
 }
 
+/**
+ * @remarks The helpers from `next/dist/client/components/*` has been removed in Next.js 15.
+ * Inlining them here instead...
+ * @see https://github.com/vercel/next.js/blob/5ae286ffd664e5c76841ed64f6e2da85a0835922/packages/next/src/client/components/not-found.ts#L33-L39
+ */
 const NOT_FOUND_ERROR_CODE = 'NEXT_NOT_FOUND';
 function isNotFoundError(error: unknown) {
   if (typeof error !== 'object' || error === null || !('digest' in error)) {
