3803ad07e1b83464b79a87cb1f3c757664386ca3
KATT <alexander@n1s.se>
2024-06-08 23:35:02 +0200

    wat

--- examples/next-prisma-sse-subscriptions/playwright.config.ts
+++ examples/next-prisma-sse-subscriptions/playwright.config.ts
@@ -17,12 +17,6 @@ const config: PlaywrightTestConfig = {
     headless: opts.headless,
   },
   retries: process.env.CI ? 3 : 0,
-  webServer: {
-    command: 'yarn dev',
-    port: 3000,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
 };
 
 export default config;
