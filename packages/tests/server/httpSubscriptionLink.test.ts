716bb56b138cdc049e94b4a0366795164ab39380
KATT <alexander@n1s.se>
2024-06-08 19:05:06 +0200

    wa

--- packages/tests/server/httpSubscriptionLink.test.ts
+++ packages/tests/server/httpSubscriptionLink.test.ts
@@ -212,7 +212,8 @@ test('disconnect and reconnect with an event id', async () => {
     expect(onStarted).toHaveBeenCalledTimes(1);
   });
 
-  const es = onStarted.mock.calls[0]![0].context?.eventSource;
+  // eslint-disable-next-line @typescript-eslint/dot-notation
+  const es = onStarted.mock.calls[0]![0].context?.['eventSource'];
   assert(es instanceof EventSource);
 
   await waitFor(() => {
