d8b4fda24eb6a110e3723637fcb61f60db800541
KATT <alexander@n1s.se>
2024-06-22 22:00:29 +0200

    providers are fine

--- examples/next-sse-chat/src/app/channels/[channelId]/page.tsx
+++ examples/next-sse-chat/src/app/channels/[channelId]/page.tsx
@@ -1,18 +1,9 @@
-import { HashtagIcon } from '@heroicons/react/24/outline';
-import { CreateChannelDialog } from '~/app/channels/create-channel';
-import { Button } from '~/components/button';
-import { auth, SignedIn, SignedOut, signIn, signOut } from '~/server/auth';
-import { caller } from '~/server/routers/_app';
-import { cx } from 'class-variance-authority';
-import Link from 'next/link';
-import { Suspense } from 'react';
 import { Chat } from './chat';
 
 export default async function Home(
   props: Readonly<{ params: { channelId: string } }>,
 ) {
   const channelId = props.params.channelId;
-  const session = await auth();
 
-  return <Chat session={session} channelId={channelId} />;
+  return <Chat channelId={channelId} key={channelId} />;
 }
