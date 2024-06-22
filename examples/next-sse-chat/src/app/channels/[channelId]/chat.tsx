9a68c4df6f88133f40ca7bb4e510219c504e022e
Alex / KATT <alexander@n1s.se>
2024-06-22 21:00:13 +0200

    chore(sse example): fix infinite scroll + anchor scroll to bottom (#5820)

--- examples/next-sse-chat/src/app/channels/[channelId]/chat.tsx
+++ examples/next-sse-chat/src/app/channels/[channelId]/chat.tsx
@@ -15,84 +15,119 @@ import {
   useWhoIsTyping,
 } from './hooks';
 
+const pluralize = (count: number, singular: string, plural: string) =>
+  count === 1 ? singular : plural;
+
+const listWithAnd = (list: string[]) => {
+  if (list.length === 0) {
+    return '';
+  }
+  if (list.length === 1) {
+    return list[0];
+  }
+  if (list.length === 2) {
+    return `${list[0]} and ${list[1]}`;
+  }
+  return `${list.slice(0, -1).join(', ')}, and ${list.at(-1)}`;
+};
+
 export function Chat(
   props: Readonly<{ session: Session | null; channelId: string }>,
 ) {
   const { channelId } = props;
   const livePosts = useLivePosts(channelId);
   const currentlyTyping = useWhoIsTyping(channelId);
-  const scrollTargetRef = React.useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    // scroll to bottom on mount
-    scrollTargetRef.current?.scrollIntoView({
-      behavior: 'smooth',
-      block: 'end',
-    });
-  }, []);
+  const scrollRef = React.useRef<HTMLDivElement>(null);
 
   return (
     <main className="flex-1 overflow-hidden">
       <div className="flex h-full flex-col">
-        <div className="flex-1 overflow-y-scroll p-4 sm:p-6 lg:p-8">
-          <div className="grid gap-4">
-            {livePosts.messages?.map((item) => {
-              const isMe = item.author === props.session?.user?.name;
-
-              return (
-                <div
-                  key={item.id}
-                  className={cx(
-                    'flex items-start gap-3',
-                    isMe ? 'justify-end' : 'justify-start',
-                  )}
+        <div
+          className="flex flex-1 flex-col-reverse overflow-y-scroll p-4 sm:p-6 lg:p-8"
+          ref={scrollRef}
+        >
+          {/* Inside this div things will not be reversed. */}
+          <div>
+            <div className="grid gap-4">
+              <div>
+                <Button
+                  disabled={
+                    !livePosts.query.hasNextPage ||
+                    livePosts.query.isFetchingNextPage
+                  }
+                  onClick={() => {
+                    livePosts.query.fetchNextPage();
+                  }}
                 >
-                  <Avatar
-                    alt="Avatar"
-                    className="size-8"
-                    initials={item.author?.substring(0, 2)}
-                    src={`https://github.com/${item.author}.png`}
-                  />
+                  {livePosts.query.isFetchingNextPage
+                    ? 'Loading...'
+                    : !livePosts.query.hasNextPage
+                    ? 'Fetched everything!'
+                    : 'Load more'}
+                </Button>
+              </div>
+              {livePosts.messages?.map((item) => {
+                const isMe = item.author === props.session?.user?.name;
 
-                  <div className="flex flex-col gap-1">
-                    <div
-                      className={cx(
-                        'rounded-lg bg-gray-100 p-3 text-sm ',
-                        isMe
-                          ? 'bg-gray-300 dark:bg-gray-800'
-                          : 'bg-gray-200 dark:bg-gray-700',
-                      )}
-                    >
-                      <p>{item.text}</p>
-                    </div>
-                    <div className="text-xs text-gray-500 dark:text-gray-400">
-                      {item.author} ·{' '}
-                      {isToday(item.createdAt)
-                        ? formatDistanceToNow(item.createdAt) + ' ago'
-                        : format(item.createdAt, 'MMM d, yyyy h:mm a')}
+                return (
+                  <div
+                    key={item.id}
+                    className={cx(
+                      'flex items-start gap-3',
+                      isMe ? 'justify-end' : 'justify-start',
+                    )}
+                  >
+                    <Avatar
+                      alt="Avatar"
+                      className="size-8"
+                      initials={item.author?.substring(0, 2)}
+                      src={`https://github.com/${item.author}.png`}
+                    />
+
+                    <div className="flex flex-col gap-1">
+                      <div
+                        className={cx(
+                          'rounded-lg bg-gray-100 p-3 text-sm ',
+                          isMe
+                            ? 'bg-gray-300 dark:bg-gray-800'
+                            : 'bg-gray-200 dark:bg-gray-700',
+                        )}
+                      >
+                        <p>{item.text}</p>
+                      </div>
+                      <div className="text-xs text-gray-500 dark:text-gray-400">
+                        {item.author} ·{' '}
+                        {isToday(item.createdAt)
+                          ? formatDistanceToNow(item.createdAt) + ' ago'
+                          : format(item.createdAt, 'MMM d, yyyy h:mm a')}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-            <div ref={scrollTargetRef} />
+                );
+              })}
+            </div>
+            <p className="text-sm italic text-gray-400">
+              {currentlyTyping.length ? (
+                `${listWithAnd(currentlyTyping)} ${pluralize(
+                  currentlyTyping.length,
+                  'is',
+                  'are',
+                )} typing...`
+              ) : (
+                <>&nbsp;</>
+              )}
+            </p>
           </div>
-          <p className="text-sm italic text-gray-400">
-            {currentlyTyping.length ? (
-              `${currentlyTyping.join(', ')} typing...`
-            ) : (
-              <>&nbsp;</>
-            )}
-          </p>
         </div>
         <div className="border-t bg-white p-2 dark:border-gray-800 dark:bg-gray-900">
           <AddMessageForm
             signedIn={!!props.session?.user}
             channelId={channelId}
             onMessagePost={() => {
-              scrollTargetRef.current?.scrollIntoView({
+              scrollRef.current?.scroll({
+                // `top: 0` is actually the bottom of the chat due to `flex-col-reverse`
+                top: 0,
                 behavior: 'smooth',
-                block: 'end',
               });
             }}
           />
