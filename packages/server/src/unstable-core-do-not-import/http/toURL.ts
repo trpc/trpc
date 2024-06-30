0fe6f76581066433481857501c1ffb26192d4bb1
KATT <alexander@n1s.se>
2024-06-30 03:00:31 +0200

    wat

--- packages/server/src/unstable-core-do-not-import/http/toURL.ts
+++ packages/server/src/unstable-core-do-not-import/http/toURL.ts
@@ -1,7 +1,4 @@
-export function toURL(urlOrPathname: string | URL): URL {
-  if (urlOrPathname instanceof URL) {
-    return urlOrPathname;
-  }
+export function toURL(urlOrPathname: string): URL {
   const url = urlOrPathname.startsWith('/')
     ? `http://127.0.0.1${urlOrPathname}`
     : urlOrPathname;
