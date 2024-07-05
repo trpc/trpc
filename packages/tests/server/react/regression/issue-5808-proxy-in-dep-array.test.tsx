3b31a05601987118dd3c623b70cbd64c4797fe8d
KATT <alexander@n1s.se>
2024-07-05 08:51:29 +0200

    mv

--- packages/tests/server/react/regression/issue-5808-proxy-in-dep-array.test.tsx
+++ packages/tests/server/react/regression/issue-5808-proxy-in-dep-array.test.tsx
@@ -1,63 +1,185 @@
 import { getServerAndReactClient } from '../__reactHelpers';
+import {
+  defaultShouldDehydrateQuery,
+  QueryClient,
+} from '@tanstack/react-query';
 import { render, waitFor } from '@testing-library/react';
+import { createHydrationHelpers } from '@trpc/react-query/rsc';
 import { initTRPC } from '@trpc/server';
 import { konn } from 'konn';
 import * as React from 'react';
 import { z } from 'zod';
 
-const ctx = konn()
-  .beforeEach(() => {
-    const { router, procedure } = initTRPC.create();
+describe('original regression', () => {
+  const ctx = konn()
+    .beforeEach(() => {
+      const { router, procedure } = initTRPC.create();
 
-    const appRouter = router({
-      deeply: {
-        nested: {
-          greeting: procedure.query(() => 'hello'),
-          echo: procedure.input(z.string()).query((opts) => opts.input),
+      const appRouter = router({
+        deeply: {
+          nested: {
+            greeting: procedure.query(() => 'hello'),
+            echo: procedure.input(z.string()).query((opts) => opts.input),
+          },
         },
-      },
+      });
+
+      return getServerAndReactClient(appRouter);
+    })
+    .afterEach(async (ctx) => {
+      await ctx?.close?.();
+    })
+    .done();
+
+  test('utils proxy in dependency array', async () => {
+    const { client, App } = ctx;
+    const nonce = '_______________nonce_______________';
+    let effectCount = 0;
+
+    function MyComponent() {
+      const result = client.deeply.nested.greeting.useQuery(undefined);
+      const utils = client.useUtils();
+
+      React.useEffect(() => {
+        utils.deeply.nested.greeting.setData(undefined, nonce);
+        effectCount++;
+      }, [utils.deeply.nested.greeting]);
+
+      return <pre>{result.data}</pre>;
+    }
+
+    const utils = render(
+      <App>
+        <MyComponent />
+      </App>,
+    );
+
+    await waitFor(() => {
+      expect(utils.container).toHaveTextContent(nonce);
     });
-
-    return getServerAndReactClient(appRouter);
-  })
-  .afterEach(async (ctx) => {
-    await ctx?.close?.();
-  })
-  .done();
-
-test('utils proxy in dependency array', async () => {
-  const { client, App } = ctx;
-  const nonce = '_______________nonce_______________';
-  let effectCount = 0;
-
-  function MyComponent() {
-    const result = client.deeply.nested.greeting.useQuery(undefined);
-    const utils = client.useUtils();
-
-    React.useEffect(() => {
-      utils.deeply.nested.greeting.setData(undefined, nonce);
-      effectCount++;
-    }, [utils.deeply.nested.greeting]);
-
-    return <pre>{result.data}</pre>;
-  }
-
-  const utils = render(
-    <App>
-      <MyComponent />
-    </App>,
-  );
-
-  await waitFor(() => {
-    expect(utils.container).toHaveTextContent(nonce);
+    expect(effectCount).toBe(1);
   });
-  expect(effectCount).toBe(1);
+
+  test('call twice', async () => {
+    const client = ctx.opts.client;
+
+    const nested = client.deeply.nested;
+    expect(await nested.echo.query('hello')).toBe('hello');
+    expect(await nested.echo.query('world')).toBe('world');
+  });
 });
 
-test('call twice', async () => {
-  const client = ctx.opts.client;
+describe('RSC', () => {
+  // mock of React.cache deduplication
+  const cache = <T extends (...args: any[]) => any>(fn: T) => {
+    const cache = new Map<string, ReturnType<T>>();
+    return (...args: Parameters<T>) => {
+      const key = JSON.stringify(args);
+      if (cache.has(key)) {
+        return cache.get(key);
+      }
+      const result = fn(...args);
+      cache.set(key, result);
+      return result;
+    };
+  };
 
-  const nested = client.deeply.nested;
-  expect(await nested.echo.query('hello')).toBe('hello');
-  expect(await nested.echo.query('world')).toBe('world');
+  const ctx = konn()
+    .beforeEach(() => {
+      const t = initTRPC.create();
+      const appRouter = t.router({
+        post: t.router({
+          byId: t.procedure
+            .input(
+              z.object({
+                id: z.string(),
+              }),
+            )
+            .query(() => '__result' as const),
+        }),
+      });
+
+      const ctx = getServerAndReactClient(appRouter);
+
+      const serverQueryClient = new QueryClient({
+        defaultOptions: {
+          queries: {
+            // Since queries are prefetched on the server, we set a stale time so that
+            // queries aren't immediately refetched on the client
+            staleTime: 1000 * 30,
+          },
+          dehydrate: {
+            // include pending queries in dehydration
+            shouldDehydrateQuery: (query) =>
+              defaultShouldDehydrateQuery(query) ||
+              query.state.status === 'pending',
+          },
+        },
+      });
+
+      const createTRPCContext = cache(() => ({}));
+      const getQueryClient = cache(() => serverQueryClient);
+      const serverClient = t.createCallerFactory(appRouter)(createTRPCContext);
+
+      const { trpc, HydrateClient } = createHydrationHelpers<typeof appRouter>(
+        serverClient,
+        getQueryClient,
+      );
+
+      return {
+        ...ctx,
+        trpc,
+        HydrateClient,
+        getQueryClient,
+      };
+    })
+    .afterEach(async (ctx) => {
+      await ctx?.close?.();
+    })
+    .done();
+
+  test('rsc prefetch helpers', async () => {
+    const { client, App, trpc, HydrateClient, getQueryClient } = ctx;
+
+    const fetchSpy = vi.spyOn(globalThis, 'fetch');
+
+    function MyComponent() {
+      const { data } = client.post.byId.useQuery({
+        id: '1',
+      });
+
+      return <>{data}</>;
+    }
+
+    // Imaginary RSC prefetch parent component
+    function Parent() {
+      void trpc.post.byId.prefetch({ id: '1' });
+      return (
+        <HydrateClient>
+          <MyComponent />
+        </HydrateClient>
+      );
+    }
+
+    const utils1 = render(
+      <App>
+        <Parent />
+      </App>,
+    );
+    await waitFor(() => {
+      expect(utils1.container).toHaveTextContent(`__result`);
+    });
+
+    const utils2 = render(
+      <App>
+        <Parent />
+      </App>,
+    );
+    await waitFor(() => {
+      expect(utils2.container).toHaveTextContent(`__result`);
+    });
+
+    // Should not have fetched from CC but taken promise from server client
+    expect(fetchSpy).toHaveBeenCalledTimes(0);
+  });
 });
