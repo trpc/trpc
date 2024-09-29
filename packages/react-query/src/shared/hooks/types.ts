167ff7a361d63f4ac906e657d382f214dcca449f
KATT <alexander@n1s.se>
2024-09-29 18:53:15 +0200

    fix moar

--- packages/react-query/src/shared/hooks/types.ts
+++ packages/react-query/src/shared/hooks/types.ts
@@ -160,10 +160,11 @@ export interface UseTRPCMutationOptions<
 > extends UseMutationOptions<TOutput, TError, TInput, TContext>,
     TRPCUseQueryBaseOptions {}
 
+type coerceAsyncIterable<T> = T extends AsyncIterable<infer U> ? U : T;
 export interface UseTRPCSubscriptionOptions<TOutput, TError> {
   enabled?: boolean;
   onStarted?: () => void;
-  onData: (data: TOutput) => void;
+  onData: (data: coerceAsyncIterable<TOutput>) => void;
   onError?: (err: TError) => void;
 }
 export interface TRPCProviderProps<TRouter extends AnyRouter, TSSRContext>
