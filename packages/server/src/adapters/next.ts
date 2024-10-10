437c6b370d8d1e5427b813be857662a802323fe3
KATT <alexander@n1s.se>
2024-10-11 00:12:50 +0200

    cool

--- packages/server/src/adapters/next.ts
+++ packages/server/src/adapters/next.ts
@@ -13,12 +13,12 @@ import type { AnyRouter } from '../@trpc/server';
 // @trpc/server
 import { TRPCError } from '../@trpc/server';
 // eslint-disable-next-line no-restricted-imports
-import { getErrorShape } from '../unstable-core-do-not-import';
+import { getErrorShape, run } from '../unstable-core-do-not-import';
 import type {
   NodeHTTPCreateContextFnOptions,
   NodeHTTPHandlerOptions,
 } from './node-http';
-import { nodeHTTPRequestHandler } from './node-http';
+import { internal_exceptionHandler, nodeHTTPRequestHandler } from './node-http';
 
 export type CreateNextContextOptions = NodeHTTPCreateContextFnOptions<
   NextApiRequest,
@@ -33,44 +33,37 @@ export type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 export function createNextApiHandler<TRouter extends AnyRouter>(
   opts: NodeHTTPHandlerOptions<TRouter, NextApiRequest, NextApiResponse>,
 ): NextApiHandler {
+  let path = '';
   return async (req, res) => {
-    function getPath(): string | null {
-      if (typeof req.query['trpc'] === 'string') {
-        return req.query['trpc'];
-      }
-      if (Array.isArray(req.query['trpc'])) {
-        return req.query['trpc'].join('/');
-      }
-      return null;
-    }
-    const path = getPath();
-
-    if (path === null) {
-      const error = getErrorShape({
-        config: opts.router._def._config,
-        error: new TRPCError({
+    await run(async () => {
+      function getPath() {
+        if (typeof req.query['trpc'] === 'string') {
+          return req.query['trpc'];
+        }
+        if (Array.isArray(req.query['trpc'])) {
+          return req.query['trpc'].join('/');
+        }
+        throw new TRPCError({
           message:
             'Query "trpc" not found - is the file named `[trpc]`.ts or `[...trpc].ts`?',
           code: 'INTERNAL_SERVER_ERROR',
-        }),
-        type: 'unknown',
-        ctx: undefined,
-        path: undefined,
-        input: undefined,
-      });
-      res.statusCode = 500;
-      res.json({
-        id: -1,
-        error,
-      });
-      return;
-    }
+        });
+      }
+      path = getPath();
 
-    await nodeHTTPRequestHandler({
-      ...(opts as any),
-      req,
-      res,
-      path,
-    });
+      await nodeHTTPRequestHandler({
+        ...(opts as any),
+        req,
+        res,
+        path,
+      });
+    }).catch(
+      internal_exceptionHandler({
+        req,
+        res,
+        path,
+        ...opts,
+      }),
+    );
   };
 }
