a79b2da629d2c57cab2e5b48acb12c0319646e4c
KATT <alexander@n1s.se>
2024-10-12 15:26:58 +0200

    tweak

--- packages/client/src/links/httpSubscriptionLink.ts
+++ packages/client/src/links/httpSubscriptionLink.ts
@@ -201,12 +201,12 @@ export function unstable_httpSubscriptionLink<
           });
           observer.complete();
         }).catch((error) => {
-          observer.error(TRPCClientError.from(error));
           connectionState.next({
             type: 'state',
             state: 'idle',
             error: null,
           });
+          observer.error(TRPCClientError.from(error));
         });
 
         return () => {
