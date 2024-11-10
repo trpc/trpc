7409b8df6e9b442a85f93ace5e5003eaa99d8590
Alexander Johansson <alexander@n1s.se>
2024-11-10 15:46:52 +0100

    meow

--- packages/tests/server/using.test.ts
+++ packages/tests/server/using.test.ts
@@ -1,16 +1,15 @@
 import { expect, test } from 'vitest';
 
 test('should handle async disposable resources', async () => {
-  let disposed = false;
 
   const resource = {
     [Symbol.asyncDispose]: async () => {
-      disposed = true;
+      // do nothing
     },
     value: 42,
   };
 
   await using thing = resource;
-  expect(disposed).toBe(false);
+
   expect(thing.value).toBe(42);
 });
