16f40e59ab90536e986115b7620755aa1c163555
Alexander Johansson <alexander@n1s.se>
2024-11-29 01:20:07 +0100

    this could maybe work

--- packages/server/src/unstable-core-do-not-import/stream/utils/readableStreamFrom.test.ts
+++ packages/server/src/unstable-core-do-not-import/stream/utils/readableStreamFrom.test.ts
@@ -49,12 +49,26 @@ test('calls return on iterator when stream is cancelled', async () => {
   expect(returnMock).toHaveBeenCalled();
 });
 
-function overrideCancel<T>(stream: ReadableStream<T>) {
-  const originalCancel = stream.cancel.bind(stream);
-
-  const mock = vi.spyOn(stream, 'cancel').mockImplementation(originalCancel);
-
-  return mock;
+function streamWithEndCallback<T>(
+  stream: ReadableStream<T>,
+  onEnd: () => void,
+) {
+  const reader = stream.getReader();
+  return new ReadableStream({
+    async pull(controller) {
+      const result = await reader.read();
+      if (result.done) {
+        controller.close();
+        onEnd();
+        return;
+      }
+      controller.enqueue(result.value);
+    },
+    async cancel() {
+      await reader.cancel();
+      onEnd();
+    },
+  });
 }
 
 test('override cancel', async () => {
@@ -67,9 +81,11 @@ test('override cancel', async () => {
 
   const stream = readableStreamFrom(generator());
 
-  const mock = overrideCancel(stream);
+  const onEnd = vi.fn();
 
-  const reader = stream.getReader();
+  const withInfo = streamWithEndCallback(stream, onEnd);
+
+  const reader = withInfo.getReader();
   const res: number[] = [];
   while (true) {
     const result = await reader.read();
@@ -79,5 +95,25 @@ test('override cancel', async () => {
 
   expect(res).toEqual(values);
 
-  expect(mock).toHaveBeenCalled();
+  expect(onEnd).toHaveBeenCalledOnce();
+});
+
+test('cancel should work', async () => {
+  const values = [1, 2, 3];
+  async function* generator() {
+    for (const value of values) {
+      yield value;
+    }
+  }
+
+  const stream = readableStreamFrom(generator());
+  const onEnd = vi.fn();
+  const withInfo = streamWithEndCallback(stream, onEnd);
+
+  const reader = withInfo.getReader();
+  const result = await reader.read();
+  expect(result.value).toBe(1);
+
+  await reader.cancel();
+  expect(onEnd).toHaveBeenCalledOnce();
 });
