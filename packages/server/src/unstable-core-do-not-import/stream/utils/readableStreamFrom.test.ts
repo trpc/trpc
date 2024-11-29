43a2b37719498a206757c8b912e1234f04dda917
Alexander Johansson <alexander@n1s.se>
2024-11-29 01:03:56 +0100

    example
    https://github.com/trpc/trpc/pull/6228#issuecomment-2506861324

--- packages/server/src/unstable-core-do-not-import/stream/utils/readableStreamFrom.test.ts
+++ packages/server/src/unstable-core-do-not-import/stream/utils/readableStreamFrom.test.ts
@@ -48,3 +48,36 @@ test('calls return on iterator when stream is cancelled', async () => {
 
   expect(returnMock).toHaveBeenCalled();
 });
+
+function overrideCancel<T>(stream: ReadableStream<T>) {
+  const originalCancel = stream.cancel.bind(stream);
+
+  const mock = vi.spyOn(stream, 'cancel').mockImplementation(originalCancel);
+
+  return mock;
+}
+
+test('override cancel', async () => {
+  const values = [1, 2, 3];
+  async function* generator() {
+    for (const value of values) {
+      yield value;
+    }
+  }
+
+  const stream = readableStreamFrom(generator());
+
+  const mock = overrideCancel(stream);
+
+  const reader = stream.getReader();
+  const res: number[] = [];
+  while (true) {
+    const result = await reader.read();
+    if (result.done) break;
+    res.push(result.value);
+  }
+
+  expect(res).toEqual(values);
+
+  expect(mock).toHaveBeenCalled();
+});
