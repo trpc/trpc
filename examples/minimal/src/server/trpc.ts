1ad1a1ec34306113f40cf117aa307226e835f1dd
Alex / KATT <alexander@n1s.se>
2024-12-29 22:01:44 +0100

    chore: fix stackblitz output types (#6346)

--- examples/minimal/src/server/trpc.ts
+++ examples/minimal/src/server/trpc.ts
@@ -1,10 +1,13 @@
 import { initTRPC } from '@trpc/server';
+import { transformer } from '../shared/transformer.js';
 
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.create();
+const t = initTRPC.create({
+  transformer,
+});
 
 /**
  * Export reusable router and procedure helpers
