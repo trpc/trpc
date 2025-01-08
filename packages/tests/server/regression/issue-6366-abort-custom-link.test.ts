dc3e82e83de84d3cbf567aa5ebcca761c04b16a4
Alexander Johansson <alexander@n1s.se>
2025-01-08 14:53:42 +0100

    meep

--- packages/tests/server/regression/issue-6366-abort-custom-link.test.ts
+++ packages/tests/server/regression/issue-6366-abort-custom-link.test.ts
@@ -4,6 +4,30 @@ import type { AnyRouter } from '@trpc/server';
 import { initTRPC } from '@trpc/server';
 import { observable } from '@trpc/server/observable';
 
+type Maybe<T> = T | null | undefined;
+
+/**
+ * Like `Promise.race` but for abort signals
+ *
+ * Basically, a ponyfill for
+ * [`AbortSignal.any`](https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/any_static).
+ */
+export function raceAbortSignals(
+  ...signals: Maybe<AbortSignal>[]
+): AbortSignal {
+  const ac = new AbortController();
+
+  for (const signal of signals) {
+    if (signal?.aborted) {
+      ac.abort();
+    } else {
+      signal?.addEventListener('abort', () => ac.abort(), { once: true });
+    }
+  }
+
+  return ac.signal;
+}
+
 test('abort', async () => {
   const t = initTRPC.create({ isServer: true });
 
@@ -19,11 +43,26 @@ test('abort', async () => {
     return () => {
       return ({ op }) => {
         return observable(() => {
-          console.log('op', op);
-          console.log('signal', op.signal); // the signal from L37
+          const ac = new AbortController();
+
+          // This combined signal can be used for e.g. a `fetch()`-request
+          const combinedSignal = raceAbortSignals(op.signal, ac.signal);
+
+          console.log('the passed in signal:', op.signal);
+
+          combinedSignal.addEventListener(
+            'abort',
+            () => {
+              console.log('aborted signal');
+            },
+            { once: true },
+          );
 
           return () => {
+            // subscription.unsubscribe() was called
             console.log('unsubscribed');
+
+            ac.abort();
           };
         });
       };
@@ -34,7 +73,7 @@ test('abort', async () => {
   });
 
   const abort = trpcClient.testSubscribe.subscribe(undefined, {
-    signal: new AbortController().signal, // <--- you can pass a signal here
+    signal: new AbortController().signal, // <--- you can pass a custom signal here
   });
-  abort.unsubscribe(); // <-- Can't detect this in the link in any way as far as I can tell
+  abort.unsubscribe(); // Will call the abort signal
 });
