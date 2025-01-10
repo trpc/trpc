0925af9bdfb534cf858e8ce6e6e3f4086036987b
Alexander Johansson <alexander@n1s.se>
2025-01-10 23:06:27 +0100

    meep

--- packages/tests/server/react/regression/issue-6374-untyped-client.test.tsx
+++ packages/tests/server/react/regression/issue-6374-untyped-client.test.tsx
@@ -1,8 +1,10 @@
+import { serverResource } from '../../../../server/src/unstable-core-do-not-import/stream/utils/__tests__/serverResource';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { httpLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { initTRPC } from '@trpc/server';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import React from 'react';
 
 // Create router and client
@@ -12,44 +14,77 @@ const appRouter = t.router({
 });
 const trpc = createTRPCReact<typeof appRouter>();
 
-test('issue-6374-untyped-client', async () => {
-  function MyComponent() {
-    trpc.greeting.useQuery();
-    const utils = trpc.useUtils();
+function App(props: { children: React.ReactNode; url: string }) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  const [trpcClient] = React.useState(() =>
+    trpc.createClient({
+      links: [
+        httpLink({
+          url: props.url,
+        }),
+      ],
+    }),
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
 
+test('useQuery()', async () => {
+  await using server = serverResource((req) =>
+    fetchRequestHandler({
+      router: appRouter,
+      createContext: () => ({}),
+      req,
+      endpoint: '/',
+    }),
+  );
+  function MyComponent() {
+    const greeting = trpc.greeting.useQuery();
+
+    return <div>{greeting.data}</div>;
+  }
+  const utils = render(
+    <App url={server.url}>
+      <MyComponent />
+    </App>,
+  );
+
+  await waitFor(() => {
+    expect(utils.container).toHaveTextContent('hello');
+  });
+});
+
+test('useUtils()', async () => {
+  await using server = serverResource((req) =>
+    fetchRequestHandler({
+      router: appRouter,
+      createContext: () => ({}),
+      req,
+      endpoint: '/',
+    }),
+  );
+
+  function MyComponent() {
+    const [data, setData] = React.useState<string>();
+    const utils = trpc.useUtils();
     React.useEffect(() => {
-      utils.greeting.prefetch();
+      utils.greeting.fetch().then((data) => setData(data));
     }, [utils]);
 
-    return <div>Test Component</div>;
+    return <div>{data}</div>;
   }
-  function App() {
-    const [queryClient] = React.useState(() => new QueryClient());
-    const [trpcClient] = React.useState(() =>
-      trpc.createClient({
-        links: [
-          httpLink({
-            url: 'http://localhost:3000',
-          }),
-        ],
-      }),
-    );
-    return (
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-          <MyComponent />
-        </QueryClientProvider>
-      </trpc.Provider>
-    );
-  }
+  const utils = render(
+    <App url={server.url}>
+      <MyComponent />
+    </App>,
+  );
 
-  const utils = render(<App />);
-
-  expect(utils.container).toMatchInlineSnapshot(`
-    <div>
-      <div>
-        Test Component
-      </div>
-    </div>
-  `);
+  await waitFor(() => {
+    expect(utils.container).toHaveTextContent('hello');
+  });
 });
