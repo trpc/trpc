4b02b75fc9c266f1a02745718ff1767c25455678
AlexJWayne <alex.wayne@solvenergy.com>
2025-01-09 19:57:33 -0800

    Improve useQueries combine test
    By making it conform to the other tests

--- packages/tests/server/react/useQueries.test.tsx
+++ packages/tests/server/react/useQueries.test.tsx
@@ -161,7 +161,7 @@ test('combine function', async () => {
       },
     );
 
-    return <pre>{results}</pre>;
+    return <pre>{JSON.stringify(results ?? 'n/a', null, 4)}</pre>;
   }
 
   const utils = render(
