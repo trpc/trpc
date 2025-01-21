7f54c96a215b238ff409731999f84c70363f6176
Alexander Johansson <alexander@n1s.se>
2025-01-21 11:07:44 +0100

    wip

--- examples/.test/monotest-app/src/App.tsx
+++ examples/.test/monotest-app/src/App.tsx
@@ -1,21 +1,25 @@
-
 import { useEffect } from 'react';
-import { trpcReact } from './trpc-setup'
+import { trpcReact, TRPCReactProvider } from './trpc-setup';
 
-export function App() {
-  const q1 = trpcReact.router01.foo.useQuery()
-  
+function MyComponent() {
+  const q1 = trpcReact.router01.foo.useQuery();
+
   const client = trpcReact.useUtils().client;
 
   useEffect(() => {
-    client.router01.foo.query()
-  }, [client])
-
-
-
+    client.router01.foo.query();
+  }, [client]);
 
   q1.data;
   // ^?
 
-  return null
+  return null;
+}
+
+export function App() {
+  return (
+    <TRPCReactProvider>
+      <MyComponent />
+    </TRPCReactProvider>
+  );
 }
