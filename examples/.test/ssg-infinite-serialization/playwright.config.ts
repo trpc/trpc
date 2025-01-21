fce26cb5421eab55562b8db1c87791d60187bd47
Alexander Johansson <alexander@n1s.se>
2025-01-21 11:09:56 +0100

    tweak

--- examples/.test/ssg-infinite-serialization/playwright.config.ts
+++ examples/.test/ssg-infinite-serialization/playwright.config.ts
@@ -1,4 +1,4 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test';
+import { devices, PlaywrightTestConfig } from '@playwright/test';
 
 const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000';
 console.log(`ℹ️ Using base URL "${baseUrl}"`);
