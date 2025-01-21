6cf24fced3e1a58a12db2d161916afacfe2acb8f
Alexander Johansson <alexander@n1s.se>
2025-01-21 10:29:03 +0100

    wip

--- examples/.test/monotest-app/src/App.tsx
+++ examples/.test/monotest-app/src/App.tsx
@@ -1,12 +1,8 @@
-import { router01Api } from './lib/router01'
+
 import { trpcReact } from './lib/trpc'
 
 export function App() {
   const q1 = trpcReact.router01.foo.useQuery() // <-- bug in trpc?
-  const q2 = router01Api.foo.useQuery() // <-- speedy and works with go to definition
-
-  if (q2.data === 'bar') {
-  }
 
   // 1. run `bun dev`
   // 2. change `generated-routers/router0/src/index.ts` and see it update here
