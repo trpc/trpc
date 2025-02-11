b2b3ec99a5f4cc364519d4ff936de1acaa1f6792
Alexander Johansson <alexander@n1s.se>
2025-02-11 15:43:43 +0100

    fixxxtures

--- packages/upgrade/test/transforms.test.ts
+++ packages/upgrade/test/transforms.test.ts
@@ -30,7 +30,7 @@ const testFixture = async (file: string, transform: any) => {
 };
 
 describe('hooks', () => {
-  const literal = './fixtures/hooks'; // idk why but Vite seems to do some shit when the string is in-lined to URL
+  const literal = './__fixtures__/hooks'; // idk why but Vite seems to do some shit when the string is in-lined to URL
   const fixturesDir = new URL(literal, import.meta.url).pathname;
 
   const ONLY_RUN: string[] = [];
@@ -43,7 +43,7 @@ describe('hooks', () => {
 });
 
 describe('provider', () => {
-  const literal = './fixtures/provider'; // idk why but Vite seems to do some shit when the string is in-lined to URL
+  const literal = './__fixtures__/provider'; // idk why but Vite seems to do some shit when the string is in-lined to URL
   const fixturesDir = new URL(literal, import.meta.url).pathname;
 
   const ONLY_RUN: string[] = [];
