7cd9f84f69dfdf89c6ff9fe1c1631d6356cf8f78
Alexander Johansson <alexander@n1s.se>
2025-02-11 17:55:00 +0100

    tweak

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -12,10 +12,6 @@ export {
    */
   createTRPCClient as createTRPCProxyClient,
   /**
-   * @deprecated - use `CreateTRPCClient` instead
-   */
-  type CreateTRPCClient as CreateTRPCProxyClient,
-  /**
    * @deprecated - use `inferRouterClient` instead
    */
   type inferRouterClient as inferRouterProxyClient,
