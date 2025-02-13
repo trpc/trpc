8971d99b27efcb1e02ec1a10effd32a5453cab5b
Alexander Johansson <alexander@n1s.se>
2025-02-13 11:09:13 +0100

    cool

---
+++ examples/minimal/src/server/context.ts
@@ -0,0 +1,14 @@
+import type { CreateHTTPContextOptions } from '@trpc/server/adapters/standalone';
+
+export async function createContext(opts: CreateHTTPContextOptions) {
+  // Create your context based on the request object
+  // Will be available as `ctx` in all your resolvers
+
+  return {
+    ...opts,
+    // ^?
+  };
+}
+
+// This context will be available as `ctx` in all your resolvers
+export type Context = Awaited<ReturnType<typeof createContext>>;
