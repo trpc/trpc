ecf1fe8e660fb8d9ba2f575506946746c6bfce92
Alexander Johansson <alexander@n1s.se>
2025-02-13 01:42:56 +0100

    is this fixed

--- examples/minimal-react/client/src/Greeting.tsx
+++ examples/minimal-react/client/src/Greeting.tsx
@@ -3,5 +3,6 @@ import { trpc } from './utils/trpc';
 export function Greeting() {
   const greeting = trpc.greeting.useQuery({ name: 'tRPC user' });
 
-  return <div>{greeting.data?.text}</div>;
+  return <div>{greeting.data?.text}</div>
 }
+      
