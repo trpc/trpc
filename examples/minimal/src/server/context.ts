5d81704a809227ed775ee00749b6f8c6e4a7e148
Alexander Johansson <alexander@n1s.se>
2025-02-13 11:12:03 +0100

    cool

--- examples/minimal/src/server/context.ts
+++ examples/minimal/src/server/context.ts
@@ -1,3 +1,7 @@
+/**
+ * Depending on your adapter, the first argument of {@link createContext} will be different.
+ * @see https://trpc.io/docs/server/context
+ */
 import type { CreateHTTPContextOptions } from '@trpc/server/adapters/standalone';
 
 export async function createContext(opts: CreateHTTPContextOptions) {
