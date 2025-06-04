3449cfcaf407f1cc8f97a4ce522030706c248708
Alexander Johansson <alexander@n1s.se>
2025-06-04 21:00:55 +0200

    whatever

--- packages/react-query/test/regression/issue-5808-proxy-in-dep-array.test.tsx
+++ packages/react-query/test/regression/issue-5808-proxy-in-dep-array.test.tsx
@@ -37,7 +37,10 @@ describe('original regression', () => {
     let effectCount = 0;
 
     function MyComponent() {
-      const result = client.deeply.nested.greeting.useQuery(undefined);
+      const result = client.deeply.nested.greeting.useQuery(undefined, {
+        enabled: false,
+        initialData: 'hello',
+      });
       const utils = client.useUtils();
 
       React.useEffect(() => {
