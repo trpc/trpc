801a080b06182943935e12336bd662ef1bc2a00a
Alexander Johansson <alexander@n1s.se>
2025-06-05 16:27:36 +0200

    cool

--- packages/server/src/unstable-core-do-not-import/clientish/serialize.test.ts
+++ packages/server/src/unstable-core-do-not-import/clientish/serialize.test.ts
@@ -1,3 +1,5 @@
+import type { inferRouterOutputs } from '@trpc/server';
+import { initTRPC } from '@trpc/server';
 import { z } from 'zod/v4';
 import type { Serialize } from './serialize';
 
@@ -18,202 +20,250 @@ test('zod v4 json', () => {
 });
 
 // regression test for https://github.com/trpc/trpc/issues/5197
-test('outputWithIndexSignature', () => {
-  type Source = {
-    a: number;
-    b: number;
-    [c: string]: number;
-  };
-  type Transformed = Serialize<Source>;
+describe('index signature and record', () => {
+  test('outputWithIndexSignature', () => {
+    type Source = {
+      a: number;
+      b: number;
+      [c: string]: number;
+    };
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<Source>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<Source>();
+  });
 
-test('outputWithRecord', () => {
-  type Source = {
-    a: number;
-    b: number;
-  } & Record<string, number>;
-  type Transformed = Serialize<Source>;
+  test('outputWithRecord', () => {
+    type Source = {
+      a: number;
+      b: number;
+    } & Record<string, number>;
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<Source>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<Source>();
+  });
 
-test('outputWithRecordAndIndexSignature', () => {
-  type Source = {
-    a: number;
-    b: number;
-    [c: string]: number;
-  } & Record<string, number>;
-  type Transformed = Serialize<Source>;
+  test('outputWithRecordAndIndexSignature', () => {
+    type Source = {
+      a: number;
+      b: number;
+      [c: string]: number;
+    } & Record<string, number>;
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<Source>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<Source>();
+  });
 
-test('outputWithUndefinedAndUndefinedIndexSignature', () => {
-  type Source = {
-    a: number;
-    b: number | undefined;
-    [c: string]: number | undefined;
-  };
-  type Transformed = Serialize<Source>;
+  test('outputWithUndefinedAndUndefinedIndexSignature', () => {
+    type Source = {
+      a: number;
+      b: number | undefined;
+      [c: string]: number | undefined;
+    };
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<{
-    [x: string]: number;
-    [x: number]: number;
-    a: number;
-  }>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<{
+      [x: string]: number;
+      [x: number]: number;
+      a: number;
+    }>();
+  });
 
-test('outputWithUndefinedAndUndefinedRecord', () => {
-  type Source = {
-    a: number;
-    b: number | undefined;
-  } & Record<string, number | undefined>;
-  type Transformed = Serialize<Source>;
+  test('outputWithUndefinedAndUndefinedRecord', () => {
+    type Source = {
+      a: number;
+      b: number | undefined;
+    } & Record<string, number | undefined>;
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<{
-    [x: string]: number;
-    a: number;
-  }>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<{
+      [x: string]: number;
+      a: number;
+    }>();
+  });
 
-test('outputWithUndefinedAndUndefinedRecordAndUndefinedIndexSignature', () => {
-  type Source = {
-    a: number;
-    b: number | undefined;
-    [c: string]: number | undefined;
-  } & Record<string, number | undefined>;
-  type Transformed = Serialize<Source>;
+  test('outputWithUndefinedAndUndefinedRecordAndUndefinedIndexSignature', () => {
+    type Source = {
+      a: number;
+      b: number | undefined;
+      [c: string]: number | undefined;
+    } & Record<string, number | undefined>;
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<{
-    [x: string]: number;
-    [x: number]: number;
-    a: number;
-  }>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<{
+      [x: string]: number;
+      [x: number]: number;
+      a: number;
+    }>();
+  });
 
-test('outputWithUndefinedIndexSignature', () => {
-  type Source = {
-    a: number;
-    b: number;
-    [c: string]: number | undefined;
-  };
-  type Transformed = Serialize<Source>;
+  test('outputWithUndefinedIndexSignature', () => {
+    type Source = {
+      a: number;
+      b: number;
+      [c: string]: number | undefined;
+    };
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<{
-    [x: string]: number;
-    [x: number]: number;
-    a: number;
-    b: number;
-  }>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<{
+      [x: string]: number;
+      [x: number]: number;
+      a: number;
+      b: number;
+    }>();
+  });
 
-test('outputWithUndefinedRecord', () => {
-  type Source = {
-    a: number;
-    b: number;
-  } & Record<string, number | undefined>;
-  type Transformed = Serialize<Source>;
+  test('outputWithUndefinedRecord', () => {
+    type Source = {
+      a: number;
+      b: number;
+    } & Record<string, number | undefined>;
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<{
-    [x: string]: number;
-    a: number;
-    b: number;
-  }>();
-});
+    expectTypeOf<Transformed>().toEqualTypeOf<{
+      [x: string]: number;
+      a: number;
+      b: number;
+    }>();
+  });
 
-test('outputWithUndefinedRecordAndUndefinedIndexSignature', () => {
-  type Source = {
-    a: number;
-    b: number;
-    [c: string]: number | undefined;
-  } & Record<string, number | undefined>;
-  type Transformed = Serialize<Source>;
+  test('outputWithUndefinedRecordAndUndefinedIndexSignature', () => {
+    type Source = {
+      a: number;
+      b: number;
+      [c: string]: number | undefined;
+    } & Record<string, number | undefined>;
+    type Transformed = Serialize<Source>;
 
-  expectTypeOf<Transformed>().toEqualTypeOf<{
-    [x: string]: number;
-    [x: number]: number;
-    a: number;
-    b: number;
-  }>();
+    expectTypeOf<Transformed>().toEqualTypeOf<{
+      [x: string]: number;
+      [x: number]: number;
+      a: number;
+      b: number;
+    }>();
+  });
 });
 
 // regression test for https://github.com/trpc/trpc/issues/5075
-test('Test serialization of different zod schemas against z.infer', () => {
-  const mixedSchema = z.object({
-    zArray: z.array(z.string()),
-    zRecord: z.record(z.string(), z.string()),
-    zTuple: z.tuple([z.string(), z.number()]),
-    zUnion: z.union([z.string(), z.number()]),
-    zIntersection: z.intersection(
-      z.object({ name: z.string() }),
-      z.object({ age: z.number() }),
-    ),
-    zLazy: z.lazy(() => z.string()),
-    zPromise: z.promise(z.string()),
-    zFunction: z.function(),
-    zMap: z.map(z.string(), z.number()),
-    zSet: z.set(z.string()),
-    zEnum: z.enum(['foo', 'bar']),
-    zNativeEnum: z.nativeEnum({ foo: 1, bar: 2 }),
-    zUnknown: z.unknown(),
-    zNullable: z.nullable(z.string()),
-    zOptional: z.optional(z.string()),
-    zLiteral: z.literal('foo'),
-    zBoolean: z.boolean(),
-    zString: z.string(),
-    zNumber: z.number(),
-    zBigint: z.bigint(),
-    zDate: z.date(),
-    zUndefined: z.undefined(),
-    zAny: z.any(),
-    zArrayOptional: z.array(z.string()).optional(),
-    zArrayOrRecord: z.union([
-      z.array(z.string()),
-      z.record(z.string(), z.string()),
-    ]),
+describe('complex zod schema serialization', () => {
+  test('Test serialization of different zod schemas against z.infer', () => {
+    const mixedSchema = z.object({
+      zArray: z.array(z.string()),
+      zRecord: z.record(z.string(), z.string()),
+      zTuple: z.tuple([z.string(), z.number()]),
+      zUnion: z.union([z.string(), z.number()]),
+      zIntersection: z.intersection(
+        z.object({ name: z.string() }),
+        z.object({ age: z.number() }),
+      ),
+      zLazy: z.lazy(() => z.string()),
+      zPromise: z.promise(z.string()),
+      zFunction: z.function(),
+      zMap: z.map(z.string(), z.number()),
+      zSet: z.set(z.string()),
+      zEnum: z.enum(['foo', 'bar']),
+      zNativeEnum: z.nativeEnum({ foo: 1, bar: 2 }),
+      zUnknown: z.unknown(),
+      zNullable: z.nullable(z.string()),
+      zOptional: z.optional(z.string()),
+      zLiteral: z.literal('foo'),
+      zBoolean: z.boolean(),
+      zString: z.string(),
+      zNumber: z.number(),
+      zBigint: z.bigint(),
+      zDate: z.date(),
+      zUndefined: z.undefined(),
+      zAny: z.any(),
+      zArrayOptional: z.array(z.string()).optional(),
+      zArrayOrRecord: z.union([
+        z.array(z.string()),
+        z.record(z.string(), z.string()),
+      ]),
+    });
+
+    type Source = z.infer<typeof mixedSchema>;
+    type Transformed = Serialize<Source>;
+
+    expectTypeOf<Transformed['zArray']>().toEqualTypeOf<string[]>();
+    expectTypeOf<Transformed['zRecord']>().toEqualTypeOf<
+      Record<string, string>
+    >();
+    expectTypeOf<Transformed['zTuple']>().toEqualTypeOf<[string, number]>();
+    expectTypeOf<Transformed['zUnion']>().toEqualTypeOf<string | number>();
+    expectTypeOf<Transformed['zIntersection']>().toMatchObjectType<{
+      name: string;
+      age: number;
+    }>();
+    expectTypeOf<Transformed['zLazy']>().toEqualTypeOf<string>();
+
+    expectTypeOf<Transformed['zMap']>().toEqualTypeOf<object>();
+    expectTypeOf<Transformed['zSet']>().toEqualTypeOf<object>();
+    expectTypeOf<Transformed['zEnum']>().toEqualTypeOf<'foo' | 'bar'>();
+    expectTypeOf<Transformed['zNativeEnum']>().toEqualTypeOf<number>();
+    expectTypeOf<Transformed['zUnknown']>().toEqualTypeOf<unknown>();
+    expectTypeOf<Transformed['zNullable']>().toEqualTypeOf<string | null>();
+    expectTypeOf<Transformed['zOptional']>().toEqualTypeOf<
+      string | undefined
+    >();
+    expectTypeOf<Transformed['zLiteral']>().toEqualTypeOf<'foo'>();
+    expectTypeOf<Transformed['zBoolean']>().toEqualTypeOf<boolean>();
+    expectTypeOf<Transformed['zString']>().toEqualTypeOf<string>();
+    expectTypeOf<Transformed['zNumber']>().toEqualTypeOf<number>();
+    // @ts-expect-error - not serialized, OK.
+    expectTypeOf<Transformed['zBigint']>().toEqualTypeOf<bigint>();
+    expectTypeOf<Transformed['zDate']>().toEqualTypeOf<string>();
+
+    // @ts-expect-error - not serialized, OK.
+    expectTypeOf<Transformed['zUndefined']>().toEqualTypeOf<undefined>();
+
+    expectTypeOf<Transformed['zAny']>().toEqualTypeOf<any>();
+
+    expectTypeOf<Transformed['zArrayOptional']>().toEqualTypeOf<
+      string[] | undefined
+    >();
+    expectTypeOf<Transformed['zArrayOrRecord']>().toEqualTypeOf<
+      string[] | Record<string, string>
+    >();
   });
 
-  type Source = z.infer<typeof mixedSchema>;
-  type Transformed = Serialize<Source>;
+  test('Non-records should not erroneously be inferred as Records in serialized types', () => {
+    const zChange = z.object({
+      status: z
+        .tuple([
+          z.literal('tmp').or(z.literal('active')),
+          z.literal('active').or(z.literal('disabled')),
+        ])
+        .optional(),
+      validFrom: z.tuple([z.string().nullable(), z.string()]).optional(),
+      validTo: z
+        .tuple([z.string().nullable(), z.string().nullable()])
+        .optional(),
+      canceled: z.tuple([z.boolean(), z.boolean()]).optional(),
+      startsAt: z
+        .tuple([z.string().nullable(), z.string().nullable()])
+        .optional(),
+      endsAt: z
+        .tuple([z.string().nullable(), z.string().nullable()])
+        .optional(),
+    });
+    type Change = z.infer<typeof zChange>;
 
-  expectTypeOf<Transformed['zArray']>().toEqualTypeOf<string[]>();
-  expectTypeOf<Transformed['zRecord']>().toEqualTypeOf<
-    Record<string, string>
-  >();
-  expectTypeOf<Transformed['zTuple']>().toEqualTypeOf<[string, number]>();
-  expectTypeOf<Transformed['zUnion']>().toEqualTypeOf<string | number>();
-  expectTypeOf<Transformed['zIntersection']>().toMatchObjectType<{
-    name: string;
-    age: number;
-  }>();
-  expectTypeOf<Transformed['zLazy']>().toEqualTypeOf<string>();
+    const t = initTRPC.create();
 
-  expectTypeOf<Transformed['zMap']>().toEqualTypeOf<object>();
-  expectTypeOf<Transformed['zSet']>().toEqualTypeOf<object>();
-  expectTypeOf<Transformed['zEnum']>().toEqualTypeOf<'foo' | 'bar'>();
-  expectTypeOf<Transformed['zNativeEnum']>().toEqualTypeOf<number>();
-  expectTypeOf<Transformed['zUnknown']>().toEqualTypeOf<unknown>();
-  expectTypeOf<Transformed['zNullable']>().toEqualTypeOf<string | null>();
-  expectTypeOf<Transformed['zOptional']>().toEqualTypeOf<string | undefined>();
-  expectTypeOf<Transformed['zLiteral']>().toEqualTypeOf<'foo'>();
-  expectTypeOf<Transformed['zBoolean']>().toEqualTypeOf<boolean>();
-  expectTypeOf<Transformed['zString']>().toEqualTypeOf<string>();
-  expectTypeOf<Transformed['zNumber']>().toEqualTypeOf<number>();
-  // @ts-expect-error - not serialized, OK.
-  expectTypeOf<Transformed['zBigint']>().toEqualTypeOf<bigint>();
-  expectTypeOf<Transformed['zDate']>().toEqualTypeOf<string>();
+    const router = t.router({
+      createProject: t.procedure.output(zChange).query(() => {
+        return zChange.parse(null as any);
+      }),
+      createProjectNoExplicitOutput: t.procedure.query(() => {
+        return zChange.parse(null as any);
+      }),
+    });
 
-  // @ts-expect-error - not serialized, OK.
-  expectTypeOf<Transformed['zUndefined']>().toEqualTypeOf<undefined>();
+    type SerializedOutput = inferRouterOutputs<typeof router>['createProject'];
+    type SerializedOutputNoExplicitOutput = inferRouterOutputs<
+      typeof router
+    >['createProjectNoExplicitOutput'];
 
-  expectTypeOf<Transformed['zAny']>().toEqualTypeOf<any>();
-
-  expectTypeOf<Transformed['zArrayOptional']>().toEqualTypeOf<
-    string[] | undefined
-  >();
-  expectTypeOf<Transformed['zArrayOrRecord']>().toEqualTypeOf<
-    string[] | Record<string, string>
-  >();
+    expectTypeOf<SerializedOutput>().toEqualTypeOf<Change>();
+    expectTypeOf<SerializedOutputNoExplicitOutput>().toEqualTypeOf<Change>();
+  });
 });
