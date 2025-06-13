5bff718155207d275b435bd9db55aef971bbe4c6
Alexander Johansson <alexander@n1s.se>
2025-06-13 18:05:13 +0200

    docs: sponsor sync

--- www/src/components/sponsors/script.output.ts
+++ www/src/components/sponsors/script.output.ts
@@ -11,6 +11,14 @@ export const topSponsors = [
     "createdAt": 1656581731000
   },
   {
+    "name": "Retool",
+    "imgSrc": "https://avatars.githubusercontent.com/u/33817679?v=4",
+    "weight": 68,
+    "login": "tryretool",
+    "link": "https://retool.com/?ref=trpc&utm_source=github&utm_medium=referral&utm_campaign=trpc",
+    "createdAt": 1720565283000
+  },
+  {
     "name": "Ahoy Labs",
     "imgSrc": "https://avatars.githubusercontent.com/u/95662801?v=4",
     "weight": 31,
@@ -33,14 +41,6 @@ export const topSponsors = [
     "login": "coderabbitai",
     "link": "https://www.coderabbit.ai/?utm_source=github&utm_medium=referral&ref=trpc&utm_campaign=trpc",
     "createdAt": 1737645299000
-  },
-  {
-    "name": "Dr. B",
-    "imgSrc": "https://avatars.githubusercontent.com/u/77294655?v=4",
-    "weight": 14,
-    "login": "hidrb",
-    "link": "https://github.com/hidrb",
-    "createdAt": 1633034677000
   }
 ] as const;
 
@@ -231,6 +231,14 @@ export const allSponsors = [
     "createdAt": 1719988994000
   },
   {
+    "name": "Retool",
+    "imgSrc": "https://avatars.githubusercontent.com/u/33817679?v=4",
+    "weight": 68,
+    "login": "tryretool",
+    "link": "https://retool.com/?ref=trpc&utm_source=github&utm_medium=referral&utm_campaign=trpc",
+    "createdAt": 1720565283000
+  },
+  {
     "name": "Leo Jace",
     "imgSrc": "https://avatars.githubusercontent.com/u/5610115?u=be201750effeb8826a788dea206e594008cfe3f2&v=4",
     "weight": 3,
@@ -325,5 +333,13 @@ export const allSponsors = [
     "login": "enricoros",
     "link": "https://github.com/enricoros",
     "createdAt": 1748302641000
+  },
+  {
+    "name": "Graphite",
+    "imgSrc": "https://github.com/withgraphite.png",
+    "weight": 7,
+    "login": "withgraphite",
+    "link": "https://graphite.dev",
+    "createdAt": 1749679200000
   }
 ] as const;
