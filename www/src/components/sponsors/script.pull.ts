4ea1d25de8ba93f6c1108952adc13a7af4ccbbb4
Alexander Johansson <alexander@n1s.se>
2025-07-11 19:35:50 +0200

    chore: sponsor update

--- www/src/components/sponsors/script.pull.ts
+++ www/src/components/sponsors/script.pull.ts
@@ -198,7 +198,7 @@ async function main() {
       name: 'Graphite',
       imgSrc: 'https://github.com/withgraphite.png',
       monthlyPriceInDollars: 1000,
-      link: 'https://graphite.dev/?utm_source=github&utm_medium=repo&utm_campaign=neovim',
+      link: 'https://graphite.dev/?utm_source=github&utm_medium=repo&utm_campaign=trpc',
       privacyLevel: 'PUBLIC',
       login: 'withgraphite',
       createdAt: new Date(2025, 5, 12).getTime(),
