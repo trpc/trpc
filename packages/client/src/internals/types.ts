9ff4f407f172add5bec0304be4486de7c36588b9
Alexander Johansson <alexander@n1s.se>
2025-08-04 13:15:36 +0200

    maybe

--- packages/client/src/internals/types.ts
+++ packages/client/src/internals/types.ts
@@ -34,7 +34,14 @@ export interface RequestInitEsque {
   /**
    * Sets the request's body.
    */
-  body?: FormData | string | null | Uint8Array | Blob | File;
+  body?:
+    | FormData
+    | string
+    | null
+    | ArrayBufferView<ArrayBuffer>
+    | ArrayBuffer
+    | Blob
+    | File;
 
   /**
    * Sets the request's associated headers.
