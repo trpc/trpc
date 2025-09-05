a10128df1c677db938521941439f758c5c311b8c
Alexander Johansson <alexander@n1s.se>
2025-09-05 16:50:40 +0200

    rm tanstack start

--- examples/tanstack-start/app/routes/posts.index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/posts/')({
-  component: PostsIndexComponent,
-});
-
-function PostsIndexComponent() {
-  return <div>Select a post.</div>;
-}
