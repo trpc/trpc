4067270ab06810a09e1566d3c1407e9c8907372d
wobsoriano <sorianorobertc@gmail.com>
2025-10-24 20:46:11 -0700

    chore(examples): Fix nuxt e2e test

--- examples/nuxt/server/api/trpc/[trpc].ts
+++ examples/nuxt/server/api/trpc/[trpc].ts
@@ -2,7 +2,7 @@
  * This is the API-handler of your app that contains all your API routes.
  * On a bigger app, you will probably want to split this file up into multiple files.
  */
-import { publicProcedure, router } from '~/server/trpc/init';
+import { publicProcedure, router } from '~~/server/trpc/init';
 import { createTRPCNuxtHandler } from 'trpc-nuxt/server';
 import { z } from 'zod';
 
