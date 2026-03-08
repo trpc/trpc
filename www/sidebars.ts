2db61bc836ab3bbdc51ddb148669f99f6aaaca37
Nick Lucas <me@nicklucas.co.uk>
2026-03-08 09:55:52 +0000

    Add intiial docs

--- www/sidebars.ts
+++ www/sidebars.ts
@@ -180,6 +180,7 @@ const config: SidebarsConfig = {
             'client/links/retryLink',
           ],
         },
+        'client/openapi',
         'client/headers',
         'client/cors',
       ],
